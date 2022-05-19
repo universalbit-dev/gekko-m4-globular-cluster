e5da533ee5d16e29c4845908a2fcbd5cb9847253
universalbit-blockchain <infouniversalbits@gmail.com>
2022-05-19 22:12:07 +0800

    Upload files to ''

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf backtest*
+#Remove all "*.js~" extension file from folder and subfolder before start
+find . -name "*.js~" -type f -delete
+#Remove all "*.toml~" extension file from folder and subfolder before start
+find . -name "*.toml~" -type f -delete
+find . -name "*.json.swp" -type f -delete
+find . -name ".*.json.swp" -type f -delete
+find . -name "*.swp" -type f -delete
+echo "App Cleaned"
+
