f235dbf3e5e25d108dc4b2ac08137f0b2a01dfc0
universalbit-blockchain <infouniversalbits@gmail.com>
2022-06-14 06:52:30 +0800

    Update 'strategies/INVERTER.js'

--- strategies/INVERTER.js
+++ strategies/INVERTER.js
@@ -3,8 +3,8 @@
 
 Extra Indicators : https://github.com/Gab0/gekko-extra-indicators
 Gabriel Araujo (Gab0)
--Stop Loss Stop Gain
--Short Spread
+-Stop_Loss Stop_Gain
+-Min_Loss  Min_Gain
 
 RSI Bull and Bear + ADX modifier
 Use different RSI-strategies depending on a longer trend
@@ -19,9 +19,10 @@ INVERTER
 https://creativecommons.org/licenses/by-sa/4.0/
 Update:
 - Switch
-- Inverter  
-- Indicators : DEMA && SMA
+- Inverter
+- Indicators DEMA && SMA
 */
+
 var log = require('../core/log.js');
 var config = require('../core/util.js').getConfig();
 var coretulind = require('../core/tulind.js');
@@ -35,10 +36,10 @@ var strat = {
 // INIT
 init: function()
 	{
-		this.name = 'INVERTER';
-		this.resetTrend();
+this.name = 'INVERTER';
+this.resetTrend();
 // DEBUG
-		this.debug = true;
+this.debug = true;
 //SMA
 this.addTulipIndicator('maFast', 'sma', { optInTimePeriod: this.settings.SMA_long});
 this.addTulipIndicator('maSlow', 'sma', { optInTimePeriod: this.settings.SMA_short});
@@ -50,9 +51,9 @@ this.addTulipIndicator('longDEMA', 'dema', {optInTimePeriod : this.settings.DEMA
 
 this.addTulipIndicator('shortDEMA', 'dema', {optInTimePeriod : this.settings.DEMA_short});
 //RSI
-this.addTulipIndicator('rsi', 'rsi', {optInTimePeriod : this.settings.rsiSize});
+this.addTulipIndicator('rsi', 'rsi', {optInTimePeriod : this.settings.RSI});
 //ADX
-this.addTulipIndicator('ADX', 'adx', { optInTimePeriod: this.settings.ADX});
+this.addTulipIndicator('ADX', 'adx', {optInTimePeriod: this.settings.ADX});
 //Mod (RSI modifiers)
 this.BULL_MOD_high = this.settings.BULL_MOD_high;
 this.BULL_MOD_low = this.settings.BULL_MOD_low;
@@ -70,104 +71,95 @@ this.Min_Gain_Percent = this.settings.Min_Gain_Percent;
 //Debug
 this.startTime = new Date();
 if( this.debug ){
-this.stat = {
-adx:{ min: 1000, max: 0 },
-	   bear: { min: 1000, max: 0 },
-	   bull: { min: 1000, max: 0 }};
-	}
+this.stat = {adx: { min: 1000, max: 0 },bear: { min: 1000, max: 0 },bull: { min: 1000, max: 0 }};}
 //Messages
-	log.info("==========================================");
-	log.info('Running', this.name);
-	log.info('==========================================');
+		log.info("==========================================");
+		log.info('Running', this.name);
+		log.info('==========================================');
 	},
 //Reset Trend
 resetTrend: function()
-	{
-    let trend = {
-	duration: 0,
-	direction: 'none',
-	longPos: 0,
-	pingPong : {gainsPercentage: this.settings.PINGPONG_GAINS_PERCENTAGE }
-	};
-	this.trend = trend;
+{
+let trend = {duration: 0,direction: 'none',longPos: 0,pingPong : {gainsPercentage: this.settings.PINGPONG_GAINS_PERCENTAGE }};
+this.trend = trend;
 //Log Trend
-	console.log(this.trend);
-	},
+console.log(this.trend);
+},
 //Low/High backtest-period
-	lowHigh: function(val,type)
-	{
-    let cur = this.stat.bear;
-	if( type == 'bear' )
-	{
-	cur = this.stat.bear;
-	if( val < cur.min ) this.stat.bear.min = val;
+lowHigh: function(val,type)
+{
+let cur = this.stat.bear;
+if( type == 'bear' )
+{
+cur = this.stat.bear;
+if( val < cur.min ) this.stat.bear.min = val;
 	else if( val > cur.max ) this.stat.bear.max = val;
-	}
+}
 
-	else if( type == 'bull' )
-	{
-	cur = this.stat.bull;
+if( type == 'bull' )
+{
+cur = this.stat.bull;
 	if( val < cur.min ) this.stat.bull.min = val;
 	else if( val > cur.max ) this.stat.bull.max = val;
-	}
+}
 
-	else
-	{
-	cur = this.stat.adx;
-	if( val < cur.min ) this.stat.adx.min = val;
+else
+{
+cur = this.stat.adx;
+if( val < cur.min ) this.stat.adx.min = val;
 	else if( val > cur.max ) this.stat.adx.max = val;
-	}
-
-	},
+}
+  },
 //Check
-	check: function()
-	{
+check: function()
+{
 //Indicators
 let ind = this.tulipIndicators,rsi = ind.rsi.result.result,maSlow = ind.maSlow.result.result,maFast = ind.maFast.result.result,longDEMA = ind.longDEMA.result.result,shortDEMA = ind.shortDEMA.result.result,adx = ind.ADX.result.result;
 
 switch (this.candle.close) {
 	case ((this.stoplow != 0.0)&&(this.candle.close < this.stoplow)):
 	this.advice('short');
-        break;
+    break;
 	case ((this.stophigh != 0.0)&&(this.candle.close > this.stophigh)):
 	this.advice('short');
 	break;
 }
 //Bear
-	if((longDEMA < shortDEMA) && (maFast < maSlow))
+if((longDEMA < shortDEMA) && (maFast < maSlow))
 {
 	rsi = ind.BEAR_RSI.result.result;
 	let rsi_hi = this.settings.BEAR_RSI_high,rsi_low = this.settings.BEAR_RSI_low;
 // Adx
 	if( adx > this.settings.ADX_high ) rsi_hi = rsi_hi + this.BEAR_MOD_high;
-	 else if( adx < this.settings.ADX_low ) rsi_low = rsi_low + this.BEAR_MOD_low;
+	else if( adx < this.settings.ADX_low ) rsi_low = rsi_low + this.BEAR_MOD_low;
 
 	if( rsi > rsi_hi ) this.short();
-	else if( rsi < rsi_low ) this.long();
+	else if( rsi < rsi_low ) this.long(this.candle);
 	else this.pingPong();
-	if(this.debug) this.lowHigh( rsi, 'bear' );
+	this.lowHigh( rsi, 'bear' );
 }
+
 //Bull
-  else
+else
 {
-	rsi = ind.BULL_RSI.result.result;
-	let rsi_hi = this.settings.BULL_RSI_high,rsi_low = this.settings.BULL_RSI_low;
-// Adx
+rsi = ind.BULL_RSI.result.result;
+let rsi_hi = this.settings.BULL_RSI_high,rsi_low = this.settings.BULL_RSI_low;
+
 	if( adx > this.settings.ADX_high ) rsi_hi = rsi_hi + this.BULL_MOD_high;
-	 else if( adx < this.settings.ADX_low ) rsi_low = rsi_low + this.BULL_MOD_low;
+	else if( adx < this.settings.ADX_low ) rsi_low = rsi_low + this.BULL_MOD_low;
 
 	if( rsi > rsi_hi ) this.short();
-	else if( rsi < rsi_low ) this.long();
+	else if( rsi < rsi_low ) this.long(this.candle);
 	else this.pingPong();
 
-	if(this.debug) this.lowHigh( rsi, 'bull' );
+	this.lowHigh( rsi, 'bull' );
 }
 
 },
 
 //LONG
-	long: function()
-	{
+long: function()
+{
 	if( this.trend.direction !== 'up' )
 	{
 	this.resetTrend();
@@ -189,7 +181,7 @@ switch (this.candle.close) {
 //SHORT
 	short: function()
 	{
-	if ((this.trend.direction !== 'down') || (this.stoplow != 0.0 ) && (this.candle.close < this.stoplow ) || (this.stophigh != 0.0 ) && (this.candle.close > this.stophigh))
+	if ((this.trend.direction !== 'down') || ((this.stoplow != 0.0 ) && (this.candle.close < this.stoplow )) || ((this.stophigh != 0.0 ) && (this.candle.close > this.stophigh)) )
 	{
  	this.resetTrend();
  	this.trend.direction = 'down';
@@ -197,15 +189,6 @@ switch (this.candle.close) {
  	this.advice('short');
  	if(this.debug) log.info('Going short');
  	}
-
-	else if ((this.trend.direction !== 'down') || (this.candle.close > this.lastLongPrice+(this.lastLongPrice * this.Min_Gain_Percent / 100) && (this.candle.close < this.lastLongPrice-(this.lastLongPrice * this.Min_Loss_Percent / 100))))
-	{
-	this.resetTrend();
-	this.trend.direction = 'down';
-    this.trend.longPos = false;
-	this.advice('short');
-	if( this.debug ) log.info('Going short');
-	}
 
 	if( this.debug )
 	{
@@ -217,19 +200,20 @@ switch (this.candle.close) {
 
 //PingPong
 pingPong: function() {
+
  switch (this.trend.longPos) {
 	case (this.candle.close < (this.trend.longPos - (this.trend.longPos * (this.trend.pingPong.gainsPercentage / 3) / 100))):
 	this.trend.longPos = this.candle.close;
 	break;
-  case (this.candle.close < (this.trend.longPos + (this.trend.longPos * this.trend.pingPong.gainsPercentage / 100))):
+  	case (this.candle.close < (this.trend.longPos + (this.trend.longPos * this.trend.pingPong.gainsPercentage / 100))):
 	this.trend.longPos = false;
 	this.advice('short');
  	break;
-  case (this.trend.direction == 'down'):return;
+	case (this.trend.direction == 'down'):return;
 	break;
-  default:
-    this.trend.longPos = this.candle.close;
-    this.advice('long');
+  	default:
+  	this.trend.longPos = this.candle.close;
+  	this.advice('long');
 }},
 //End Backtest
 	end: function()
@@ -239,15 +223,12 @@ pingPong: function() {
 	log.info('====================================');
 	log.info('Finished in ' + str);
 	log.info('====================================');
-
 // Stats and Messages
-	if(this.debug)
-	{
 	let stat = this.stat;
 	log.info('BEAR RSI low/high: ' + stat.bear.min + ' / ' + stat.bear.max);
 	log.info('BULL RSI low/high: ' + stat.bull.min + ' / ' + stat.bull.max);
 	log.info('ADX min/max: ' + stat.adx.min + ' / ' + stat.adx.max);
 	}
-	}
+
 };
 module.exports = strat;
