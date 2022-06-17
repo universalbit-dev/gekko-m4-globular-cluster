fe9236ec588f7b6b46f7f9867f08056d6419b1c8
universalbit-blockchain <infouniversalbits@gmail.com>
2022-06-17 18:19:46 +0800

    Delete 'web/vue/statics/indicators/SMA.js'

--- web/vue/statics/indicators/SMA.js
+++
@@ -1,7 +0,0 @@
-exports.Indicator = function ()
-{
-  var Indicator=function(windowLength){this.input='price';this.windowLength=windowLength;this.prices=[];
-  this.result=0;this.age=0;this.sum=0;}
-  Indicator.prototype.update=function(price){var tail=this.prices[this.age]||0;this.prices[this.age]=price;this.sum+=price-tail;this.result=this.sum/this.prices.length;this.age=(this.age+1)%this.windowLength}
-};
-require('./SMA.js')() //require itself and run the exports object
