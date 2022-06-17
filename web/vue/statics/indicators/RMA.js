536901969fc944a6c45da8a741495e7877af211f
universalbit-blockchain <infouniversalbits@gmail.com>
2022-06-17 18:17:41 +0800

    Delete 'web/vue/statics/indicators/RMA.js'

--- web/vue/statics/indicators/RMA.js
+++
@@ -1,8 +0,0 @@
-exports.Indicator = function () {
-
-var Indicator=function(weight){this.input='price';this.weight=weight;this.result=false;this.age=0;}
-Indicator.prototype.update=function(price){if(this.result===false)
-this.result=price;this.age++;this.calculate(price);return this.result;}
-Indicator.prototype.calculate=function(price){var k=1/this.weight;var y=this.result;this.result=price*k+y*(1-k);}
-};
-require('./RMA.js')() //require itself and run the exports object
