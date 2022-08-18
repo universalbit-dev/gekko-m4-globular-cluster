4a2f9d828a15a499658fadc275886e9376e2dbd2
universalbit-blockchain <infouniversalbits@gmail.com>
2022-08-19 05:30:25 +0800

    Update 'config.js'

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 /*
-
 The MIT License (MIT)
 Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -13,13 +12,12 @@ by this software. There can be bugs and the bot may not perform as expected
 or specified. Please consider testing it first with paper trading and/or
 backtesting on historical data. Also look at the code to see what how
 it is working.
-
 */
 
 var config = {};
 //General Settings
 config.debug =true;
-//Watching a Market
+//**Watching a Market [BTC-LTC]
 config.watch = {exchange: 'bitfinex',currency: 'BTC',asset: 'LTC'};
 config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC'};
 config.watch = {exchange: 'poloniex',currency:'BTC',asset:'LTC'};
@@ -32,37 +30,27 @@ config.tradingAdvisor.historySize=1;
 config.tradingAdvisor.method= 'INVERTER';
 //Plugin
 config.paperTrader ={enabled:true};
-config.paperTrader ={reportInCurrency: true,simulationBalance: {asset: 1,currency: 100},verbose: false,feeMaker: 0.5,feeTaker: 0.5,feeUsing: 'maker',slippage: 0.05};
+config.paperTrader = {reportInCurrency: true,simulationBalance: {asset: 1,currency: 100},verbose: false,feeMaker: 0.5,feeTaker:0.5,feeUsing: 'maker',slippage: 0.05};
 //Adapter
 config.adapter='sqlite';
-//Api
-config.trader ={enabled:false,exchange:'bitfinex',currency:'BTC',asset:'LTC',key:'API',secret:'SECRET'};
-config.trader ={enabled:false,exchange:'kraken',currency:'XBT',asset:'LTC',key:'API',secret:'SECRET'};
-config.trader ={enabled:false,exchange:'poloniex',currency:'BTC',asset:'LTC',key:'API',secret:'SECRET'};
-config.trader ={enabled:false,exchange:'exmo',currency:'BTC',asset:'LTC',key:'API',secret:'SECRET'};
+//**BTC-LTC trader=enabled:false
+config.trader ={enabled:false,exchange:'bitfinex',currency:'BTC',asset:'LTC',key:'',secret:''};
+config.trader={enabled:false,exchange:'kraken',currency:'XBT',asset:'LTC',key:'',secret:''};
+config.trader={enabled:false,exchange:'poloniex',currency:'BTC',asset:'LTC',key:'',secret:''};
+config.trader={enabled:false,exchange:'exmo',currency:'LTC',asset:'ETH',key:'',secret:''};
 
 config.candleWriter={enabled:false,adapter:'sqlite'};
 //BackTest
 config.backtest ={enabled:true};
-config.backtest = {
-daterange: 'scan',
-// daterange: {from: "2022-07-13",to: "2022-07-20"},
-batchSize: 50};
-//**BackTest Result Exporter
-config.backtestResultExporter = {
-enabled: false,writeToDisk: false,
-data: {stratUpdates: false,portfolioValues: true,stratCandles: true,roundtrips: true,trades: true}
-};
-
 //Importer
 config.importer={enabled:true};
-config.sqlite = {path: 'plugins/sqlite',dataDirectory: 'history',version: 0.1,journalMode: require('./web/isWindows.js') ? 'DELETE' : 'WAL',dependencies:[{module: 'sqlite3'}]};
+config.sqlite = {path: 'plugins/sqlite',dataDirectory: 'history',version: 0.1,journalMode: require('./web/isWindows.js') ? 'DELETE' : 'WAL',dependencies:[{module: 'sqlite3'}] };
 //Child to Parent
 //config.childToParent = {enabled: true};
-
 //Strategy
 config.method='INVERTER';
-config.INVERTER = {
+
+config.INVERTER= {
 SMA_long:10,SMA_short:55,DEMA_short:10,DEMA_long:21,RSI:14,
 BULL_RSI:7,BULL_RSI_high:82,BULL_RSI_low:60,
 BEAR_RSI:14,BEAR_RSI_high:60,BEAR_RSI_low:15,
@@ -70,6 +58,6 @@ BULL_MOD_high:5,BULL_MOD_low:-5,BEAR_MOD_high:15,BEAR_MOD_low:-5,
 ADX:14,ADX_high:70,ADX_low:50,
 Stop_Loss_Percent:3,Stop_Gain_Percent:3,Min_Loss_Percent:1.5,Min_Gain_Percent:1.5,
 PINGPONG_GAINS_PERCENTAGE:10};
-config['I understand that Gekko only automates MY OWN trading strategies']=false;
-module.exports = config;
 
+config['I understand that Gekko only automates MY OWN trading strategies']=true;
+module.exports = config;
