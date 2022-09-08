536d6f87ff8eb936891abc910c2cfe4931db5643
universalbit-blockchain <infouniversalbits@gmail.com>
2022-09-09 05:20:12 +0800

    Update 'gekko-m4-nodejs_v12-ccxt.js'

--- gekko-m4-nodejs_v12-ccxt.js
+++ gekko-m4-nodejs_v12-ccxt.js
@@ -8,7 +8,7 @@ pm2.connect(function(err) {
   pm2.start({
     script    : 'gekko.js',
     args      : '-c config.js --ui',
-    name      : 'gekko-m4-nodejs_v12_ccxt'
+    name      : 'gekko-m4.js'
   }, function(err, apps) {
     if (err) {
       console.error(err)
