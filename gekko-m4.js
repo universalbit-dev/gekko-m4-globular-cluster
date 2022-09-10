c18b0f6900dfc39596f674362c2dc9e6394472f6
universalbit-blockchain <infouniversalbits@gmail.com>
2022-09-11 00:31:17 +0800

    Update 'gekko-m4.js'

--- gekko-m4.js
+++ gekko-m4.js
@@ -1,15 +1,30 @@
 const pm2 = require('pm2')
-  pm2.connect(function(err) {
-  if (err) {console.error(err)
-  process.exit(2)}
+pm2.connect(function(err) {
+  if (err) {
+    console.error(err)
+    process.exit(2)
+  }
 
-  pm2.start({script:'gekko.js',args:'-c config.js --ui',name:'gekko-m4.js'},
-  function(err, apps) {if (err) {console.error(err)
-  return pm2.disconnect()}
-  pm2.list((err, list) => {console.log(err, list)})
-  })})
+  pm2.start({
+    script    : 'gekko.js',
+    args      : '-c config.js --ui',
+    name      : 'gekko-m4.js'
+
+  },
+
+function(err, apps) {
+if (err) {
+      console.error(err)
+      return pm2.disconnect()
+    }
+
+    pm2.list((err, list) => {
+      console.log(err, list)
 
 
+    })
+  })
+})
 /*
 The MIT License (MIT)
 Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
