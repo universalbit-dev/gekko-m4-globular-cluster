140e783b6bc2adf8bdc6dbb346f97e92b43e1ad4
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-13 04:17:40 +0100

    Update prepareDateRange.js

--- core/prepareDateRange.js
+++ core/prepareDateRange.js
@@ -15,29 +15,22 @@ var setDateRange = function(from, to) {
   };
   util.setConfig(config);
 }
-
 module.exports = function(done) {
   scan((err, ranges) => {
-
     if(_.size(ranges) === 0)
       util.die('No history found for this market', true);
-
     if(_.size(ranges) === 1) {
       var r = _.first(ranges);
       log.info('Gekko was able to find a single daterange in the locally stored history:');
       log.info('\t', 'from:', moment.unix(r.from).utc().format('YYYY-MM-DD HH:mm:ss'));
-      log.info('\t', 'to:', moment.unix(r.to).utc().format('YYYY-MM-DD HH:mm:ss'));
-
-      
+      log.info('\t', 'to:', moment.unix(r.to).utc().format('YYYY-MM-DD HH:mm:ss'));  
       setDateRange(r.from, r.to);
       return done();
     }
-
     log.info(
       'Gekko detected multiple dateranges in the locally stored history.',
       'Please pick the daterange you are interested in testing:'
     );
-
     _.each(ranges, (range, i) => {
       log.info('\t\t', `OPTION ${i + 1}:`);
       log.info('\t', 'from:', moment.unix(range.from).utc().format('YYYY-MM-DD HH:mm:ss'));
@@ -45,20 +38,16 @@ module.exports = function(done) {
     });
 
     prompt.get({name: 'option'}, (err, result) => {
-
       var option = parseInt(result.option);
       if(option === NaN)
         util.die('Not an option..', true);
-
       var range = ranges[option - 1];
 
       if(!range)
         util.die('Not an option..', true);
-
       setDateRange(range.from, range.to);
       return done();
     });
-
   });
 }
 /*
