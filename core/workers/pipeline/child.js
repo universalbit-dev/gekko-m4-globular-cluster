b5aa1afab9313805a7d58139b7e024cff5678060
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-13 02:34:32 +0100

    Update child.js

--- core/workers/pipeline/child.js
+++ core/workers/pipeline/child.js
@@ -1,25 +1,3 @@
-/*
-
-  Gekko is a Bitcoin trading bot for popular Bitcoin exchanges written 
-  in node, it features multiple trading methods using technical analysis.
-
-  If you are interested in how Gekko works, read more about Gekko's 
-  architecture here:
-
-  https://github.com/askmike/gekko/blob/stable/docs/internals/architecture.md
-
-  Disclaimer:
-
-  USE AT YOUR OWN RISK!
-
-  The author of this project is NOT responsible for any damage or loss caused 
-  by this software. There can be bugs and the bot may not perform as expected 
-  or specified. Please consider testing it first with paper trading and/or 
-  backtesting on historical data. Also look at the code to see what how 
-  it is working.
-
-*/
-
 var start = (mode, config) => {
   var util = require(__dirname + '/../../util');
 
