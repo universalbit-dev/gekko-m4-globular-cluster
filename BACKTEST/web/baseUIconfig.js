54152e98d655122f3a8c3556b94c5c615fbb580d
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-13 15:44:55 +0100

    Add files via upload

--- BACKTEST/web/baseUIconfig.js
+++ BACKTEST/web/baseUIconfig.js
@@ -1,26 +1,14 @@
-// Note: this file gets copied around, make sure you edit
-// the UIconfig located at `gekko/web/vue/dist/UIconfig.js`.
-
-// This config is used by both the frontend as well as the web server.
-// see https://gekko.wizb.it/docs/installation/installing_gekko_on_a_server.html#Configuring-Gekko
-
-const CONFIG = {
-  headless: false,
-  api: {
-    host: '127.0.0.1',
-    port: 3000,
-    timeout: 120000 // 2 minutes
-  },
-  ui: {
-    ssl: false,
-    host: 'localhost',
-    port: 3000,
-    path: '/'
-  },
-  adapter: 'sqlite'
-}
-
-if(typeof window === 'undefined')
-  module.exports = CONFIG;
+const CONFIG={headless:true,api:{host:'localhost',port:3000,timeout:60000},ui:{ssl:false,host:'localhost',port:3000,path:'/'}}
+if(typeof window==='undefined')
+module.exports=CONFIG;
 else
-  window.CONFIG = CONFIG;
+window.CONFIG=CONFIG;
+/*
+
+The MIT License (MIT)
+Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
