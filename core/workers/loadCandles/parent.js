253dc9f7026983b7a067e36047f0368b8304785a
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-13 02:49:21 +0100

    Update parent.js

--- core/workers/loadCandles/parent.js
+++ core/workers/loadCandles/parent.js
@@ -1,39 +1,8 @@
-// example usage:
 
-// let config = {
-//   watch: {
-//     exchange: 'poloniex',
-//     currency: 'USDT',
-//     asset: 'BTC'
-//   },
-//   daterange: {
-//     from: '2016-05-22 11:22',
-//     to: '2016-06-03 19:56'
-//   },
-//   adapter: 'sqlite',
-//   sqlite: {
-//     path: 'plugins/sqlite',
-
-//     dataDirectory: 'history',
-//     version: 0.1,
-
-//     dependencies: [{
-//       module: 'sqlite3',
-//       version: '3.1.4'
-//     }]
-//   },
-//   candleSize: 100
-// }
-
-// module.exports(config, function(err, data) {
-//   console.log('FINAL CALLBACK');
-//   console.log('err', err);
-//   console.log('data', data.length);
-// })
 
 
 const fork = require('child_process').fork;
-const _ = require('lodash');
+var _ = require('lodash');
 
 module.exports = (config, callback) => {
   var debug = typeof v8debug === 'object';
