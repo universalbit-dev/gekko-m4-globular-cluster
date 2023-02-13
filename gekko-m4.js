fcbc0c8ad52ec3483a484dcec6ebeaef3b637e94
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-13 02:30:48 +0100

    Update gekko-m4.js

--- gekko-m4.js
+++ gekko-m4.js
@@ -3,24 +3,24 @@ pm2.connect(function(err) {
   if (err) {
     console.error(err)
     process.exit(2)
-  }
+}
 
-  pm2.start({
-    script    : 'gekko.js',
-    args      : '-c config.js --ui',
-    name      : 'gekko-m4.js'
-  },
+pm2.start({
+  script    : 'gekko.js',
+  args      : '-c config.js --ui',
+  name      : 'gekko-m4.js'
+},
 
 function(err, apps) {
-if (err) {
-      console.error(err)
-      return pm2.disconnect()
-    }
+  if (err) {
+    console.error(err)
+    return pm2.disconnect()
+}
 
-    pm2.list((err, list) => {
-      console.log(err, list)
-    })
-  })
+pm2.list((err, list) => {
+  console.log(err, list)
+})
+})
 })
 
 /*
