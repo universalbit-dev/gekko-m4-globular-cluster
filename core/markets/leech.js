9183449d6df890cf9b2172be50c4f8bebee41115
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-13 03:39:14 +0100

    Update leech.js

--- core/markets/leech.js
+++ core/markets/leech.js
@@ -2,22 +2,17 @@
 // database (which is expected to be updated regularly, like with a
 // realtime market running in parallel).
 
-const _ = require('lodash');
-const moment = require('moment');
-
-const util = require('../util');
-const dirs = util.dirs();
-const config = util.getConfig();
-
-const exchangeChecker = require(dirs.gekko + 'exchange/exchangeChecker');
-
-const adapter = config[config.adapter];
-const Reader = require(dirs.gekko + adapter.path + '/reader');
-
-const TICKINTERVAL = 20 * 1000; // 20 seconds
-
-const slug = config.watch.exchange.toLowerCase();
-const exchange = exchangeChecker.getExchangeCapabilities(slug);
+var _ = require('lodash');
+var moment = require('moment');
+var util = require('../util');
+var dirs = util.dirs();
+var config = util.getConfig();
+var exchangeChecker = require(dirs.gekko + 'exchange/exchangeChecker');
+var adapter = config[config.adapter];
+var Reader = require(dirs.gekko + adapter.path + '/reader');
+var TICKINTERVAL = 20 * 1000; // 20 seconds
+var slug = config.watch.exchange.toLowerCase();
+var exchange = exchangeChecker.getExchangeCapabilities(slug);
 
 if(!exchange)
   util.die(`Unsupported exchange: ${slug}`)
@@ -89,3 +84,10 @@ Market.prototype.processCandles = function(err, candles) {
 }
 
 module.exports = Market;
+/*
+The MIT License (MIT)
+Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
