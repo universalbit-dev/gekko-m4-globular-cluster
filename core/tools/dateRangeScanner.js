4405381b883a0d5f7ad0ae4268aca1ab1ffcd7ff
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-13 03:26:16 +0100

    Update dateRangeScanner.js

--- core/tools/dateRangeScanner.js
+++ core/tools/dateRangeScanner.js
@@ -1,18 +1,14 @@
 var BATCH_SIZE = 60; // minutes
 var MISSING_CANDLES_ALLOWED = 3; // minutes, per batch
-
 var _ = require('lodash');
 var moment = require('moment');
 var async = require('async');
-
 var util = require('../util');
 var config = util.getConfig();
 var dirs = util.dirs();
 var log = require(dirs.core + 'log');
-
 var adapter = config[config.adapter];
 var Reader = require(dirs.gekko + adapter.path + '/reader');
-
 var reader = new Reader();
 
 // todo: rewrite with generators or async/await..
@@ -61,7 +57,6 @@ var scan = function(done) {
       }
 
       var batches = [];
-
       // loop through all candles we have
       // in batches and track whether they
       // are complete
@@ -136,3 +131,10 @@ var scan = function(done) {
 }
 
 module.exports = scan;
+/*
+The MIT License (MIT)
+Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
