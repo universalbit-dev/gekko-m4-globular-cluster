75c2461a3c1f065883ce1a1ea158e60922410b3c
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-13 04:26:52 +0100

    Update emitter.js

--- core/emitter.js
+++ core/emitter.js
@@ -1,37 +1,31 @@
-// Gekko uses a custom event emitter within the GekkoStream (the plugins) to guarantee
-// the correct order of events that are triggered by eachother. Turns sync events from
-// LIFO into a FIFO stack based model.
-//
-// More details here: https://forum.gekko.wizb.it/thread-56579.html
+/*
+Gekko uses a custom event emitter within the GekkoStream (the plugins) to guarantee
+ the correct order of events that are triggered by eachother. Turns sync events from
+LIFO into a FIFO stack based model(https://forum.gekko.wizb.it/thread-56579.html)
+*/
 
 var util = require('util');
 var events = require('events');
 var NativeEventEmitter = events.EventEmitter;
-
-const GekkoEventEmitter = function() {
+var GekkoEventEmitter = function() {
   NativeEventEmitter.call(this);
   this.defferedEvents = [];
 }
-
 util.inherits(GekkoEventEmitter, NativeEventEmitter);
 
-// push to stack
 GekkoEventEmitter.prototype.deferredEmit = function(name, payload) {
   this.defferedEvents.push({name, payload});
 }
-
-// resolve FIFO
 GekkoEventEmitter.prototype.broadcastDeferredEmit = function() {
   if(this.defferedEvents.length === 0)
     return false;
-
-  const event = this.defferedEvents.shift();
-
+  var event = this.defferedEvents.shift();
   this.emit(event.name, event.payload);
   return true;
 }
 
 module.exports = GekkoEventEmitter;
+
 /*
 The MIT License (MIT)
 Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
