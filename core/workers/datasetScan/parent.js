dc4c1f6e4806ee3e4f1c05a81cb5fa432e4e1f15
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-13 02:43:42 +0100

    Update parent.js

--- core/workers/datasetScan/parent.js
+++ core/workers/datasetScan/parent.js
@@ -2,18 +2,16 @@ var _ = require('lodash');
 var moment = require('moment');
 var async = require('async');
 var os = require('os');
-
 var util = require('../../util');
 var dirs = util.dirs();
-
 var dateRangeScan = require('../dateRangeScan/parent');
 
 module.exports = function(config, done) {
 
   util.setConfig(config);
 
-  var adapter = config[config.adapter];
-  var scan = require(dirs.gekko + adapter.path + '/scanner');
+var adapter = config[config.adapter];
+var scan = require(dirs.gekko + adapter.path + '/scanner');
 
   scan((err, markets) => {
 
@@ -52,3 +50,10 @@ module.exports = function(config, done) {
     })
   });
 }
+/*
+The MIT License (MIT)
+Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
