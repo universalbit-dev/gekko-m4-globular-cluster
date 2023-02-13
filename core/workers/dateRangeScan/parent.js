c80200861b23ae0eaa65967927e9a9760c6800a7
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-13 03:09:10 +0100

    Update parent.js

--- core/workers/dateRangeScan/parent.js
+++ core/workers/dateRangeScan/parent.js
@@ -8,9 +8,7 @@ module.exports = function(config, done) {
   }
 
   task = new ForkTask(fork(__dirname + '/child'));
-
   task.send('start', config);
-
   task.once('ranges', ranges => {
     return done(false, ranges);
   });
