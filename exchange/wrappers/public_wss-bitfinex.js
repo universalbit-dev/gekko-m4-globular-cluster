bd72c6523bc06a2706f1078fd53d8e85cbdfc67e
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-19 02:07:20 +0100

    Create public_wss-bitfinex.js

---
+++ exchange/wrappers/public_wss-bitfinex.js
@@ -0,0 +1,12 @@
+const ws = require('ws')
+const w = new ws('wss://api-pub.bitfinex.com/ws/2')
+
+w.on('message', (msg) => console.log(msg))
+
+let msg = JSON.stringify({ 
+  event: 'subscribe', 
+  channel: 'trades', 
+  symbol: 'tLTCBTC' 
+})
+
+w.on('open', () => w.send(msg))
