058bb012b79d9a44fb41328c63c702ee4c13cc22
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-20 11:38:41 +0100

    Update plugins.js

--- BACKTEST/plugins.js
+++ BACKTEST/plugins.js
@@ -4,7 +4,7 @@ var plugins = [
     description: 'Store candles in a database',
     slug: 'candleWriter',
     async: true,
-    modes: ['realtime', 'importer'],
+    modes: ['importer'],
     path: config => config.adapter + '/writer',
     version: 0.1,
   },
@@ -13,46 +13,17 @@ var plugins = [
     description: 'Calculate trading advice',
     slug: 'tradingAdvisor',
     async: true,
-    modes: ['realtime', 'backtest'],
+    modes: ['backtest'],
     emits: true,
     path: config => 'tradingAdvisor/tradingAdvisor.js',
   },
 
   {
-    name: 'Pushover',
-    description: 'Sends pushover.',
-    slug: 'pushover',
-    async: false,
-    modes: ['realtime'],
-    dependencies: [{
-      module: 'pushover-notifications',
-      version: '0.2.3'
-    }]
-  },
-
-  {
-    name: 'Advice logger',
-    description: '',
-    slug: 'adviceLogger',
-    async: false,
-    silent: true,
-    modes: ['realtime']
-  },
-  {
-    name: 'Trader',
-    description: 'Follows the advice and create real orders.',
-    slug: 'trader',
-    async: true,
-    modes: ['realtime'],
-    emits: true,
-    path: config => 'trader/trader.js',
-  },
-  {
     name: 'Paper Trader',
     description: 'Paper trader that simulates fake trades.',
     slug: 'paperTrader',
     async: false,
-    modes: ['realtime', 'backtest'],
+    modes: ['backtest'],
     emits: true,
     path: config => 'paperTrader/paperTrader.js',
   },
@@ -61,17 +32,9 @@ var plugins = [
     description: 'Analyzes performances of trades',
     slug: 'performanceAnalyzer',
     async: false,
-    modes: ['realtime', 'backtest'],
+    modes: ['backtest'],
     emits: true,
     path: config => 'performanceAnalyzer/performanceAnalyzer.js',
-  },
-
-  {
-    name: 'Candle Uploader',
-    description: 'Upload candles to an extneral server',
-    slug: 'candleUploader',
-    async: true,
-    modes: ['realtime']
   },
 
   {
@@ -79,7 +42,7 @@ var plugins = [
     description: 'Logs all gekko events.',
     slug: 'eventLogger',
     async: false,
-    modes: ['realtime', 'backtest'],
+    modes: ['backtest'],
     greedy: true
   },
   {
@@ -89,32 +52,9 @@ var plugins = [
     async: false,
     modes: ['backtest']
   },
-  {
-    name: 'Child to parent',
-    description: 'Relays events from the child to the parent process',
-    slug: 'childToParent',
-    async: false,
-    modes: ['realtime'],
-    greedy: true
-  },
-  {
-    name: 'Candle Uploader',
-    description: 'Upload realtime market candles to an external server',
-    slug: 'candleUploader',
-    async: true,
-    modes: ['realtime'],
-    dependencies: [{
-      module: 'axios',
-      version: '0.18.0'
-    }]
-  },
-  {
-    name: 'Blotter',
-    description: 'Writes all buy/sell trades to a blotter CSV file',
-    slug: 'blotter',
-    async: false,
-    modes: ['realtime'],
-  },
+  
+  
+  
 ];
 
 module.exports = plugins;
