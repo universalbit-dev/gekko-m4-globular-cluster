1ebf46ade1b820dbf026ff4093232eb8f0ed09cc
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-20 12:05:38 +0100

    Update backtest.js

--- BACKTEST/web/routes/backtest.js
+++ BACKTEST/web/routes/backtest.js
@@ -1,5 +1,9 @@
-// simple POST request that returns the backtest result
+/*
 
+
+*/
+
+// simple POST request that returns the backtest result
 const _ = require('lodash');
 const promisify = require('tiny-promisify');
 const pipelineRunner = promisify(require('../../core/workers/pipeline/parent'));
@@ -18,14 +22,19 @@ const pipelineRunner = promisify(require('../../core/workers/pipeline/parent'));
 // }
 module.exports = function *() {
   var mode = 'backtest';
-
   var config = {};
-
   var base = require('./baseConfig');
-
   var req = this.request.body;
 
   _.merge(config, base, req);
 
   this.body = yield pipelineRunner(mode, config);
 }
+
+/*
+The MIT License (MIT)
+Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
