76b2f8be4efba8582e1f514e7bbb0a3adc9f804a
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-20 11:57:46 +0100

    Update gekko.js

--- BACKTEST/gekko.js
+++ BACKTEST/gekko.js
@@ -4,15 +4,7 @@
   If you are interested in how Gekko works, read more about Gekko's 
   architecture here:
   https://gekko.wizb.it/docs/internals/architecture.html
-  
-  Disclaimer:
-  USE AT YOUR OWN RISK!
 
-  The author of this project is NOT responsible for any damage or loss caused 
-  by this software. There can be bugs and the bot may not perform as expected 
-  or specified. Please consider testing it first with paper trading and/or 
-  backtesting on historical data. Also look at the code to see what how 
-  it is working.
 */
 
 console.log('##########################################');
