8f4a826bf4cb91c27cdbe37a776f5681d3adebcd
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-21 03:57:32 +0100

    Add files via upload

---
+++ BACKTEST/web/routes/exchanges.js
@@ -0,0 +1,50 @@
+/*
+
+
+*/
+
+const _ = require('lodash');
+const fs = require('co-fs');
+const gekkoRoot = __dirname + '/../../';
+var util = require(__dirname + '/../../core/util');
+var config = {};
+
+config.debug = false;
+config.silent = false;
+
+util.setConfig(config);
+
+module.exports = function *() {
+  const exchangesDir = yield fs.readdir(gekkoRoot + 'exchange/wrappers/');
+  const exchanges = exchangesDir
+    .filter(f => _.last(f, 3).join('') === '.js')
+    .map(f => f.slice(0, -3));
+
+  let allCapabilities = [];
+
+  exchanges.forEach(function (exchange) {
+    let Trader = null;
+
+    try {
+      Trader = require(gekkoRoot + 'exchange/wrappers/' + exchange);
+    } catch (e) {
+      return;
+    }
+
+    if (!Trader || !Trader.getCapabilities) {
+      return;
+    }
+
+    allCapabilities.push(Trader.getCapabilities());
+  });
+
+  this.body = allCapabilities;
+}
+
+/*
+The MIT License (MIT)
+Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
