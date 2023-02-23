10156605c622655502aef7b597b2b34bbe441901
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-23 05:32:46 +0100

    Update handle.js

--- plugins/sqlite/handle.js
+++ plugins/sqlite/handle.js
@@ -8,7 +8,6 @@ var fs = require('fs');
 var util = require('../../core/util.js');
 var config = util.getConfig();
 var dirs = util.dirs();
-
 var adapter = config.sqlite;
 
 // verify the correct dependencies are installed
@@ -17,20 +16,14 @@ var pluginMock = {};
 
 var cannotLoad = pluginHelper.cannotLoad(pluginMock);
 if (cannotLoad) util.die(cannotLoad);
-
-// should be good now
 if (config.debug) var sqlite3 = require('sqlite3');
 else var sqlite3 = require('sqlite3');
 
 var plugins = require(util.dirs().gekko + 'plugins');
-
 var version = adapter.version;
-
 var dbName = config.watch.exchange.toLowerCase() + '_' + version + '.db';
 var dir = dirs.gekko + adapter.dataDirectory;
-
 var fullPath = [dir, dbName].join('/');
-
 var mode = util.gekkoMode();
 if (mode === 'realtime' || mode === 'importer') {
   if (!fs.existsSync(dir)) fs.mkdirSync(dir);
