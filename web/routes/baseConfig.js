e992d6d39ba7c13184e31f8ed012d165fbddfb62
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-23 05:58:36 +0100

    Update baseConfig.js

--- web/routes/baseConfig.js
+++ web/routes/baseConfig.js
@@ -51,30 +51,6 @@ config.sqlite = {
   }]
 }
 
-  // Postgres adapter example config (please note: requires postgres >= 9.5):
-config.postgresql = {
-  path: 'plugins/postgresql',
-  version: 0.1,
-  connectionString: 'postgres://gekko:gekko@192.168.178.52:5432', // if default port
-  database: "gekko", // if set, we'll put all tables into a single database.
-  schema: 'public',
-  dependencies: [{
-    module: 'pg',
-    version: '7.4.3'
-  }]
-}
-
-// Mongodb adapter, requires mongodb >= 3.3 (no version earlier tested)
-config.mongodb = {
-  path: 'plugins/mongodb',
-  version: 0.1,
-  connectionString: 'mongodb://mongodb/gekko', // connection to mongodb server
-  dependencies: [{
-    module: 'mongojs',
-    version: '2.4.0'
-  }]
-}
-
 config.adviceWriter = {
   enabled: false,
   muteSoft: true,
@@ -86,7 +62,6 @@ config.adviceWriter = {
 
 // Note that these settings are only used in backtesting mode, see here:
 // @link: https://github.com/askmike/gekko/blob/stable/docs/Backtesting.md
-
 config.backtest = {
   daterange: 'scan',
   batchSize: 50
@@ -95,7 +70,7 @@ config.backtest = {
 config.importer = {
   daterange: {
     // NOTE: these dates are in UTC
-    from: "2016-06-01 12:00:00"
+    from: "2017-06-01 12:00:00"
   }
 }
 
