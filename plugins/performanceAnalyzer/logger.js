caf7b93e7e4764407a56ca3c702c734a54bc12ea
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-02-23 05:38:00 +0100

    Update logger.js

--- plugins/performanceAnalyzer/logger.js
+++ plugins/performanceAnalyzer/logger.js
@@ -1,14 +1,15 @@
-// log trade performance results
+/*
 
+
+*/
+// log trade performance results
 const _ = require('lodash');
 const moment = require('moment');
 const humanizeDuration = require('humanize-duration');
-
 const util = require('../../core/util.js');
 const dirs = util.dirs();
 const mode = util.gekkoMode();
 const log = require(dirs.core + 'log');
-
 const Logger = function(watchConfig) {
   this.currency = watchConfig.currency;
   this.asset = watchConfig.asset;
@@ -19,13 +20,11 @@ const Logger = function(watchConfig) {
 Logger.prototype.round = function(amount) {
   return amount.toFixed(8);
 }
-
 // used for:
 // - realtime logging (per advice)
 // - backtest logging (on finalize)
 Logger.prototype.logReport = function(trade, report) {
   // ignore the trade
-
   var start = this.round(report.startBalance);
   var current = this.round(report.balance);
 
@@ -128,7 +127,11 @@ if(mode === 'backtest') {
 
 }
 
-
-
-
 module.exports = Logger;
+/*
+The MIT License (MIT)
+Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
