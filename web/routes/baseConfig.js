1205a1d36f962b91f5d333bf92ea261e6209895d
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-03-10 15:15:28 +0100

    Add files via upload

---
+++ web/routes/baseConfig.js
@@ -0,0 +1,58 @@
+var UIconfig = require('../vue/statics/UiConfig');
+var config = {};
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                          GENERAL SETTINGS
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+config.silent = false;
+config.debug = true;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                       CONFIGURING TRADING ADVICE
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+config.tradingAdvisor = {
+}
+
+config.candleWriter = {
+  enabled: true
+}
+
+config.backtestResultExporter = {
+  enabled: true,
+  writeToDisk: false,
+  data: {
+    stratUpdates: true,
+    roundtrips: true,
+    stratCandles: true,
+    trades: true
+  }
+}
+
+config.childToParent = {
+  enabled: false,
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                       CONFIGURING ADAPTER
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// configurable in the UIconfig
+config.adapter = UIconfig.adapter;
+config.sqlite = {path: 'plugins/sqlite',
+version: 0.1,dataDirectory: 'history',
+journalMode: require('../isWindows.js') ? 'PERSIST' : 'WAL',
+dependencies: [{module: 'sqlite3'}]};
+
+config.adviceWriter = {enabled: true,muteSoft: true,};
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                       CONFIGURING BACKTESTING
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Note that these settings are only used in backtesting mode, see here:
+// @link: https://github.com/askmike/gekko/blob/stable/docs/Backtesting.md
+
+config.backtest = {daterange: 'scan',batchSize: 50};
+
+module.exports = config;
---
+++ web/routes/exchanges.js
@@ -0,0 +1,38 @@
+const _ = require('lodash');
+const fs = require('co-fs');
+
+const gekkoRoot = __dirname + '/../../';
+var util = require(__dirname + '/../../core/util');
+
+var config = {};
+
+config.debug = false;
+config.silent = false;
+
+util.setConfig(config);
+
+module.exports = function *() {
+  const exchangesDir = yield fs.readdir(gekkoRoot + 'exchange/wrappers/');
+  exchangesDir.join('') === '.js';
+  const exchanges = exchangesDir.filter(f => _.last(f, 3)).map(f => f.slice(0, -3));
+
+  let allCapabilities = [];
+
+  exchanges.forEach(function (exchange) {
+    let Trader = null;
+
+    try {
+      Trader = require(gekkoRoot + 'exchange/wrappers/' + exchange);
+    } catch (e) {
+      return;
+    }
+
+    if (!Trader || !Trader.getCapabilities) {
+      return;
+    }
+
+    allCapabilities.push(Trader.getCapabilities());
+  });
+
+  this.body = allCapabilities;
+}
