c22a2c2ca9e928799265a3a3dc14067d733be270
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-03-10 15:31:14 +0100

    Add files via upload

---
+++ exchange/wrappers/WebSocketsPolyfill.js
@@ -0,0 +1,13 @@
+/*
+
+
+*/
+const WebSocketLib = require('ws');
+
+function createWSConnection(props) {
+  return typeof WebSocket !== 'undefined' ? new WebSocket(props) : new WebSocketLib(props);
+}
+
+module.exports = {
+  createWSConnection,
+};
