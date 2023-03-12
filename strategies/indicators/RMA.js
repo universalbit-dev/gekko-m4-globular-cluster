43301158eee043890df2deb6a359434f0ab70f51
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-03-12 11:37:46 +0100

    Delete strategies/indicators directory

--- strategies/indicators/RMA.js
+++
@@ -1,5 +0,0 @@
-var Indicator=function(weight){this.input='price';this.weight=weight;this.result=false;this.age=0;}
-Indicator.prototype.update=function(price){if(this.result===false)
-this.result=price;this.age++;this.calculate(price);return this.result;}
-Indicator.prototype.calculate=function(price){var k=1/this.weight;var y=this.result;this.result=price*k+y*(1-k);}
-module.exports=Indicator;
