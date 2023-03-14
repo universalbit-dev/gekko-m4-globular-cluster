7ebe9fa2492728b3ae237af4c9b4df8ec4fa0e0f
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-03-14 19:31:44 +0100

    Update baseConfig.js

--- web/routes/baseConfig.js
+++ web/routes/baseConfig.js
@@ -1,49 +1,35 @@
-const path = require('path');
 var UIconfig = require('../vue/statics/UiConfig');
 var config = {};
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                          GENERAL SETTINGS
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-config.silent = false;
-config.debug = true;
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       CONFIGURING TRADING ADVICE
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+//General Settings
+config.debug =true;
+//Watching
+config.watch = {};
+//Trading Advisor
 config.tradingAdvisor = {};
-
-config.candleWriter = {enabled: true};
-
-config.backtestResultExporter = {
-  enabled: true,
-  writeToDisk: false,
-  data: {
-    stratUpdates: true,
-    roundtrips: true,
-    stratCandles: true,
-    trades: true
-  }
-};
-
-config.childToParent = {enabled: false,};
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       CONFIGURING ADAPTER
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// configurable in the UIconfig
-config.adapter = UIconfig.adapter;
-config.sqlite = {
-  path: 'plugins/sqlite',
-  version: 0.1,
-  dataDirectory: 'history',
-  journalMode: require('../isWindows.js') ? 'PERSIST' : 'WAL',
-  dependencies: [{module: 'sqlite3'}]
-};
-
+//Adapter
+config.adapter='sqlite';
+//Trader
+config.trader ={};
+config.candleWriter={};
+config.adviceLogger={};
+config.adviceWriter={};
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //                       CONFIGURING BACKTESTING
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.backtest = {daterange: 'scan',batchSize: 50};
+config.backtestResultExporter = {};
+config.data={};
+config.importer = {};
+config.backtest = {};
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                       CONFIGURING PAPERTRADER
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+config.paperTrader = {};
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                       CONFIGURING PERFORMANCE ANALYZER
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+config.performanceAnalyzer = {};
+//Importer
+config.sqlite = {};
+//Child to Parent
+config.childToParent = {};
 module.exports = config;
