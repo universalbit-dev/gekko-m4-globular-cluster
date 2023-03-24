222d6d82a90270d2b1ad5072a50be3168e1dc42f
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-03-24 03:47:21 +0100

    Update baseConfig.js

--- web/routes/baseConfig.js
+++ web/routes/baseConfig.js
@@ -10,32 +10,24 @@ config.debug = true;
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 config.tradingAdvisor = {}
 config.candleWriter = {enabled: true}
-
 config.backtestResultExporter = {
 enabled: true,
 writeToDisk: false,
 data: {stratUpdates: true,roundtrips: true,stratCandles: true,trades: true}
 }
-
 config.childToParent = {enabled: true}
-
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //                       CONFIGURING ADAPTER
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 config.adapter = UIconfig.adapter;
-
 config.sqlite = {
 path: 'plugins/sqlite',version: 0.1,dataDirectory: 'history',
 dependencies: [{module: 'sqlite3'}]
 }
-
 config.adviceWriter = {enabled: false,muteSoft: true}
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //                       CONFIGURING BACKTESTING
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 config.backtest = {daterange: 'scan',batchSize: 50}
-config.importer = {daterange: {from: "2020-03-08 00:00:00"}}
-
+config.importer = {daterange: {from: "2020-03-08 00:00:00",to:"2023-03-08 00:00:00"}}
 module.exports = config;
