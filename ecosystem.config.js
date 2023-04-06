19f6dd8c4f4a412c928bc5a0787bdff1f4e85ab6
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-04-06 20:05:50 +0200

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apps : [
+{name:"exchange-simulator-strategy01",script:"gekko.js",args: "-c exchange-simulator-strategy01.js"},
+{name:"exchange-simulator-strategy02",script:"gekko.js",args: "-c exchange-simulator-strategy02.js"},
+{name:"exchange-simulator-strategy03",script:"gekko.js",args: "-c exchange-simulator-strategy03.js"}
+]
+}
