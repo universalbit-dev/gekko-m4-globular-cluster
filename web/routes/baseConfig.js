02e107b64c0f9175528642f81ab96dbc791c3abb
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-04-06 20:01:18 +0200

    Update baseConfig.js

--- web/routes/baseConfig.js
+++ web/routes/baseConfig.js
@@ -1,33 +1,28 @@
-var UIconfig = require('../vue/statics/UiConfig');
-var config = {};
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                          GENERAL SETTINGS
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.silent = false;
-config.debug = true;
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       CONFIGURING TRADING ADVICE
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.tradingAdvisor = {}
-config.candleWriter = {enabled: true}
-config.backtestResultExporter = {
-enabled: true,
-writeToDisk: false,
-data: {stratUpdates: true,roundtrips: true,stratCandles: true,trades: true}
-}
-config.childToParent = {enabled: true}
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       CONFIGURING ADAPTER
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.adapter = UIconfig.adapter;
-config.sqlite = {
-path: 'plugins/sqlite',version: 0.1,dataDirectory: 'history',
-dependencies: [{module: 'sqlite3'}]
-}
-config.adviceWriter = {enabled: false,muteSoft: true}
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       CONFIGURING BACKTESTING
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.backtest = {daterange: 'scan',batchSize: 50}
-config.importer = {daterange: {from: "2020-03-08 00:00:00",to:"2023-03-08 00:00:00"}}
-module.exports = config;
+/*
+The MIT License (MIT)
+Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+var ui_config = require('../../web/vue/statics/UiConfig');
+var base_config={};
+//General Settings
+base_config.debug =true;
+base_config.watch= {};
+base_config.tradingAdvisor = {enabled:true};
+base_config.adapter=ui_config.adapter;
+base_config.trader={enabled:true};
+base_config.candleWriter={enabled:true};
+base_config.adviceLogger={enabled:true};
+base_config.backtestResultExporter={enabled:true};
+base_config.backtest = {enabled:true};
+base_config.paperTrader = {enabled: true};
+base_config.performanceAnalyzer = {enabled: true};
+base_config.importer={enabled:true};
+base_config.sqlite = {enabled:true};
+base_config.childToParent = {enabled: true};
+base_config['I understand that Gekko only automates MY OWN trading strategies']=true;
+
+module.exports = base_config;
