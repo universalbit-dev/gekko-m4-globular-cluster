3418819464044b5f36567b15daabb733905380d0
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-04-11 23:13:21 +0200

    Update parent.js

--- core/workers/pipeline/parent.js
+++ core/workers/pipeline/parent.js
@@ -1,5 +1,9 @@
-var fork = require('child_process').fork;
+var _ = require('../../lodash-core');
+var util = require('../../util');
+var config = util.getConfig();
+var dirs = util.dirs();
 
+var fork = require('child_process').fork;
 module.exports = (mode, config, callback) => {
   var debug = typeof v8debug === 'object';
   if (debug) {
