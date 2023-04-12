6f8c3e3952872366d6d0e8ea4f1f3b92bc2b95f5
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-04-12 03:20:23 +0200

    Update apiKeys.js

--- web/routes/apiKeys.js
+++ web/routes/apiKeys.js
@@ -1,26 +1,24 @@
 const cache = require('../state/cache');
 const manager = cache.get('apiKeyManager');
-const path = require('path');
+var _ = require('../../core/lodash-core');
+var util = require('../../core/util');
+var dirs = util.dirs();
+var config = util.getConfig();
+
+var base_config= require('./baseConfig');
 module.exports = {
-  get: function *() {
-    this.body = manager.get();
+get: function *()
+{
+	this.body = manager.get();
+},
+add: function *() {
+	const content = this.request.body;
+	manager.add(content.exchange, content.values);
+	this.body = {status: 'ok'};
   },
-  add: function *() {
-    const content = this.request.body;
-
-    manager.add(content.exchange, content.values);
-
-    this.body = {
-      status: 'ok'
-    };
-  },
-  remove: function *() {
-    const exchange = this.request.body.exchange;
-
-    manager.remove(exchange);
-
-    this.body = {
-      status: 'ok'
-    };
+remove: function *() {
+    	const exchange = this.request.body.exchange;
+    	manager.remove(exchange);
+    	this.body = {status: 'ok'};
   }
 }
