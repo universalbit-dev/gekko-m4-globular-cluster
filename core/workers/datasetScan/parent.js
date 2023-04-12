0e39bff350258ae903d00fba21eb506720c03bfd
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-04-12 02:33:19 +0200

    Update parent.js

--- core/workers/datasetScan/parent.js
+++ core/workers/datasetScan/parent.js
@@ -9,7 +9,7 @@ var os = require('os');
 var dateRangeScan = require('../dateRangeScan/parent');
 module.exports = function(config, done) {
   util.setConfig(config);
-  var adapter = config[config.sqlite];
+  var adapter = config[config.adapter];
   var scan = require(dirs.gekko + adapter.path + '/scanner');
   scan((err, markets) => {
     if(err)
