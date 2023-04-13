8ed32a4a147015d1b502e4e418006094aedac918
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-04-13 19:37:13 +0200

    Add files via upload

---
+++ web/vue/statics/UiConfig.js
@@ -0,0 +1,28 @@
+const CONFIG = {
+  headless: false,
+  api: {
+    host: "192.168.1.146",
+    port: 3007,
+    path:"/",
+    timeout: 120000
+  },
+  ui: {
+    ssl: false,
+    host: "192.168.1.146",
+    port: 3007,
+    path: "/",
+    timeout:120000,
+    cert:"./ssl/cert.pem",
+    key:"./ssl/key.pem"
+  },
+  adapter: "sqlite",
+
+  userChartConfig: {
+    //patterns:['hasInvertedHammer']
+    indicators: []
+    //overlays: []
+  }
+};
+
+if (typeof window === "undefined") module.exports = CONFIG;
+else window.CONFIG = CONFIG;
