0b56e0e938d94d9be7493970b8ba188b4c2013b4
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-04-21 00:15:12 +0200

    Delete ecosystem.exchange-simulator.config.js

--- ecosystem.exchange-simulator.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  apps : [
-{name:"exchange-strategy01",script:"node gekko -c exchange-strategy01.js"},
-{name:"exchange-strategy02",script:"node gekko -c exchange-strategy02.js"},
-{name:"exchange-strategy03",script:"node gekko -c exchange-strategy03.js"}
-]
-}
