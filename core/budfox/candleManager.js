cea5f1b0941d4508a0511df1240fbf341db12eab
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-06-07 12:44:46 +0200

    Update candleManager.js

--- core/budfox/candleManager.js
+++ core/budfox/candleManager.js
@@ -1,23 +1,19 @@
 // The candleManager consumes trades and emits:
 // - `candles`: array of minutly candles.
 // - `candle`: the most recent candle after a fetch Gekko.
-
 var _ = require('lodash');
 var moment = require('moment');
 var fs = require('fs');
-
-var util = require(__dirname + '/../util');
+var util = require('../../core/util');
 var dirs = util.dirs();
-var config = util.getConfig();
-var log = require(dirs.core + 'log');
+var config = require('../../core/util.js').getConfig();
+var log = require('../../core/log');
 
 var CandleCreator = require(dirs.budfox + 'candleCreator');
-
 var Manager = function() {
   _.bindAll(this);
 
   this.candleCreator = new CandleCreator;
-
   this.candleCreator
     .on('candles', this.relayCandles);
 };
