76829fafdf99a5832c464441a6b90e4b8c47bfba
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-06-07 13:11:40 +0200

    Update backtest.js

--- core/markets/backtest.js
+++ core/markets/backtest.js
@@ -9,8 +9,8 @@ var adapter = config[config.adapter];
 var Reader = require(dirs.gekko + adapter.path + '/reader');
 var daterange = config.backtest.daterange;
 
-var to = moment.utc(daterange.to);
-var from = moment.utc(daterange.from);
+var to = moment.utc(config.daterange.to);
+var from = moment.utc(config.daterange.from);
 
 if(to <= from)
   util.die('This daterange does not make sense.')
