2d43df3b5d3033d17c336dea1367057b1397277f
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-07-20 19:05:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -39,8 +39,7 @@ data: {
 
 config.backtest = {daterange: 'scan',batchSize: 50};
 config.daterange= {from: "2020-03-01 00:00:00",to: "2023-07-03 00:00:00"};
-
-config.importer = {daterange: {from: "2020-03-08 00:00:00",to:"2023-03-08 00:00:00"}};
+config.importer = {daterange: {from: "2020-03-01 00:00:00",to:"2023-07-03 00:00:00"}};
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //                       CONFIGURING TRADER
