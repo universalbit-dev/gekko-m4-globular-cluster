9e4d7ea7503f42e0c21744c0aec266ab9b333389
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-07-29 14:58:51 +0200

    Update scanner.js

--- plugins/sqlite/scanner.js
+++ plugins/sqlite/scanner.js
@@ -5,7 +5,7 @@
 let _ = require('../../core./lodash');
 require('lodash-migrate');
 const async = require('async');
-const fs = require('fs-extra');
+let fs = require('fs-extra');
 const util = require('../../core/util.js');
 const config = util.getConfig();
 const dirs = util.dirs();
