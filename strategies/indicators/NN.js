92e1a44ce0538f3d1544cfb201866be010970ab3
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-08-21 21:22:10 +0200

    Add files via upload

--- strategies/indicators/NN.js
+++ strategies/indicators/NN.js
@@ -1,3 +1,4 @@
+//https://cs.stanford.edu/people/karpathy/convnetjs/started.html
 let DEMA = require('./DEMA.js');
 let _ = require('../../core/lodash');
 let util = require('../../core/util');
@@ -8,12 +9,11 @@ let convnetjs = require('../../core/convnet.js');
 let DQNAgent = require('../../core/rl.js');
 let deepqlearn = require('convnet/build/deepqlearn');
 var math = require('mathjs');
+
 var Indicator = function(settings) {
-  this.input = 'candle';
   this.result = false;
   this.lastPrice = 0;
   this.settings = settings;
-
   this.priceBuffer = [];
   this.predictionCount = 0;
   this.batchsize = 8;
@@ -23,18 +23,32 @@ var Indicator = function(settings) {
   this.prevAction = 'wait';
   this.prevPrice = 0;
   this.stoplossCounter = 0;
-
   this.DEMA = new DEMA(5);
-  this.out_depth=4;
 
-    let layers = [
-      {type:'input', out_sx:7, out_sy:8, out_depth: this.out_depth},
-      {type:'fc', num_neurons: this.layer_neurons, activation: this.layer_activation},
-      {type:'regression', num_neurons: 1}
-    ];
+  var layers = [
+    {type:'input', out_sx: 7, out_sy:8, out_depth: 4},
+    {type:'svm', num_classes:20},
+    {type:'conv', num_neurons: 10000,group_size: 2,activation:'sigmoid'},
+    {type:'svm', num_classes:20},
+    {type:'regression', num_neurons: 20}
+  ];
+  var layers2 = [
+    {type:'input', out_sx: 7, out_sy:8, out_depth: 4},
+    {type:'svm', num_classes:20},
+    {type:'fc', num_neurons: 10000,group_size: 2,activation:'relu'},
+    {type:'svm', num_classes:20},
+    {type:'regression', num_neurons: 20}
+  ];
+  var layers3 = [
+    {type:'input', out_sx: 7, out_sy:8, out_depth: 4},
+    {type:'svm', num_classes:20},
+    {type:'conv', num_neurons: 10000,group_size: 2,activation:'sigmoid'},
+    {type:'svm', num_classes:20},
+    {type:'regression', num_neurons: 20}
+  ];
 
     this.nn = new convnetjs.Net();
-    this.nn.makeLayers(layers);
+    this.nn.makeLayers(layers,layers2,layers3);
     if(settings.method == 'sgd')
     {
       this.trainer = new convnetjs.SGDTrainer(this.nn, {
@@ -71,15 +85,12 @@ Indicator.prototype.setNormalizeFactor = function(candle) {
   log.debug('Set normalization factor to',this.scale);
 }
 
-Indicator.prototype.learn = function() {
-  for (let i = 0; i < this.priceBuffer.length - this.out_depth; i++) {
-    let data = this.priceBuffer[i];
-    let current_price = this.priceBuffer[i + 1];
+Indicator.prototype.learn = function () {
+  for (let i = 0; i < this.priceBuffer.length - 1; i++) {
+    let data = [this.priceBuffer[i]];
+    let current_price = [this.priceBuffer[i + 1]];
     let vol = new convnetjs.Vol(data);
     this.trainer.train(vol, current_price);
-    let predicted_values = this.nn.forward(vol);
-    let accuracymatch = predicted_values.w[0] === current_price.first;
-    this.nn.backward(accuracymatch);
     this.predictionCount++;
   }
 }
@@ -96,7 +107,7 @@ Indicator.prototype.predictCandle = function() {
 }
 
 Indicator.prototype.update = function(candle) {
-  this.DEMA.update((candle.high + candle.open + candle.low + candle.vwp)/4);
+  this.DEMA.update((candle.high + candle.close + candle.low + candle.vwp)/4);
   let demaFast = this.DEMA.result;
 
   if (1 === this.scale && 1 < candle.high && 0 === this.predictionCount) this.setNormalizeFactor(candle);
@@ -104,9 +115,7 @@ Indicator.prototype.update = function(candle) {
   this.priceBuffer.push(demaFast / this.scale );
   if (2 > this.priceBuffer.length) return;
 
-   for (i=0;i<3;++i)
-    this.learn();
-
+  for (i=0;i<3;++i)this.learn();
   while (this.settings.price_buffer_len < this.priceBuffer.length) this.priceBuffer.shift();
 }
 
@@ -115,23 +124,23 @@ Indicator.prototype.check = function(candle){
   if(this.predictionCount > this.settings.min_predictions)
   {
     let prediction = this.predictCandle() * this.scale;
-    let currentPrice = candle.open;
+    let currentPrice = candle.close;
     let meanp = math.mean(prediction, currentPrice);
     let meanAlpha = (meanp - currentPrice) / currentPrice * 100;
 
-    let signalSell = candle.open > this.prevPrice;
+    let signalSell = candle.close > this.prevPrice;
 
     let signal = meanp < currentPrice;
     if ('buy' !== this.prevAction && signal === false  && meanAlpha> this.settings.threshold_buy )
     {
-      log.debug("Buy - Predicted variation: ",meanAlpha);
-      return 'long';
+      log.debug('|NN|NO-BUY|',meanAlpha);
+      //return 'long';
     }
     else if
     ('sell' !== this.prevAction && signal === true && meanAlpha < this.settings.threshold_sell && signalSell)
     {
-      log.debug("Sell - Predicted variation: ",meanAlpha);
-      return 'short';
+      log.debug('|NN|NO-SELL|',meanAlpha);
+      //return 'short';
     }
   }
 }
