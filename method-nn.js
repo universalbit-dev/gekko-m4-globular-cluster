0e39b0eb2ed3ad15f4f2b9299d70a70188087ea2
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-09-08 23:10:19 +0200

    Update method-nn.js

--- method-nn.js
+++ method-nn.js
@@ -19,23 +19,27 @@ var config = {};
 //General Settings
 config.debug =true;
 
-config.watch = {exchange: '',currency:'BTC',asset:'LTC',key:'',secret:'',ticker:20};
+config.watch = {exchange: '',currency:'BTC',asset:'LTC',key:'',secret:'',ticker:10};
 //Trading Advisor
-config.tradingAdvisor = {enabled:true,candleSize:1,historySize:60};
-config.tradingAdvisor.method= 'NN';
+config.tradingAdvisor = {enabled:true,candleSize:1,historySize:10};
+config.tradingAdvisor.method= 'NEURALNET';
 
 /*
-|NN|NO-SELL|
-
-|NN|NO-BUY|
+|NO-BUY||NO-SELL|
 */
+
+///https://cs.stanford.edu/people/karpathy/convnetjs/demo/trainers.html
+config.NEURALNET={
+threshold_buy:1.0,threshold_sell:-1.0,method:'sgd',learning_rate:0.0004,momentum:0.9,
+decay:0.001,SMA_long:10,SMA_short:55,stoploss_enabled:false,stoploss_threshold:0.85,
+hodl_threshold:1,price_buffer_len:100,min_predictions:1000,
+};
 
 config.NN= {
 DEMA:5,
 threshold_buy:1.0,
 interval:3,
 threshold_sell:-1.0,
-//https://cs.stanford.edu/people/karpathy/convnetjs/demo/trainers.html
 method:'sgd',
 learning_rate:0.01,
 momentum:0.9,
