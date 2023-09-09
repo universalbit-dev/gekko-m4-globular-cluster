9e3b8525d090c8898fe5000daa9862bf811177c9
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-09-09 17:41:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,71 +1,73 @@
 /*
+The MIT License (MIT)
+Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
+Disclaimer:
+                              USE AT YOUR OWN RISK!
+The author of this project is NOT responsible for any damage or loss caused
+by this software. There can be bugs and the bot may not perform as expected
+or specified. Please consider testing it first with paper trading and/or
+backtesting on historical data. Also look at the code to see what how
+it is working.
 */
+
+//backtest configuration file
+
 var config = {};
+
 //General Settings
 config.debug =true;
-//Watching
-config.watch = {exchange: 'exchange_simulator',currency: 'LTC',asset: 'BTC',tickrate:10};
-//Trading Advisor
-config.tradingAdvisor = {enabled:true,candleSize:15,historySize:1};
+config.watch = {exchange: 'exchange',currency:'BTC',asset:'LTC',tickrate:10};
 
-//Strategy
-config.tradingAdvisor.method= 'Noop';
+//Trading Advisor
+config.tradingAdvisor = {enabled:true,candleSize:1,historySize:10,strat:'strategy'};
 
 //Adapter
 config.adapter='sqlite';
-config.sqlite = {
-  adapter:'sqlite',path: 'plugins/sqlite',version: 0.1,
-  dataDirectory: 'history',dependencies: [{module: 'sqlite3'}]
+//Api
+config.trader={
+  enabled:false,exchange:'exchange',currency:'BTC',asset:'LTC',
+  key:'',secret:''
 };
 
-config.candleWriter={enabled:true,adapter:'sqlite'};
-config.adviceLogger={enabled:false};
+config.candleWriter={enabled:false,adapter:'sqlite'};
+config.adviceLogger={enabled:true};
 config.backtestResultExporter={enabled:false};
-
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //                       CONFIGURING BACKTESTING
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.backtestResultExporter = {
-  enabled: true,
-  writeToDisk: false,
-data: {
-  stratUpdates: true,
-  roundtrips: true,
-  stratCandles: true,
-  trades: true}
-};
-
-config.backtest ={enabled:true,daterange:'scan',batchSize: 60};
-config.daterange={from:'2020-03-01 00:00:00',to:'2023-07-03 00:00:00'};
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       CONFIGURING TRADER
-// Disable Trader if PAPERTRADER Enabled
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.trader ={enabled:false,
-exchange:'exchange_simulator',
-currency:'',asset:'',
-key:'',secret:'',username:''
-};
+config.backtest = {
+  daterange: {
+    from: "2020-01-01 00:00:00",
+    to: "2023-01-01 00:00:00"
+  },
+  batchSize: 50
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //                       CONFIGURING PAPERTRADER
-// Disable PaperTrader if TRADER Enabled
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.paperTrader = {enabled: true,
-reportInCurrency: true,
-feeMaker: 0.15,
-feeTaker: 0.25,
-feeUsing: 'maker',
-slippage: 0.05
+config.paperTrader = {
+  enabled: true,reportInCurrency: true,
+  simulationBalance: {asset: 1,currency: 100},
+  feeMaker: 0.15,
+  feeTaker: 0.25,
+  feeUsing: 'maker',
+  slippage: 0.05
 };
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //                       CONFIGURING PERFORMANCE ANALYZER
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 config.performanceAnalyzer = {enabled: true,riskFreeReturn: 5};
+
+//Importer
+config.importer={enabled:false};
+
+config.sqlite = {path: 'plugins/sqlite',dataDirectory: 'history',version: 0.1,dependencies:[{module: 'sqlite3'}] };
 //Child to Parent
 config.childToParent = {enabled: false};
 config['I understand that Gekko only automates MY OWN trading strategies']=true;
