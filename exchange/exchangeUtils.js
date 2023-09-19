1a567688034521a7c264e0cf5467bf5c14adcf6b
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-09-19 16:44:48 +0200

    Add files via upload

--- exchange/exchangeUtils.js
+++ exchange/exchangeUtils.js
@@ -1,4 +1,6 @@
-const retry=require('retry');const errors=require('./exchangeErrors');let _=require('lodash');require('lodash-migrate');
+const retry=require('retry');const errors=require('./exchangeErrors');
+const _=require('../core/lodash');
+
 const retryInstance=(options,checkFn,callback,e)=>{if(!options){options={retries:100,factor:1.2,minTimeout:1*1000,maxTimeout:4*1000};}
 let attempt=0;const operation=retry.operation(options);operation.attempt(function(currentAttempt){checkFn((err,result)=>{if(!err){return callback(undefined,result);}
 console.log(new Date,err.message);let maxAttempts=err.retry;if(maxAttempts===true)
