6974a1f8808e019fb651b3462792e070ca71a71d
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-09-27 23:52:46 +0200

    Update NEURALNET_conf.js

--- strategies/conf/NEURALNET_conf.js
+++ strategies/conf/NEURALNET_conf.js
@@ -1,3 +1,8 @@
+var log = require('../../core/log.js');
+var util = require('../../core/util.js');
+var config = util.getConfig();
+var _ = require('../../core/lodash');
+
 config.NEURALNET={
 threshold_buy:1.0,threshold_sell:-1.0,method:'sgd',learning_rate:0.0004,momentum:0.9,
 decay:0.001,SMA_long:10,SMA_short:55,stoploss_enabled:false,stoploss_threshold:0.85,
