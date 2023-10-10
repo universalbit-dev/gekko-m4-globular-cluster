0779d1abaffb3ef8f1e9a1681806cd917b72e0dc
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-10-10 16:58:25 +0200

    Update method-nn.js

--- method-nn.js
+++ method-nn.js
@@ -19,7 +19,8 @@ var config = {};
 //General Settings
 config.debug =true;
 
-config.watch = {exchange: '',currency:'BTC',asset:'LTC',key:'',secret:''};
+config.watch = {exchange: '',currency:'LTC',asset:'BTC',key:'',secret:''};
+
 //Trading Advisor
 config.tradingAdvisor = {enabled:true,candleSize:1,historySize:10};
 config.tradingAdvisor.method= 'NEURALNET';
@@ -31,7 +32,7 @@ config.tradingAdvisor.method= 'NEURALNET';
 ///https://cs.stanford.edu/people/karpathy/convnetjs/demo/trainers.html
 config.NEURALNET={
 threshold_buy:1.0,threshold_sell:-1.0,method:'sgd',learning_rate:0.0004,momentum:0.9,
-decay:0.001,SMA_long:10,SMA_short:55,stoploss_enabled:false,stoploss_threshold:0.85,
+decay:0.001,SMA_long:1000,SMA_short:55,stoploss_enabled:false,stoploss_threshold:0.85,
 hodl_threshold:1,price_buffer_len:100,min_predictions:1000
 };
 
