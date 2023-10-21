57409f1c3bcaa8c282a4b1531f4268f315dfdea9
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-10-21 16:51:48 +0200

    Update asyncIndicatorRunner.js

--- plugins/tradingAdvisor/asyncIndicatorRunner.js
+++ plugins/tradingAdvisor/asyncIndicatorRunner.js
@@ -4,16 +4,11 @@ let fs = require('fs-extra');
 const util = require('../../core/util');
 var config = util.getConfig();
 const dirs = util.dirs();
-//const talib=require('../../core/talib');
 const tulind=require('../../core/tulind');
 const log = require('../../core/log');
-
-
-//const allowedTalibIndicators = _.keys(talib);
 const allowedTulipIndicators = _.keys(tulind);
 
 const AsyncIndicatorRunner = function() {
-  //this.talibIndicators = {};
   this.tulipIndicators = {};
   this.candleProps = {open: [],high: [],low: [],close: [],volume: []};
   this.candlePropsCacheSize = 10000;
@@ -50,31 +45,9 @@ AsyncIndicatorRunner.prototype.processCandle = function(candle, next) {
 
 AsyncIndicatorRunner.prototype.calculateIndicators = function(next) {
   const done = _.after(
-    //_.size(this.talibIndicators) +
     _.size(this.tulipIndicators),this.handlePostFlight(next)
   );
 
-  // handle result from talib
-  /*
-  const talibResultHander = name => (err, result) => {
-    if(err)
-      util.die('TALIB ERROR:', err);
-
-    //this.talibIndicators[name].result = _.mapValues(result, v => _.last(v));
-    done();
-  }
-  */
-
-  // handle result from talib
-/*
-  _.each(
-    this.talibIndicators,
-    (indicator, name) => indicator.run(
-      this.candleProps,
-      talibResultHander(name)
-    )
-  );
-*/
   // handle result from tulip
   var tulindResultHander = name => (err, result) => {
     if(err)
@@ -143,3 +116,10 @@ AsyncIndicatorRunner.prototype.addTulipIndicator = function(name, type, paramete
 }
 
 module.exports = AsyncIndicatorRunner;
+/*
+The MIT License (MIT)
+Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
