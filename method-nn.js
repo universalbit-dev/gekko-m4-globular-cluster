b5e500d40c3b98f35e829e60e1e647da7b2ba740
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2023-12-03 11:21:03 +0100

    Update method-nn.js

--- method-nn.js
+++ method-nn.js
@@ -34,7 +34,7 @@ config.tradingAdvisor.method= 'NN';
 ///https://cs.stanford.edu/people/karpathy/convnetjs/demo/trainers.html
 config.NN= {
 threshold_buy:1.0,threshold_sell:-1.0,method:'sgd',learning_rate:0.01,momentum:0.9,
-l1_decay:0.001,decay:0.001,emaFast:1,stoploss_enabled:true,stoploss_threshold:0.85,
+l1_decay:0.001,decay:0.001,emaFast:1,stoploss_enabled:true,threshold:1,
 hodl_threshold:1,price_buffer_len:100,min_predictions:9,prevAction:'wait',prevPrice:0, predictionCount:0,scale:5
 };
 
