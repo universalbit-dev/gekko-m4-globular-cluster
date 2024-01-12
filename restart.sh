a0b8d034eb6d01fab43ca2f3757f88c7910402c7
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-01-13 00:38:46 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,16 +2,16 @@
 ## sleep in bash for loop ##
 for i in {1..1000}
 do
-  pm2 restart all
-  echo 'Restart After 8 Hour of activity'
-  sleep 8h
+  pm2 restart all --update-env
+  pm2 save
+  echo 'Restart After 1000minutes of activity'
+  sleep 1000m
 done
 
-# sleep 5m  --- sleep 10m --- sleep 1h --- sleep 8h --- sleep 1d --- sleep 1y
+# sleep 5m  --- sleep 10m --- sleep 1h --- sleep 1d --- sleep 1y
 # Unix Command https://en.wikipedia.org/wiki/Sleep_(command)
 #
 # m = minutes
 # h = hours
 # d = days
 # y = years
-
