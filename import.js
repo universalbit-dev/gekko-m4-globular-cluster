eae09cf977c9d6d59537d85b436f77f3b267f0fe
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-01-24 12:57:25 +0100

    Update import.js

--- import.js
+++ import.js
@@ -31,7 +31,7 @@ config.tradingAdvisor.method= 'NN';
 ///https://cs.stanford.edu/people/karpathy/convnetjs/demo/trainers.html
 config.NN={
 threshold_buy:1.0,threshold_sell:-1.0,method:'sgd',learning_rate:0.001,momentum:0.9,
-l1_decay:0.001,l2_decay:0.001,dema:1,threshold:1,price_buffer_len:100,min_predictions:10,prevAction:'wait',prevPrice:0,
+l1_decay:0.001,l2_decay:0.001,threshold:1,price_buffer_len:100,min_predictions:10,prevAction:'wait',prevPrice:0,
 predictionCount:0,scale:5,batch_size:8
 };
 
