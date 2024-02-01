95e5379ba0b8bed04a02dfcfeeb37685c0daafe3
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-02-02 00:31:01 +0100

    Update import.js

--- import.js
+++ import.js
@@ -72,7 +72,7 @@ config.backtestResultExporter = {
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 config.paperTrader = {enabled: false,
   reportInCurrency: true,
-  simulationBalance: {asset: 1,currency: 100},
+  simulationBalance: {asset: 100,currency: 1},
   feeMaker: 0.15,feeTaker: 0.25,feeUsing: 'maker',
   slippage: 0.05
 };
