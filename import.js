85fbd99e442ec89506643c15a3a49abf759a41f1
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-02-02 03:37:22 +0100

    Update import.js

--- import.js
+++ import.js
@@ -52,16 +52,16 @@ config.backtest = {
   daterange: {
     from: "2021-01-01",to: "2021-03-01"
   },
-  batchSize: 1000
+batchSize: 60
 };
 
 config.backtestResultExporter = {
-  enabled: false,
+  enabled: true,
   writeToDisk: true,
   data: {
     stratUpdates: false,
     portfolioValues: true,
-    stratCandles: true,
+    stratCandles: false,
     roundtrips: true,
     trades: true
   }
