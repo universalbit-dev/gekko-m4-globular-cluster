216cb060377bdd77a953bacd685b5fae6a5544ec
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-02-03 07:52:12 +0100

    Update trailingStop.js

--- exchange/triggers/trailingStop.js
+++ exchange/triggers/trailingStop.js
@@ -1,4 +1,5 @@
-const EventEmitter=require('events');class TrailingStop extends EventEmitter{constructor({trail,initialPrice,onTrigger}){super();this.trail=trail;this.isLive=true;this.onTrigger=onTrigger;this.previousPrice=initialPrice;this.trailingPoint=initialPrice-this.trail;}
+const EventEmitter=require('node:events');
+class TrailingStop extends EventEmitter{constructor({trail,initialPrice,onTrigger}){super();this.trail=trail;this.isLive=true;this.onTrigger=onTrigger;this.previousPrice=initialPrice;this.trailingPoint=initialPrice-this.trail;}
 updatePrice(price){if(!this.isLive){return;}
 if(price>this.trailingPoint+this.trail){this.trailingPoint=price-this.trail;}
 this.previousPrice=price;if(price<=this.trailingPoint){this.trigger();}}
