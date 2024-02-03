b206b5b664230f747e55139626ac65d4c9afb191
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-02-03 08:09:06 +0100

    Update handle.js

--- plugins/sqlite/handle.js
+++ plugins/sqlite/handle.js
@@ -48,7 +48,7 @@ module.exports = {
     var db = new sqlite3.Database(fullPath);
     db.run('PRAGMA synchronous = ' + syncMode);
     db.run('PRAGMA journal_mode = ' + journalMode);
-    db.configure('busyTimeout', 10000);
+    db.configure('busyTimeout', 100000);
     return db;
   }
 };
