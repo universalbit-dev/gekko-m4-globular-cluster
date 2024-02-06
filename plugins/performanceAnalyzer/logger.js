231780c89a4b351795366b80331b02e0a99ba1ca
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-02-07 00:37:58 +0100

    Update logger.js

--- plugins/performanceAnalyzer/logger.js
+++ plugins/performanceAnalyzer/logger.js
@@ -33,16 +33,16 @@ Logger.prototype.logReport = function(trade, report) {
   var start = this.round(report.startBalance);
   var current = this.round(report.balance);
 
-  log.info(`(PROFIT REPORT) original balance:\t\t ${start} ${this.currency}`);
-  log.info(`(PROFIT REPORT) current balance:\t\t ${current} ${this.currency}`);
+  log.info(`original balance:\t\t ${start} ${this.currency}`);
+  log.info(`current balance:\t\t ${current} ${this.currency}`);
   log.info(
-    `(PROFIT REPORT) profit:\t\t\t\t ${this.round(report.profit)} ${this.currency}`,
+    `profit:\t\t\t\t ${this.round(report.profit)} ${this.currency}`,
     `(${this.round(report.relativeProfit)}%)`
   );
 }
 
 Logger.prototype.logRoundtripHeading = function() {
-  log.info('(ROUNDTRIP)', 'entry date (UTC)  \texit date (UTC)  \texposed duration\tP&L \tprofit');
+  log.info('', 'entry date (UTC)  \texit date (UTC)  \texposed duration\tP&L \tprofit');
 }
 
 Logger.prototype.logRoundtrip = function(rt) {
@@ -54,7 +54,7 @@ Logger.prototype.logRoundtrip = function(rt) {
     rt.profit.toFixed(2)
   ];
 
-  log.info('(ROUNDTRIP)', display.join('\t'));
+  log.info('', display.join('\t'));
 }
 
 if(mode === 'backtest') {
@@ -96,33 +96,33 @@ if(mode === 'backtest') {
     log.write('');
 
     log.info();
-    log.info('(ROUNDTRIP) REPORT:');
+    log.info('REPORT:');
 
     this.logRoundtripHeading();
     _.each(this.roundtrips, this.logRoundtrip, this);
 
     log.info()
-    log.info(`(PROFIT REPORT) start time:\t\t\t ${report.startTime}`);
-    log.info(`(PROFIT REPORT) end time:\t\t\t ${report.endTime}`);
-    log.info(`(PROFIT REPORT) timespan:\t\t\t ${report.timespan}`);
-    log.info(`(PROFIT REPORT) exposure:\t\t\t ${report.exposure}`);
+    log.info(`start time:\t\t\t ${report.startTime}`);
+    log.info(`end time:\t\t\t ${report.endTime}`);
+    log.info(`timespan:\t\t\t ${report.timespan}`);
+    log.info(`exposure:\t\t\t ${report.exposure}`);
     log.info();
-    log.info(`(PROFIT REPORT) start price:\t\t\t ${report.startPrice} ${this.currency}`);
-    log.info(`(PROFIT REPORT) end price:\t\t\t ${report.endPrice} ${this.currency}`);
-    log.info(`(PROFIT REPORT) Market:\t\t\t\t ${this.round(report.market)}%`);
+    log.info(`start price:\t\t\t ${report.startPrice} ${this.currency}`);
+    log.info(`end price:\t\t\t ${report.endPrice} ${this.currency}`);
+    log.info(`Market:\t\t\t\t ${this.round(report.market)}%`);
     log.info();
-    log.info(`(PROFIT REPORT) amount of trades:\t\t ${report.trades}`);
+    log.info(`amount of trades:\t\t ${report.trades}`);
 
     this.logReport(null, report);
 
     log.info(
-      `(PROFIT REPORT) simulated yearly profit:\t ${report.yearlyProfit}`,
+      `simulated yearly profit:\t ${report.yearlyProfit}`,
       `${this.currency} (${report.relativeYearlyProfit}%)`
     );
 
-    log.info(`(PROFIT REPORT) sharpe ratio:\t\t\t ${report.sharpe}`);
-    log.info(`(PROFIT REPORT) expected downside:\t\t ${report.downside}`);
-    log.info(`(PROFIT REPORT) ratio roundtrips:\t\t ${report.ratioRoundTrips}%`);
+    log.info(`sharpe ratio:\t\t\t ${report.sharpe}`);
+    log.info(`expected downside:\t\t ${report.downside}`);
+    log.info(`ratio roundtrips:\t\t ${report.ratioRoundTrips}%`);
   }
 
   Logger.prototype.handleRoundtrip = function(rt) {
@@ -147,4 +147,3 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-
