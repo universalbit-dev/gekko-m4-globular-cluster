9003a3b40abd75e27da12f97ec5001e9218e0183
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-02-14 10:49:45 +0100

    Update backtest.js

--- backtest.js
+++ backtest.js
@@ -18,18 +18,18 @@ var config = {};
 config.debug =true;
 
 //import kraken exchange data
-config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:60};
+config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:5};
 
 //Trading Advisor
-config.tradingAdvisor = {enabled:true,candleSize:1,historySize:60};
-config.tradingAdvisor.method= 'INVERTER';
+config.tradingAdvisor = {enabled:true,candleSize:1,historySize:40,method:'INVERTER'};
 
 //https://cs.stanford.edu/people/karpathy/convnetjs/demo/regression.html
 config.NN={
 threshold_buy:0.1,threshold_sell:-0.1,method:'adadelta',learning_rate:0.01,momentum:0.0,
 l1_decay:0.001,l2_decay:0.001,threshold:1,price_buffer_len:100,min_predictions:3, hodl_threshold:1,scale:5,batch_size:1};
 
-config.INVERTER={RSI :14,ADX:14,dema:14,diplus:25.5,diminus:25,sma240:240,sma50:50,threshold:3};
+config.INVERTER={rsi:14,adx:14,dema:5,diplus:25.5,diminus:25,
+longema:240,shortema:50,threshold:3};
 
 config.StochRSI={interval:14,threshold:1};
 config.StochRSI.thresholds={low:30,high:70,persistence:5};
