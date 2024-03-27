ead46f47918158a915adf7290db23edcb6e56aac
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-03-27 09:11:17 +0100

    Add files via upload

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [
+  {script: 'node gekko -c ecosystem/backtest/NNSTOCH.js -b'},
+  {script: 'node gekko -c ecosystem/backtest/NN.js -b'},
+  {script: 'node gekko -c ecosystem/backtest/INVERTER.js -b'},
+  {script: 'node gekko -c ecosystem/backtest/NNCCI.js -b'},
+  {script: 'node gekko -c ecosystem/backtest/StochRSI.js -b'}
+],
+
+  deploy : {
+    development : {
+      user : '',
+      host : '',
+      ref  : 'origin/master',
+      repo : 'https://github.com/universalbit-dev/gekko-m4',
+      path : '',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
