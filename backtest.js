3f66733bc375086c0d1953c26a2797396088197c
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-03-28 09:06:39 +0100

    Update backtest.js

--- backtest.js
+++ backtest.js
@@ -46,7 +46,7 @@ threshold_buy:0.1,threshold_sell:-0.1,method:'adadelta',learning_rate:0.01,momen
 l1_decay:0.001,l2_decay:0.001,threshold:1,price_buffer_len:100,min_predictions:3,
 hodl_threshold:1,scale:5,batch_size:1,constant:0.015,history:90};
 
-config.NNCCI.thresholds={up:100,down:-100,persistence:0};
+config.NNCCI.thresholds={up:100,down:-100,persistence:3};
 
 //Adapter
 config.adapter='sqlite';
