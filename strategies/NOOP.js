d5c88095cc0bd14b5c758a6cbabe201073e1da3c
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-04-13 13:28:13 +0200

    Add files via upload

---
+++ strategies/NOOP.js
@@ -0,0 +1,29 @@
+const { spawn } = require('node:child_process');
+const { setTimeout: setTimeoutPromise } = require('node:timers/promises');
+var log = require('../core/log.js');
+var util= require('../core/util.js')
+var config = require('../core/util.js').getConfig();
+var tulind = require('../core/tulind');
+var _ = require('../core/lodash');
+//https://cs.stanford.edu/people/karpathy/convnetjs/started.html
+var convnetjs = require('../core/convnet.js');
+var deepqlearn= require('../core/deepqlearn');
+var math = require('mathjs');
+var fs = require('node:fs');
+var settings = config.NOOP;this.settings=settings;
+
+// Let's create our own method
+// This method is a noop (it doesn't do anything)
+
+var method = {
+
+init : function(){this.name = 'NOOP';},
+
+update : function(){_.noop},
+log : function(){_.noop},
+check : function(){_.noop},
+end : function() {_.noop}
+
+};
+
+module.exports = method;
