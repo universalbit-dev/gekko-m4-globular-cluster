9fb0a7639537f290c23b4e0604dcb7512b5f28a3
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-04-26 08:23:23 +0200

    Update backtest.js

--- backtest.js
+++ backtest.js
@@ -29,8 +29,9 @@ threshold_buy:0.1,threshold_sell:-0.1,method:'adadelta',learning_rate:0.01,momen
 l1_decay:0.001,l2_decay:0.001,threshold:1,price_buffer_len:100,min_predictions:3,
 hodl_threshold:1,scale:5,batch_size:1};
 
-config.INVERTER={rsi:14,adx:14,dema:1,diplus:25.5,diminus:25,
-longema:240,shortema:50,threshold:3};
+//optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377
+config.INVERTER={rsi:13,adx:13,dema:1,diplus:21,diminus:34,
+longema:233,shortema:55,threshold:3};
 
 config.STOCHRSI={interval:14,threshold:1};
 config.STOCHRSI.thresholds={low:30,high:70,persistence:5};
