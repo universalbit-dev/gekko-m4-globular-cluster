4e1859f73ce5a933d12987903d279f8a70bd132a
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-04-27 16:11:30 +0200

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -4,8 +4,8 @@ for i in {1..1000}
 do
   pm2 restart all --update-env
   pm2 save
-  echo 'Restart After 1000minutes of activity'
-  sleep 1000m
+  echo 'Restart After 180minutes of activity'
+  sleep 180m
 done
 
 # sleep 5m  --- sleep 10m --- sleep 1h --- sleep 1d --- sleep 1y
@@ -15,3 +15,4 @@ done
 # h = hours
 # d = days
 # y = years
+
