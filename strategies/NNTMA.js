61256d6db6c08b4dba50ae9557f2160748fce4b2
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-04-28 22:21:14 +0200

    Update NNTMA.js

--- strategies/NNTMA.js
+++ strategies/NNTMA.js
@@ -12,7 +12,7 @@ var math = require('mathjs');
 var fs = require('node:fs');
 var settings = config.NNTMA;this.settings=settings;
 var stoploss=require('./indicators/StopLoss');
-
+var async = require('async');
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 async function wait() {
   console.log('keep calm...');await sleep(2000);
