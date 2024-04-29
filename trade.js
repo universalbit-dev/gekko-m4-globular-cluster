a6d824c941886d028d58f093b9a45ad26b3c27ad
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-04-29 09:41:49 +0200

    Update trade.js

--- trade.js
+++ trade.js
@@ -1,4 +1,47 @@
+var config = {};
+//General Settings
+config.debug =true;
+
+config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',
+key:'000000-000000-000000',secret:'000000-000000-000000',tickrate:20};
+
+//Trading Advisor
+config.tradingAdvisor = {enabled:true,candleSize:1,historySize:40};
+config.tradingAdvisor.method= 'NOOP';
+config.NOOP={};
+
+//Adapter
+config.adapter='sqlite';
+
+//Trader
+config.trader={
+enabled:false,exchange:'kraken',currency:'XBT',asset:'LTC',
+key:'000000-000000-000000',secret:'000000-000000-000000'
+};
+
+config.candleWriter={enabled:true,adapter:'sqlite'};
+config.adviceLogger={enabled:true};
+
+//BackTest
+config.backtest = {enabled:false};
+config.backtestResultExporter = {enabled: false};
+
+//PaperTrader
+config.paperTrader = {enabled: false};
+
+//Performance Analyzer
+config.performanceAnalyzer = {enabled: true,riskFreeReturn: 5};
+
+//Import
+config.importer = {enabled:false};
+
+//DataBase
+config.sqlite = {path: 'plugins/sqlite',dataDirectory: 'history',version:'5.1.1',dependencies:[{module: 'sqlite3',version:'5.1.7'}] };
+config['I understand that Gekko only automates MY OWN trading strategies']=true;
+module.exports = config;
+
 /*
+
 The MIT License (MIT)
 Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -11,100 +54,7 @@ by this software. There can be bugs and the bot may not perform as expected
 or specified. Please consider testing it first with paper trading and/or
 backtesting on historical data. Also look at the code to see what how
 it is working.
+
 */
 
-var config = {};
-//General Settings
-config.debug =true;
 
-//import kraken exchange data
-config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',
-key:'000000-000000-000000',
-secret:'000000-000000-000000',
-tickrate:60};
-
-//Trading Advisor
-config.tradingAdvisor = {enabled:true,candleSize:1,historySize:40};
-config.tradingAdvisor.method= 'NNSTOCH';
-
-//optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377
-config.INVERTER={rsi:13,adx:13,dema:1,diplus:34,diminus:21,longema:233,shortema:55,threshold:3};
-
-//https://cs.stanford.edu/people/karpathy/convnetjs/demo/regression.html
-config.NN={
-threshold_buy:0.1,threshold_sell:-0.1,method:'adadelta',learning_rate:0.01,momentum:0.0,
-l1_decay:0.001,l2_decay:0.001,threshold:1,price_buffer_len:1000,min_predictions:3,
-hodl_threshold:1,scale:5,batch_size:1};
-
-config.NNSTOCH={
-threshold_buy:1,threshold_sell:-1,method:'adadelta',learning_rate:0.01,momentum:0.0,
-l1_decay:0.001,l2_decay:0.001,threshold:1,price_buffer_len:1000,min_predictions:1, hodl_threshold:1,scale:5,batch_size:1,interval:3};
-config.NNSTOCH.thresholds={low:30,high:70,persistence:3};
-
-config.NOOP={};
-
-//Adapter
-config.adapter='sqlite';
-
-//Trader
-config.trader={
-enabled:false,exchange:'kraken',currency:'XBT',asset:'LTC',
-key:'000000-000000-000000',
-secret:'000000-000000-000000'
-};
-
-config.candleWriter={enabled:true,adapter:'sqlite'};
-config.adviceLogger={enabled:true};
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       CONFIGURING BACKTESTING
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.backtest = {
-  enabled:false,
-  daterange:{from:"2022-01-02",to:"2022-03-01"},
-batchSize: 60
-};
-
-config.backtestResultExporter = {
-  enabled: false,
-  writeToDisk: true,
-  data: {
-    stratUpdates: false,
-    portfolioValues: true,
-    stratCandles: false,
-    roundtrips: true,
-    trades: true
-  }
-};
-
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       CONFIGURING PAPERTRADER
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.paperTrader = {enabled: false,
-  reportInCurrency: true,
-  simulationBalance: {asset: 1,currency: 1},
-  feeMaker: 0.15,feeTaker: 0.25,feeUsing: 'maker',
-  slippage: 0.05
-};
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       CONFIGURING PERFORMANCE ANALYZER
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.performanceAnalyzer = {enabled: true,riskFreeReturn: 5};
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       CONFIGURING IMPORTER
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.importer = {
-  enabled:false,
-  daterange:{from:"2022-01-02",to:"2022-03-01"}
-}
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       CONFIGURING DB
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.sqlite = {path: 'plugins/sqlite',dataDirectory: 'history',version:'5.1.1',dependencies:[{module: 'sqlite3',version:'5.1.7'}] };
-
-config['I understand that Gekko only automates MY OWN trading strategies']=true;
-module.exports = config;
