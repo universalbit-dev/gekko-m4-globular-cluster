6a8408582d6fda89b227c830022422acfde4d223
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-04-30 22:09:08 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,59 @@
 module.exports = {
-  apps : [
-  {script: 'node gekko -c ecosystem/backtest/backtest_nnstoch.js -b'},
-  {script: 'node gekko -c ecosystem/backtest/backtest_nn.js -b'},
-  {script: 'node gekko -c ecosystem/backtest/backtest_inverter.js -b'},
-  {script: 'node gekko -c ecosystem/backtest/backtest_nncci.js -b'},
-  {script: 'node gekko -c ecosystem/backtest/backtest_stochrsi.js -b'}
-],
+  apps : [{
+    name: "inverter",
+    script    : 'gekko.js',
+    args      : '-c ecosystem/backtest/backtest_inverter.js -b',
+    name      : '|INVERTER|-backtest-|',
+    instances : "1",
+    exec_mode : "cluster",
+    env: {NODE_ENV: "development",},
+    env_production: {NODE_ENV: "production",}
+  },
+
+  {
+  name: 'stochrsi',
+  script    : 'gekko.js',
+  args      : '-c ecosystem/backtest/backtest_stochrsi.js -b',
+  name      : '|STOCHRSI|-backtest-|',
+  instances : "1",
+  exec_mode : "cluster"
+  },
+
+  {
+  name: 'nnstoch',
+  script    : 'gekko.js',
+  args      : '-c ecosystem/backtest/backtest_nnstoch.js -b',
+  name      : '|NNSTOCH|-backtest-|',
+  instances : "1",
+  exec_mode : "cluster"
+  },
+
+  {
+  name: 'nncci',
+  script    : 'gekko.js',
+  args      : '-c ecosystem/backtest/backtest_nncci.js -b',
+  name      : '|NNCCI|-backtest-',
+  instances : "1",
+  exec_mode : "cluster"
+  },
+
+  {
+  name: 'nntma',
+  script    : 'gekko.js',
+  args      : '-c ecosystem/backtest/backtest_nntma.js -b',
+  name      : '|NNTMA|-backtetst-|',
+  instances : "1",
+  exec_mode : "cluster"
+  },
   
-  deploy : {
-    development : {
-      user : '',
-      host : '',
-      ref  : 'origin/master',
-      repo : 'https://github.com/universalbit-dev/gekko-m4',
-      path : '',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env development',
-      'pre-setup': ''
-    }
+  {
+  name: 'nn',
+  script    : 'gekko.js',
+  args      : '-c ecosystem/backtest/backtest_nn.js -b',
+  name      : '|NN|-backtetst-|',
+  instances : "1",
+  exec_mode : "cluster"
   }
-};
+
+]}
+
