9f288713e7566cb6969e0eb821f28f6fd6f00dcd
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-05-01 20:21:45 +0200

    Update backtest_nnstoch.js

--- ecosystem/backtest/backtest_nnstoch.js
+++ ecosystem/backtest/backtest_nnstoch.js
@@ -13,7 +13,7 @@ hodl_threshold:1,scale:5,batch_size:1,interval:3};
 config.NNSTOCH.thresholds={low:30,high:70,persistence:3};
 
 //Trading Advisor
-config.tradingAdvisor = {enabled:true,candleSize:1,historySize:40,method:'NNSTOCH'};
+config.tradingAdvisor = {enabled:true,candleSize:5,historySize:40,method:'NNSTOCH'};//candleSize (5 minutes)* historySize (40 minutes) == period
 
 //Previous Month
 var previous_month = new Date();
