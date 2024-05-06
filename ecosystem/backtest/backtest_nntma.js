83e7b4f931d8eae01ef20310e7062dea5d26826f
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-05-06 22:09:24 +0200

    Update backtest_nntma.js

--- ecosystem/backtest/backtest_nntma.js
+++ ecosystem/backtest/backtest_nntma.js
@@ -9,7 +9,7 @@ config.tradingAdvisor = {enabled:true,candleSize:15,historySize:10,method:'NNTMA
 //optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 , 610 , 987
 config.NNTMA={
 threshold_buy:1,threshold_sell:-1,method:'adadelta',learning_rate:0.01,momentum:0.0,
-l1_decay:0.001,l2_decay:0.001,threshold:1,price_buffer_len:987,min_predictions:89,
+l1_decay:0.001,l2_decay:0.001,threshold:1,price_buffer_len:987,min_predictions:144,
 hodl_threshold:1,scale:1,batch_size:1,constant:0.015,history:89,stoploss:3,
 short:10,long:80};
 
