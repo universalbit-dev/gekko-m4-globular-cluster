f55e4897be2f23c42f8a6a2e708907bc54de1244
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-05-07 11:12:31 +0200

    Update backtest_inverter.js

--- ecosystem/backtest/backtest_inverter.js
+++ ecosystem/backtest/backtest_inverter.js
@@ -6,7 +6,7 @@ config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:20};
 
 config.tradingAdvisor = {enabled:true,candleSize:1,historySize:1000,method:'INVERTER'};
 //optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 , 610 , 987
-config.INVERTER={rsi:13,adx:13,dx:13,di:13,dema:1,ema:13,diplus:21.5,diminus:21,longema:233,shortema:55,stoploss:3};
+config.INVERTER={};
 
 //Date.prototype.toISOString()
 //Previous Month
