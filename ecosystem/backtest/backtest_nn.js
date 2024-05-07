53042f5eafeb64c1a63d47d7253cef92b9011bf5
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-05-07 16:23:43 +0200

    Update backtest_nn.js

--- ecosystem/backtest/backtest_nn.js
+++ ecosystem/backtest/backtest_nn.js
@@ -1,85 +1,283 @@
-var config = {};
-//General Settings
-config.debug =true;
+const { spawn } = require('node:child_process');
+const { setTimeout: setTimeoutPromise } = require('node:timers/promises');
+var log = require('../core/log.js');
+var util= require('../core/util.js')
+var config = require('../core/util.js').getConfig();
+const _ = require('../core/lodash');
+//https://cs.stanford.edu/people/karpathy/convnetjs/started.html
+var convnetjs = require('../core/convnet.js');
+var deepqlearn= require('../core/deepqlearn');
+var math = require('mathjs');var uuid = require('uuid');
+var fs = require('node:fs');
+var settings = config.NN;this.settings=settings;
+var stoploss= require('./indicators/StopLoss.js');
 
-config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:20};
-
-//optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 , 610 , 987
-//Strategies
-config.NN={
-threshold_buy:1,threshold_sell:-1,method:'adadelta',learning_rate:0.01,momentum:0.0,
-l1_decay:0.001,l2_decay:0.001,threshold:1,price_buffer_len:987,min_predictions:89,
-hodl_threshold:1,scale:1,batch_size:1};
-
-//Trading Advisor
-config.tradingAdvisor = {enabled:true,candleSize:1,historySize:1000,method:'NN'};
-
-//Date.prototype.toISOString()
-//Previous Month
-var previous_month = new Date();
-previous_month.setDate(1);
-previous_month.setMonth(previous_month.getMonth()-1);
-previous_month.setDate(2); 
-
-//Current Month
-var current_month = new Date();
-current_month.setDate(1);
-current_month.setMonth(current_month.getMonth());
-current_month.setDate(2); 
-
-
-//Backtest Exchange Data  FROM previous month TO current month
-config.backtest = {enabled:true,
-  daterange:{from:previous_month,to:current_month},
-  batchSize: 1000
+var async = require('async');
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+async function wait() {
+  console.log('keep calm...');await sleep(200000);
+  console.log('...make something of amazing');
+  for (let i = 0; i < 5; i++)
+  {if (i === 4) await sleep(2000);}
 };
 
-//DataBase
-config.adapter='sqlite';config.adapter.path= 'plugins/sqlite';
-config.sqlite = {path: 'plugins/sqlite',dataDirectory: 'history',version:'5.1.1',
-dependencies:[{module: 'sqlite3',version:'5.1.7'}] };
 
-//Trader
-config.trader={enabled:false,exchange:'',currency:'',asset:'',key:'',secret:''};
+var method = {
+  priceBuffer : [],
+  predictionCount : 0,
+  stoplossCounter : 0,
+  prevPrice : 0,
+  prevAction : 'none',
+  hodl_threshold : 1,
 
-//Candle Writer
-config.candleWriter={enabled:false};
+  init : function() {
 
-//Advice Logger
-config.adviceLogger={enabled:false};
+    this.requiredHistory = this.settings.historySize;
+    this.RSIhistory = [];
+    log.info('================================================');
+    log.info('keep calm and make somethig of amazing');
+    log.info('================================================');
 
-//Export BackTest Result
-config.backtestResultExporter = {enabled: true,writeToDisk: true,
-  data: {stratUpdates: false,portfolioValues: true,stratCandles: false,roundtrips: true,trades: true}
+    this.trend = {
+    direction: 'none',
+    duration: 0,
+    persisted: false,
+    adviced: false
+  };
+    //Date
+    startTime = new Date();
+    //indicators
+    this.addIndicator('stoploss', 'StopLoss', {threshold : 3});
+    //DEMA
+    this.addTulipIndicator('dema', 'dema', {optInTimePeriod:1});
+    //RSI
+    this.addTulipIndicator('rsi', 'rsi', {optInTimePeriod:13});
+
+    this.name = 'NN';
+    this.nn = new convnetjs.Net();
+    //https://cs.stanford.edu/people/karpathy/convnetjs/demo/regression.html
+    var x= Math.floor((Math.random() * 100) + 1);
+    var y=Math.floor((Math.random() * 100) * 10);
+    var z=Math.floor((Math.random() * 100) + 1);
+    const layers = [
+      {type:'input', out_sx:x, out_sy:y, out_depth:z},
+      {type:'conv', num_neurons:144, activation: 'relu'},
+      {type:'fc', num_neurons:144, activation:'sigmoid'},
+      {type:'regression', num_neurons:1}
+    ];
+
+    this.nn.makeLayers(layers);
+
+    if(this.settings.method == 'sgd')
+    {
+      this.trainer = new convnetjs.SGDTrainer(this.nn, {
+        learning_rate: this.settings.learning_rate,
+        momentum: 0.9,
+        batch_size:8,
+        l2_decay: this.settings.l2_decay,
+        l1_decay: this.settings.l1_decay
+      });
+    }
+    else if(this.settings.method == 'adadelta')
+    {
+      this.trainer = new convnetjs.SGDTrainer(this.nn, {
+        method: this.settings.method,
+        learning_rate: this.settings.learning_rate,
+        eps: 1e-6,
+        ro:0.95,
+        batch_size:1,
+        l2_decay: this.settings.l2_decay
+      });
+    }
+    else if(this.settings.method == 'adagrad')
+    {
+      this.trainer = new convnetjs.SGDTrainer(this.nn, {
+        method: this.settings.method,
+        learning_rate: this.settings.learning_rate,
+        eps: 1e-6,
+        batch_size:8,
+        l2_decay: this.settings.l2_decay
+      });
+    }
+    else if(this.settings.method == 'nesterov')
+    {
+      this.trainer = new convnetjs.SGDTrainer(this.nn, {
+        method: this.settings.method,
+        learning_rate: this.settings.learning_rate,
+        momentum: 0.9,
+        batch_size:8,
+        l2_decay: this.settings.l2_decay
+      });
+    }
+    else if(this.settings.method == 'windowgrad')
+    {
+      this.trainer = new convnetjs.SGDTrainer(this.nn, {
+        method: this.settings.method,
+        learning_rate: this.settings.learning_rate,
+        eps: 1e-6,
+        ro:0.95,
+        batch_size:8,
+        l2_decay: this.settings.l2_decay
+      });
+    }
+    else
+    {
+      this.trainer = new convnetjs.Trainer(this.nn, {
+        method: 'adadelta',
+        learning_rate: 0.01,
+        momentum: 0.0,
+        batch_size:1,
+        eps: 1e-6,
+        ro:0.95,
+        l2_decay: 0.001,
+        l1_decay: 0.001
+      });
+    }
+
+    this.hodl_threshold = this.settings.hodl_threshold || 1;
+  },
+
+  learn : function () {
+    for (let i = 0; i < _.size(this.priceBuffer) - 1; i++) {
+      let data = [this.priceBuffer[i]];
+      let current_price = [this.priceBuffer[i + 1]];
+      let vol = new convnetjs.Vol(data);
+      this.trainer.train(vol, current_price);
+      this.predictionCount++;
+    }
+  },
+  setNormalizeFactor : function() {
+    this.settings.scale = Math.pow(10,Math.trunc(candle.high).toString().length+2);
+    log.debug('Set normalization factor to',this.settings.scale);
+  },
+//Reinforcement Learning
+//https://cs.stanford.edu/people/karpathy/convnetjs/docs.html
+  brain:function(){
+    var brain = new deepqlearn.Brain(this.x, this.z);
+    var state = [Math.random(), Math.random(), Math.random()];
+    for(var k=0;k < _.size(this.priceBuffer) - 1;k++)
+    {
+      var action = brain.forward(state); //returns index of chosen action
+      var reward = action === 0 ? 1.0 : 0.0;
+      brain.backward([reward]); // <-- learning magic happens here
+      state[Math.floor(Math.random()*3)] += Math.random()*2-0.5;
+    }
+    brain.epsilon_test_time = 0.0;//don't make any more random choices
+    brain.learning = false;//
+  },
+
+  update : function(candle)
+  {
+  rsi=this.tulipIndicators.rsi.result.result;this.rsi=rsi;
+  this.RSIhistory.push(this.rsi);
+  if(_.size(this.RSIhistory) > this.interval)
+  //remove oldest RSI value
+  this.RSIhistory.shift();
+  this.lowestRSI = _.min(this.RSIhistory);
+  this.highestRSI = _.max(this.RSIhistory);
+  this.stochRSI = ((this.rsi - this.lowestRSI) / (this.highestRSI - this.lowestRSI)) * 100;
+
+  if(_.size(this.priceBuffer) > this.settings.price_buffer_len)
+  // remove oldest priceBuffer value
+  this.priceBuffer.shift();
+    dema=this.tulipIndicators.dema.result.result;
+    if (1 === this.settings.scale && 1 < candle.high && 0 === this.predictionCount)
+    this.setNormalizeFactor();
+    this.priceBuffer.push(dema / this.settings.scale );
+    if (2 > _.size(this.priceBuffer)) return;
+     for (i=0;i<3;++i)
+     this.learn();this.brain();
+     while (this.settings.price_buffer_len < _.size(this.priceBuffer))
+     this.priceBuffer.shift();
+
+    fs.appendFile('logs/csv/' + config.watch.asset + ':' + config.watch.currency + '_' + this.name + '_' + startTime + '.csv',
+  	candle.start + "," + candle.open + "," + candle.high + "," + candle.low + "," + candle.close + "," + candle.vwp + "," + candle.volume + "," + candle.trades + "\n", function(err) {
+  	if (err) {return console.log(err);}
+  	});
+  },
+
+  predictCandle : function(candle) {
+    let vol = new convnetjs.Vol(this.priceBuffer);
+    let prediction = this.nn.forward(vol);
+    return prediction.w[0];
+  },
+
+  //https://www.investopedia.com/articles/investing/092115/alpha-and-beta-beginners.asp
+  check : function(candle) {
+
+    dema=this.tulipIndicators.dema.result.result;
+    rsi=this.tulipIndicators.rsi.result.result;
+    this.rsi=rsi;
+	if(this.stochRSI > 70) {
+		// new trend detected
+		if(this.trend.direction !== 'high')
+			this.trend = {
+				duration: 0,
+				persisted: false,
+				direction: 'high',
+				adviced: false
+			};
+
+		this.trend.duration++;
+
+		log.debug('In high since', this.trend.duration, 'candle(s)');
+
+		if(this.trend.duration >= 3)this.trend.persisted = true;
+
+		if(this.trend.persisted && !this.trend.adviced && this.stochRSI !=100)
+		{this.trend.adviced = true;}
+		else{this.advice();}
+
+	} else if(this.stochRSI < 30) {
+
+		// new trend detected
+		if(this.trend.direction !== 'low')
+		this.trend = {duration: 0,persisted: false,direction: 'low',adviced: false};
+		this.trend.duration++;
+
+		log.debug('In low since', this.trend.duration, 'candle(s)');
+		if(this.trend.duration >= 3){this.trend.persisted = true;}
+		if(this.trend.persisted && !this.trend.adviced && this.stochRSI != 0)
+		{this.trend.adviced = true;}
+		else {this.advice();}
+
+	} else {
+		// trends must be on consecutive candles
+		this.trend.duration = 0;
+		log.debug('In no trend');this.advice();
+	}
+
+    if(this.predictionCount > this.settings.min_predictions)
+    {
+      var prediction = this.predictCandle() * this.settings.scale;
+      var currentPrice = candle.close;
+      var meanp = math.mean(prediction, currentPrice);
+      //when alpha is the "excess" return over an index, what index are you using?
+      var meanAlpha = (meanp - currentPrice) / currentPrice * 100;
+      var signalSell = candle.close > this.prevPrice || candle.close <
+      (this.prevPrice*this.settings.hodl_threshold);
+      var signal = meanp < currentPrice;
+    }
+    if ((this.trend.adviced && this.stochRSI != 0 && 'buy' !== this.prevAction)&&
+    ('buy' !== this.prevAction && (signal === false)  && meanAlpha > 1)){
+    this.advice('long');wait();
+    this.brain();
+    }
+    if ((this.trend.adviced && this.stochRSI != 100 &&'sell' !== this.prevAction)&&
+    ('sell' !== this.prevAction && (signal === true) && meanAlpha < -1  &&
+    signalSell === true)){
+    this.advice('short');wait();
+    this.brain();}
+
+//stoploss as Reinforcement Learning
+    if ('stoploss' === this.indicators.stoploss.action)
+    {
+    this.stoplossCounter++;log.info(':',this.indicators.stoploss.action);
+    this.brain();this.prevAction='sell';signal=false;
+    }
+
+  },
+
+  end : function() {log.info('THE END');}
 };
-
-//PaperTrader
-config.paperTrader = {enabled: true,reportInCurrency: true,
-  simulationBalance: {asset: 100,currency: 1},
-  feeMaker: 0.15,feeTaker: 0.25,feeUsing: 'maker',slippage: 0.05
-};
-
-//Performance Analyzer
-config.performanceAnalyzer = {enabled: true,riskFreeReturn: 5};
-
-//Import                       
-config.importer = {enabled:false}
-
-config['I understand that Gekko only automates MY OWN trading strategies']=true;
-module.exports = config;
-/*
-The MIT License (MIT)
-Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-Disclaimer:
-                              USE AT YOUR OWN RISK!
-The author of this project is NOT responsible for any damage or loss caused
-by this software. There can be bugs and the bot may not perform as expected
-or specified. Please consider testing it first with paper trading and/or
-backtesting on historical data. Also look at the code to see what how
-it is working.
-*/
-
-
+module.exports = method;
