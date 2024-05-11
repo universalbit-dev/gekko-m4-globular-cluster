7a93312fb726dad77683a09c0666b151e2eb6660
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-05-11 12:31:43 +0200

    Add files via upload

--- plugins/adviceLogger.js
+++ plugins/adviceLogger.js
@@ -1,6 +1,6 @@
 
 const _ = require('../core/lodash3');require('lodash-migrate');
-const makeEventEmitter = require('node:events');
+const {EventEmitter} = require('node:events');
 var log = require('../core/log');
 util = require('../core/util');
 config = util.getConfig();
@@ -34,3 +34,11 @@ Actor.prototype.processAdvice = function(advice) {
 
 Actor.prototype.finalize = function(advice, done) {done();};
 module.exports = Actor;
+
+/*
+The MIT License (MIT)
+Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
