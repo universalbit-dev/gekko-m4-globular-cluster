c72de059e14e5ca90c3f4a4e58cf3179d5ef8f3c
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-05-13 02:34:21 +0200

    Update backtest_nnstoch.js

--- ecosystem/backtest/backtest_nnstoch.js
+++ ecosystem/backtest/backtest_nnstoch.js
@@ -4,6 +4,8 @@ config.debug =true;
 
 config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:20};
 //optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 , 610 , 987
+
+https://cs.stanford.edu/people/karpathy/convnetjs/demo/trainers.html
 config.NNSTOCH={method:'adadelta'};
 
 //Trading Advisor
