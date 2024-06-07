248290b67aea199e50a0457a24ef378751e99f25
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-07 21:08:22 +0200

    Add files via upload

--- strategies/NNSTOCH.js
+++ strategies/NNSTOCH.js
@@ -219,9 +219,9 @@ console.log("\t\t\t\tcourtesy of... "+ operator[result]);
     log.info('==================================================================');
 
     if ((this.trend.persisted && this.stochRSI != 0 )&&('buy' != this.prevAction && signal === false && meanAlpha > 1))
-    {this.advice('long');this.trend ={duration: 0,persisted: false,direction: 'none',adviced: false};makeoperators();wait();}
+    {this.advice('long');this.trend ={duration: 0,persisted: false,direction: 'none',adviced: false};this.makeoperators();wait();}
     if ((this.trend.persisted && this.stochRSI != 100)&&('sell' != this.prevAction && signal === true && meanAlpha < -1 && signalSell === true))
-    {this.advice('short');this.trend ={duration: 0,persisted: false,direction: 'none',adviced: false};makeoperators();wait();}
+    {this.advice('short');this.trend ={duration: 0,persisted: false,direction: 'none',adviced: false};this.makeoperators();wait();}
 
     //stoploss indicator as Reinforcement Learning
     if ('stoploss' === this.indicators.stoploss.action)
