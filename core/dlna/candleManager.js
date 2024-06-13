5d33c8448b16a915709f2ff4f3fbaa085bbff26b
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-13 11:51:24 +0200

    Add files via upload

--- core/dlna/candleManager.js
+++ core/dlna/candleManager.js
@@ -9,10 +9,11 @@ var util = require('../../core/util');
 var dirs = util.dirs();
 var config = require('../../core/util.js').getConfig();
 var log = require('../../core/log');
+const {EventEmitter} = require('node:events');
 
 var CandleCreator = require(dirs.dlna + 'candleCreator');
 var Manager = function() {
-  _.bindAll(this,_.functions(this));
+  _.bindAll(this);
   this.candleCreator = new CandleCreator;
   this.candleCreator
     .on('candles', this.relayCandles);
