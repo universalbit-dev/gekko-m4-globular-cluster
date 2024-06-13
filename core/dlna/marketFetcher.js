e6e231285d732439017ec69eaf6be07108c0efde
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-13 17:08:35 +0200

    Add files via upload

--- core/dlna/marketFetcher.js
+++ core/dlna/marketFetcher.js
@@ -11,41 +11,30 @@ var TradeBatcher = require(util.dirs().dlna + 'tradeBatcher');
 const {EventEmitter} = require('node:events');
 
 var Fetcher = function(config) {
-  if(!_.isObject(config))
-    throw new Error('TradeFetcher expects a config');
+  if(!_.isObject(config))throw new Error('TradeFetcher expects a config');
 
   var exchangeName = config.watch.exchange.toLowerCase();
   var DataProvider = require(util.dirs().gekko + 'exchange/wrappers/' + exchangeName);
 
-
-// Create a public dataProvider object which can retrieve live
-// trade information from an exchange.
+//Create a public dataProvider object which can retrieve live
+//trade information from an exchange.
   this.exchangeTrader = new DataProvider(config.watch);
   this.exchange = exchangeChecker.settings(config.watch);
-
   var requiredHistory = config.tradingAdvisor.candleSize * config.tradingAdvisor.historySize;
-// If the trading adviser is enabled we might need a very specific fetch since
-// to line up [local db, trading method, and fetching]
-  if(config.tradingAdvisor.enabled && config.tradingAdvisor.firstFetchSince) {
-    this.firstSince = config.tradingAdvisor.firstFetchSince;
+//If the trading adviser is enabled we might need a very specific fetch since
+//to line up [local db, trading method, and fetching]
 
-    if(this.exchange.providesHistory === 'date') {
-      this.firstSince = moment.unix(this.firstSince).utc();
-    }
-  }
+  if(config.tradingAdvisor.enabled && config.tradingAdvisor.firstFetchSince) {this.firstSince = config.tradingAdvisor.firstFetchSince;}
+  if(this.exchange.providesHistory === 'date') {this.firstSince = moment.unix(this.firstSince).utc();}
+  
   this.batcher = new TradeBatcher(this.exchange.tid);
-  this.pair = [
-    config.watch.asset,
-    config.watch.currency
-  ].join('/');
-
+  this.pair = [config.watch.asset,config.watch.currency].join('/');
   log.info('Starting to watch the market:',this.exchange.name,this.pair);
-// if the exchange returns an error we will keep on retrying until next scheduled fetch.
+//if the exchange returns an error we will keep on retrying until next scheduled fetch.
   this.tries = 0;
   this.limit = 20;
   this.firstFetch = true;
-  this.batcher.on('new batch', this.relayTrades);
-  _.bindAll(this);
+  this.batcher.on('new batch', this.relayTrades);_.bindAll(this);
 }
 util.makeEventEmitter(Fetcher);
 
