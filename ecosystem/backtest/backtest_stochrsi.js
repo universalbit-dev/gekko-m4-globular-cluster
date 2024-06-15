05c3d2e9101e5c9e45c6d41e0c7d528408fbd2e5
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-16 01:56:16 +0200

    Add files via upload

--- ecosystem/backtest/backtest_stochrsi.js
+++ ecosystem/backtest/backtest_stochrsi.js
@@ -1,83 +1,68 @@
 var config = {};
 //General Settings
 config.debug =true;
-
-//import kraken exchange data
 config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:30};
 
-//Trading Advisor
-config.tradingAdvisor = {enabled:true,candleSize:15,historySize:10};
-config.tradingAdvisor.method= 'STOCHRSI';
-
+config.tradingAdvisor = {enabled:true,candleSize:10,historySize:10,method:'STOCHRSI'};
 config.STOCHRSI={interval:10,threshold:1};
 config.STOCHRSI.thresholds={low:30,high:70,persistence:5};
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       BACKTESTING
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.backtest = {
-  enabled:true,
-  daterange:{from:"2022-01-02",to:"2022-03-01"},
-  batchSize: 60
+
+//Date.prototype.toISOString()
+//Previous Month
+var previous_month = new Date();
+previous_month.setDate(1);
+previous_month.setMonth(previous_month.getMonth()-1);
+previous_month.setDate(4);
+
+//Current Month
+var current_month = new Date();
+current_month.setDate(1);
+current_month.setMonth(current_month.getMonth());
+current_month.setDate(4);
+
+//Backtest Exchange Data  FROM previous month TO current month
+config.backtest = {enabled:true,
+  daterange:{from:previous_month,to:current_month},
+  batchSize: 1000
 };
 
-config.backtestResultExporter = {
-  enabled: true,
-  writeToDisk: true,
-  data: {
-    stratUpdates: false,
-    portfolioValues: true,
-    stratCandles: false,
-    roundtrips: true,
-    trades: true
-  }
-};
-
-//Adapter
-config.adapter='sqlite';
+//DataBase
+config.adapter='sqlite';config.adapter.path= 'plugins/sqlite';
+config.sqlite = {path: 'plugins/sqlite',dataDirectory: 'history',version:'5.1.1',
+dependencies:[{module: 'sqlite3',version:'5.1.7'}] };
 
 //Trader
 config.trader={enabled:false,exchange:'',currency:'',asset:'',key:'',secret:''};
 
 //Candle Writer
-config.candleWriter={enabled:false,adapter:'sqlite'};
+config.candleWriter={enabled:false};
 
 //Advice Logger
-config.adviceLogger={enabled:true};
+config.adviceLogger={enabled:false};
 
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       PAPERTRADER
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.paperTrader = {enabled: true,
-  reportInCurrency: true,
-  simulationBalance: {asset: 100,currency: 1},
-  feeMaker: 0.15,feeTaker: 0.25,feeUsing: 'maker',
-  slippage: 0.05
+//Export BackTest Result
+config.backtestResultExporter = {enabled: true,writeToDisk: true,
+  data: {stratUpdates: false,portfolioValues: true,stratCandles: false,roundtrips: true,trades: true}
 };
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       PERFORMANCE ANALYZER
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//PaperTrader
+config.paperTrader = {enabled: true,reportInCurrency: true,
+  simulationBalance: {asset: 100,currency: 1},
+  feeMaker: 0.15,feeTaker: 0.25,feeUsing: 'maker',slippage: 0.05
+};
+
+//Performance Analyzer
 config.performanceAnalyzer = {enabled: true,riskFreeReturn: 5};
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       IMPORTER
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-config.importer = {
-  enabled:false,
-  daterange:{from:"2021-01-01",to:"2021-03-01"}
-};
+//Import
+config.importer = {enabled:false}
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//                       DATABASE
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-config.sqlite = {path: 'plugins/sqlite',dataDirectory: 'history',version:'4.1.2',dependencies:[{module: 'sqlite3',version:'5.1.4'}] };
 config['I understand that Gekko only automates MY OWN trading strategies']=true;
 module.exports = config;
 
 /*
+
 The MIT License (MIT)
 Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -90,6 +75,5 @@ by this software. There can be bugs and the bot may not perform as expected
 or specified. Please consider testing it first with paper trading and/or
 backtesting on historical data. Also look at the code to see what how
 it is working.
-*/
 
-    
+*/
