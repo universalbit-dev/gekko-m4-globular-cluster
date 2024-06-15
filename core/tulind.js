169d73a34e0615efbcb9a4ee45582815664d9048
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-16 01:58:09 +0200

    Add files via upload

--- core/tulind.js
+++ core/tulind.js
@@ -1,9 +1,18 @@
-var semver = require('semver');
-const _ = require('lodash');
+var semver = require("semver");
+var _ = require('lodash');
 
+// validate that talib is installed, if not we'll throw an exception which will
+// prevent further loading or out outside this module
+try {
+    var tulind = require("tulind");
+} catch (e) {
+    module.exports = null;
+    return;
+}
 
-var tulind= require ('tulind');
 var tulindError = 'Gekko was unable to configure Tulip Indicators:\n\t';
+
+// Wrapper that executes a tulip indicator
 var execute = function(callback, params) {
     var tulindCallback = function(err, result) {
         if (err) return callback(err);
@@ -990,10 +999,4 @@ methods.zlema = {
 }
 
 module.exports = methods;
-/*
-The MIT License (MIT)
-Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+
