834541bea15c39b7d06a0cff53593c6101341bdc
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-16 21:32:54 +0200

    Update trade.config.js

--- trade.config.js
+++ trade.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps : [{
     name: 'rsibullbearadx',
     script    : 'gekko.js',
-    args      : '-c ecosystem/trade/rsibullbearadx.js',
-    name      : '|RSIBULLBEARADX|--realtime--|',
+    args      : '-c ecosystem/trade/trade_rsibullbearadx_simulator.js',
+    name      : '|RSIBULLBEARADX|--simulator--|',
     instances : "1",
     exec_mode : "cluster",
     env: {NODE_ENV: "development",},
