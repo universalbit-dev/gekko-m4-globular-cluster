0c1dee480890169d904284ebefde0402353132b2
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-16 12:25:21 +0200

    Add files via upload

--- ecosystem/backtest/backtest_nntma.js
+++ ecosystem/backtest/backtest_nntma.js
@@ -2,26 +2,31 @@ var config = {};
 //General Settings
 config.debug =true;
 
-config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:30};
-//Trading Advisor
-config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'NNTMA'};
+config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:60};
 
 //optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 , 610 , 987
-config.NNTMA={threshold_buy:1,threshold_sell:-1,method:'adadelta',learning_rate:0.01,momentum:0.0,l1_decay:0.001,l2_decay:0.001,threshold:1,price_buffer_len:987,min_predictions:233,hodl_threshold:1,scale:1,batch_size:1};
+//Strategies
+config.NNTMA={
+threshold_buy:1,threshold_sell:-1,method:'adadelta',learning_rate:0.01,momentum:0.0,
+l1_decay:0.001,l2_decay:0.001,threshold:1,price_buffer_len:987,min_predictions:144,
+hodl_threshold:1,scale:1,batch_size:1,constant:0.015,history:89,
+short:10,medium:45,long:80};
+
+//Trading Advisor
+config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'NNTMA'};
 
 //Date.prototype.toISOString()
 //Previous Month
 var previous_month = new Date();
 previous_month.setDate(1);
 previous_month.setMonth(previous_month.getMonth()-1);
-previous_month.setDate(2); 
+previous_month.setDate(4);
 
 //Current Month
 var current_month = new Date();
 current_month.setDate(1);
 current_month.setMonth(current_month.getMonth());
-current_month.setDate(2); 
-
+current_month.setDate(4);
 
 //Backtest Exchange Data  FROM previous month TO current month
 config.backtest = {enabled:true,
@@ -57,12 +62,14 @@ config.paperTrader = {enabled: true,reportInCurrency: true,
 //Performance Analyzer
 config.performanceAnalyzer = {enabled: true,riskFreeReturn: 5};
 
-//Import                       
+//Import
 config.importer = {enabled:false}
 
 config['I understand that Gekko only automates MY OWN trading strategies']=true;
 module.exports = config;
+
 /*
+
 The MIT License (MIT)
 Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -75,4 +82,5 @@ by this software. There can be bugs and the bot may not perform as expected
 or specified. Please consider testing it first with paper trading and/or
 backtesting on historical data. Also look at the code to see what how
 it is working.
+
 */
