ebb0ccb01a330815b227cb0c934b6518c814c915
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-17 17:23:13 +0200

    Update backtest_dema.js

--- ecosystem/backtest/backtest_dema.js
+++ ecosystem/backtest/backtest_dema.js
@@ -5,7 +5,7 @@ config.debug =true;
 config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:60};
 
 //optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 , 610 , 987
-config.DEMA={SMA:50,DEMA:1};
+config.DEMA={SMA:55,DEMA:1};
 config.DEMA.thresholds={down:-0.000001,up:0.000001};
 
 //Trading Advisor
