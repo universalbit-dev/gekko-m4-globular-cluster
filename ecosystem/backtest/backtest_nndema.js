2bd04f483dc4032c2b1491e158d1fa7819e5c927
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-19 22:41:51 +0200

    Add files via upload

--- ecosystem/backtest/backtest_nndema.js
+++ ecosystem/backtest/backtest_nndema.js
@@ -1,16 +1,14 @@
 var config = {};
 //General Settings
 config.debug =true;
-
 config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:60};
 
+//TradingAdvisor
+config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'NNDEMA'};//50minute
 //optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 , 610 , 987
-config.NNDEMA={threshold_buy:1,threshold_sell:-1,method:'adadelta',learning_rate:0.01,momentum:0.0,
-l1_decay:0.001,l2_decay:0.001,threshold:1,price_buffer_len:987,min_predictions:233,hodl_threshold:1,scale:1,batch_size:1};
-
-//Trading Advisor
-config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'NNDEMA'};
-
+config.NNDEMA={threshold_buy:0.1,threshold_sell:-0.1,method:'adadelta',learning_rate:0.01,momentum:0.0,
+l1_decay:0.001,l2_decay:0.001,threshold:1,price_buffer_len:987,min_predictions:233,hodl_threshold:1,scale:1,batch_size:1,RSI:13,DEMA:1,SMA:5,SMMA:5,StopLoss:3};
+config.NNDEMA.thresholds={down:-0.000001,up:0.000001};
 
 //Date.prototype.toISOString()
 //Previous Month
