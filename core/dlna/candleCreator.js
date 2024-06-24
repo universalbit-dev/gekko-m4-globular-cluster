4f69d717773bb3043fec966454f4d4e118f4b052
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-24 22:16:08 +0200

    Add files via upload

--- core/dlna/candleCreator.js
+++ core/dlna/candleCreator.js
@@ -1,8 +1,24 @@
 /*
+//CandleCreator creates candles based on trade batches.
+...
+    var second = trade.date.format('YYYY-MM-DD HH:mm:ss');
+    // create a string referencing the seconds of this trade happened 
+    var lastSecond = this.lastTrade.date.format('YYYY-MM-DD HH:mm:ss');
+...
 
+==> gekko-m4 expects a candle every 300 second, if nothing happened during 300 seconds will add empty candle.
+==> and we have a candle for 300 seconds
+
+
+//TradeBatcher date format
+...
+first.date.format('YYYY-MM-DD HH:mm:ss');
+last.date.format('YYYY-MM-DD HH:mm:ss');
+...
+
+==> tradeBatcher date format (seconds) 
 */
 
-//The CandleCreator creates candles based on trade batches.
 
 const _ = require('../lodash3');require('lodash-migrate');
 var moment = require('moment');
@@ -17,9 +33,7 @@ util.makeEventEmitter(CandleCreator);
 
 CandleCreator.prototype.write = function(batch) {
   var trades = batch.data;
-  if(_.isEmpty(trades))
-    return;
-
+  if(_.isEmpty(trades))return;
   trades = this.filter(trades);
   this.fillBuckets(trades);
   var candles = this.calculateCandles();
@@ -54,17 +68,14 @@ CandleCreator.prototype.fillBuckets = function(trades) {
 CandleCreator.prototype.calculateCandles = function() {
   var seconds = _.size(this.buckets);
   if (this.lastTrade !== undefined)
-    // create a string referencing the minute this trade happened in
-    var lastSecond = this.lastTrade.date.format('YYYY-MM-DD HH:mm:ss');
+  // create a string referencing the seconds of this trade happened 
+  var lastSecond = this.lastTrade.date.format('YYYY-MM-DD HH:mm:ss');
 
-  var candles = _.map(this.buckets, function(bucket, name) {
-    var candle = this.calculateCandle(bucket);
-    if(name !== lastSecond)
-      delete this.buckets[name];
-
-    return candle;
+  var candles = _.map(this.buckets, function(bucket, name) 
+  {
+  var candle = this.calculateCandle(bucket);
+  if(name !== lastSecond)delete this.buckets[name];return candle;
   }, this);
-
   return candles;
 }
 
@@ -89,20 +100,16 @@ CandleCreator.prototype.calculateCandle = function(trades) {
     candle.volume += f(trade.amount);
     candle.vwp += f(trade.price) * f(trade.amount);
   });
-
   candle.vwp /= candle.volume;
-
   return candle;
 }
 
-// Gekko expects a candle every 30 second, if nothing happened
-// during 30 seconds Gekko will add empty candles with:
+//expects a candle every 300 second, if nothing happened during 300 seconds will add empty candle.
 CandleCreator.prototype.addEmptyCandles = function(candles) {
   var amount = _.size(candles);
-  if(!amount)
-    return candles;
+  if(!amount)return candles;
 
-// iterator
+//iterator
   var start = _.first(candles).start.clone();
   var end = _.last(candles).start;
   var i, j = -1;
@@ -112,12 +119,12 @@ CandleCreator.prototype.addEmptyCandles = function(candles) {
   });
 
   while(start < end) {
-    start.add(60, 's');
+    start.add(300, 's');
     i = +start;
     j++;
 
     if(_.contains(seconds, i))
-      continue; // we have a candle for 60 seconds
+      continue; // we have a candle for 300 seconds
 
     var lastPrice = candles[j].close;
 
@@ -138,9 +145,11 @@ CandleCreator.prototype.addEmptyCandles = function(candles) {
 module.exports = CandleCreator;
 
 /*
+
 The MIT License (MIT)
 Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 */
