c6cff37b1971795ebfbb81ad4eb253f4d460cf76
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-25 17:43:31 +0200

    Add files via upload

--- strategies/SUPERTREND.js
+++ strategies/SUPERTREND.js
@@ -2,7 +2,15 @@ require('../core/tulind');
 var log = require('../core/log.js');
 var async = require('async');
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-async function wait() {console.log('keep calm and make something of amazing');await sleep(60000);};
+
+/* async fibonacci sequence */
+var fibonacci_sequence=['0','1','1','2','3','5','8','13','21','34','55','89','144','233','377','610','987','1597','2584','4181'];
+var sequence = ms => new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * fibonacci_sequence.length)));
+async function sequence() {console.log('');await sequence;};
+
+/* async keep calm and make something of amazing */ 
+var keepcalm = ms => new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * fibonacci_sequence.length)));
+async function amazing() {console.log('keep calm and make something of amazing');await keepcalm;};
 
 function AuxiliaryIndicators(){
    var directory = 'indicators/';
@@ -13,6 +21,12 @@ function AuxiliaryIndicators(){
        log.debug('added', auxiliaryindicators);
    }
  }
+
+function makeoperators() {
+var operator = ['==','===','!=','&&','<=','>=','>','<','||','='];
+var result = Math.floor(Math.random() * operator.length);
+console.log("\t\t\t\tcourtesy of... "+ operator[result]);
+}
 
 var method = {
 
@@ -66,14 +80,12 @@ check : function(candle) {
   }
 
   if(candle.close > this.supertrend.supertrend && this.bought == 0){
-    this.advice("long");
-    this.bought = 1;
+    this.advice("long");makeoperators();amazing();this.bought = 1;
     log.debug("Buy at: ", candle.close);
   }
 
   if(candle.close < this.supertrend.supertrend && this.bought == 1){
-    this.advice("short")
-    this.bought = 0;
+    this.advice("short");makeoperators();amazing();this.bought = 0;
     log.debug("Sell at: ", candle.close);
   }
 
@@ -84,7 +96,7 @@ check : function(candle) {
     upperBand : this.supertrend.upperBand,
     lowerBand : this.supertrend.lowerBand,
     supertrend : this.supertrend.supertrend,
-  };
+  };sequence();
 }
 
 };
