a09c25880663f9c1c57c2e11c74e287012a1ea57
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-25 22:06:42 +0200

    Update backtest_rsibullbearadx.js

--- ecosystem/backtest/backtest_rsibullbearadx.js
+++ ecosystem/backtest/backtest_rsibullbearadx.js
@@ -8,7 +8,7 @@ config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'RSIBUL
 //# BULL/BEAR is defined by the longer SMA trends {Make sure your warmup period(tradingAdvisor.historySize) matches SMA_long}
 config.RSIBULLBEARADX={SMA_long:100,SMA_short:55,RSI:14,BULL_RSI:10,
 BULL_RSI_high:80,BULL_RSI_low:60,BEAR_RSI:15,BEAR_RSI_high:50,BEAR_RSI_low:20,
-BULL_MOD_high:5,BULL_MOD_low:-5,BEAR_MOD_high:15,BEAR_MOD_low:-5,ADX:3,ADX_high:70,ADX_low:50};
+BULL_MOD_high:5,BULL_MOD_low:-5,BEAR_MOD_high:15,BEAR_MOD_low:-5,ADX:3,ADX_high:70,ADX_low:50,STOPLOSS:3};
 
 //Date.prototype.toISOString()
 //Previous Month
