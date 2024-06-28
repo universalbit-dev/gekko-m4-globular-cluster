df55ec390a0120417bdc72a0b8b0f19e49b0d45f
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-28 17:45:21 +0200

    Update backtest_inverter.js

--- ecosystem/backtest/backtest_inverter.js
+++ ecosystem/backtest/backtest_inverter.js
@@ -5,6 +5,7 @@ config.debug =true;
 //import kraken exchange data
 config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:60};
 config.api={interval:900000}; //15 minutes
+config.expects={candle:900}; //900 seconds
 
 //Trading Advisor
 config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10};
