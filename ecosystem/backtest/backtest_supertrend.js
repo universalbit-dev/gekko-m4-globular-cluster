04cb9693a021796e9fa49a6635108eadff64a280
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-28 17:00:10 +0200

    Update backtest_supertrend.js

--- ecosystem/backtest/backtest_supertrend.js
+++ ecosystem/backtest/backtest_supertrend.js
@@ -2,6 +2,7 @@ var config = {};
 //General Settings
 config.debug =true;
 config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:60};
+config.api={interval:900000}; //15 minutes
 
 config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'SUPERTREND'};
 config.SUPERTREND={ATR:7,bandFactor:3,STOPLOSS:3};
