b27d1fa2f9948d41b6cfb6fc9e346422bb4c360b
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-07-03 16:07:01 +0200

    Add files via upload

--- strategies/INVERTER.js
+++ strategies/INVERTER.js
@@ -12,11 +12,13 @@ var async = require('async');
 /* async fibonacci sequence */
 var fibonacci_sequence=['0','1','1','2','3','5','8','13','21','34','55','89','144','233','377','610','987','1597','2584','4181'];
 var sequence = ms => new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * fibonacci_sequence.length)));
-async function sequence() {console.log('keep calm and make something of amazing');;await sequence;};
+async function sequence() {console.log('');await sequence;
+};
 
 /* async keep calm and make something of amazing */
 var keepcalm = ms => new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * fibonacci_sequence.length) / Math.floor(Math.random() * fibonacci_sequence.length - 1)));
-async function amazing() {console.log('keep calm and make something of amazing');await keepcalm;};
+async function amazing() {console.log('keep calm and make something of amazing');await keepcalm;
+};
 
 function AuxiliaryIndicators(){
    var directory = 'indicators/';
--- strategies/RSIBULLBEARADX.js
+++ strategies/RSIBULLBEARADX.js
@@ -9,11 +9,13 @@ var settings = config.RSIBULLBEARADX;this.settings=settings;
 /* async fibonacci sequence */
 var fibonacci_sequence=['0','1','1','2','3','5','8','13','21','34','55','89','144','233','377','610','987','1597','2584','4181'];
 var sequence = ms => new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * fibonacci_sequence.length)));
-async function sequence() {console.log('keep calm and make something of amazing');;await sequence;};
+async function sequence() {console.log('');await sequence;
+};
 
 /* async keep calm and make something of amazing */
 var keepcalm = ms => new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * fibonacci_sequence.length) / Math.floor(Math.random() * fibonacci_sequence.length - 1)));
-async function amazing() {console.log('keep calm and make something of amazing');await keepcalm;};
+async function amazing() {console.log('keep calm and make something of amazing');await keepcalm;
+};
 
 function AuxiliaryIndicators(){
    var directory = 'indicators/';
