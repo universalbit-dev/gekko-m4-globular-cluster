ae0878fd696750a86b9c32529f2181b0141a25e7
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-07-03 19:14:51 +0200

    Update backtest_stochrsi.js

--- ecosystem/backtest/backtest_stochrsi.js
+++ ecosystem/backtest/backtest_stochrsi.js
@@ -2,8 +2,8 @@ var config = {};
 //General Settings
 config.debug =true;
 config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:60};
-config.api={interval:900000}; //15 minutes
-config.expects={candle:900}; //900 seconds
+config.api={interval:780000}; //13 minutes
+config.expects={candle:987}; //987 seconds
 
 config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'STOCHRSI'};
 config.STOCHRSI={interval:10,STOPLOSS:3,high:70,low:30,persistence:5,RSI:21,STOCH:21};
