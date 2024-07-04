333990087b9afe872099900674a333eb7b8ba72c
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-07-04 14:25:10 +0200

    Create trade_stochrsi_simulatori.js

---
+++ ecosystem/trade/trade_stochrsi_simulatori.js
@@ -0,0 +1,44 @@
+var config = {};
+config.debug =true;
+config.watch = {exchange:'exchangesimulator',currency:'BTC',asset:'LTC',tickrate:60};
+//Tick rate is the frequency of information
+
+config.trader={enabled:false,exchange:'exchangesimulator',currency:'BTC',asset:'LTC',key:'',secret:'',tickrate:60};
+
+config.api={interval:780000}; //13 minutes
+config.expects={candle:987}; //987 seconds
+
+config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'STOCHRSI'};
+config.STOCHRSI={interval:10,STOPLOSS:3,high:70,low:30,persistence:5,RSI:21,STOCH:21,rl:0.3};
+
+config.adapter='sqlite';config.adapter.path= 'plugins/sqlite';
+config.sqlite = {path: 'plugins/sqlite',dataDirectory: 'history',version:'5.1.1',
+dependencies:[{module: 'sqlite3',version:'5.1.7'}] };
+config.candleWriter={enabled:true,adapter:'sqlite'};
+config.adviceLogger={enabled:true};
+config.backtest = {enabled:true};
+config.backtestResultExporter = {enabled: false};
+config.paperTrader = {enabled: true,reportInCurrency: true,simulationBalance: {asset: 0.00118267,currency: 1},feeMaker: 0.1,feeTaker: 0.1,feeUsing: 'maker',slippage: 0.05};
+//[under review papertrader reportInCurrency (asset:currency)]
+
+config.performanceAnalyzer = {enabled: true,riskFreeReturn: 5};
+config.importer = {enabled:false};
+config['I understand that Gekko only automates MY OWN trading strategies']=true;
+module.exports = config;
+
+/*
+
+The MIT License (MIT)
+Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+Disclaimer:
+                              USE AT YOUR OWN RISK!
+The author of this project is NOT responsible for any damage or loss caused
+by this software. There can be bugs and the bot may not perform as expected
+or specified. Please consider testing it first with paper trading and/or
+backtesting on historical data. Also look at the code to see what how
+it is working.
+
+*/
