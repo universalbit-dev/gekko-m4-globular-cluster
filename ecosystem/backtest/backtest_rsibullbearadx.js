950897785bcbad3c9818014c0b2fe30c7364fcb0
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-07-05 01:02:58 +0200

    Update backtest_rsibullbearadx.js

--- ecosystem/backtest/backtest_rsibullbearadx.js
+++ ecosystem/backtest/backtest_rsibullbearadx.js
@@ -5,14 +5,12 @@ config.debug =true;
 config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:60};
 config.api={interval:780000}; //13 minutes
 config.expects={candle:987}; //987 seconds
-/*profit approximation */
-config.rl=0.3;
 
 config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'RSIBULLBEARADX'};
 //# BULL/BEAR is defined by the longer SMA trends {Make sure your warmup period(tradingAdvisor.historySize) matches SMA_long}
 config.RSIBULLBEARADX={SMA_long:100,SMA_short:55,RSI:14,BULL_RSI:10,
 BULL_RSI_high:80,BULL_RSI_low:60,BEAR_RSI:15,BEAR_RSI_high:50,BEAR_RSI_low:20,
-BULL_MOD_high:5,BULL_MOD_low:-5,BEAR_MOD_high:15,BEAR_MOD_low:-5,ADX:3,ADX_high:70,ADX_low:50,STOPLOSS:3};
+BULL_MOD_high:5,BULL_MOD_low:-5,BEAR_MOD_high:15,BEAR_MOD_low:-5,ADX:3,ADX_high:70,ADX_low:50,STOPLOSS:3,rl:03};
 
 //Date.prototype.toISOString()
 //Previous Month
