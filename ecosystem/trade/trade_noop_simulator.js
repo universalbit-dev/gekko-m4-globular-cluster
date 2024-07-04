a8e9e2975b80bd930097ab97574b7557fba0e283
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-07-04 17:25:45 +0200

    Update trade_noop_simulator.js

--- ecosystem/trade/trade_noop_simulator.js
+++ ecosystem/trade/trade_noop_simulator.js
@@ -1,14 +1,15 @@
 var config = {};
 config.debug =true;
 config.watch = {exchange:'exchangesimulator',currency:'BTC',asset:'LTC',tickrate:60};
-config.trader={enabled:false,exchange:'exchangesimulator',currency:'BTC',asset:'LTC',key:'',secret:'',tickrate:60};
+//Tick rate is the frequency of information
+
+config.trader={enabled:false,
+exchange:'exchangesimulator',currency:'BTC',asset:'LTC',key:'',secret:'',tickrate:60};
+config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'NOOP'};
 
 config.api={interval:780000}; //13 minutes
 config.expects={candle:987}; //987 seconds
 
-config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'NOOP'};
-
-//optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 , 610 , 987
 config.NOOP={};
 
 config.adapter='sqlite';config.adapter.path= 'plugins/sqlite';
@@ -16,15 +17,18 @@ config.sqlite = {path: 'plugins/sqlite',dataDirectory: 'history',version:'5.1.1'
 dependencies:[{module: 'sqlite3',version:'5.1.7'}] };
 config.candleWriter={enabled:true,adapter:'sqlite'};
 config.adviceLogger={enabled:true};
-config.backtest = {enabled:false};
+config.backtest = {enabled:true};
 config.backtestResultExporter = {enabled: false};
-config.paperTrader = {enabled: false};
+config.paperTrader = {enabled: true,reportInCurrency: true,simulationBalance: {asset: 0.00118267,currency: 1},feeMaker: 0.1,feeTaker: 0.1,feeUsing: 'maker',slippage: 0.05};
+//[under review papertrader reportInCurrency (asset:currency)]
+
 config.performanceAnalyzer = {enabled: true,riskFreeReturn: 5};
 config.importer = {enabled:false};
 config['I understand that Gekko only automates MY OWN trading strategies']=true;
 module.exports = config;
 
 /*
+
 The MIT License (MIT)
 Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -37,4 +41,5 @@ by this software. There can be bugs and the bot may not perform as expected
 or specified. Please consider testing it first with paper trading and/or
 backtesting on historical data. Also look at the code to see what how
 it is working.
+
 */
