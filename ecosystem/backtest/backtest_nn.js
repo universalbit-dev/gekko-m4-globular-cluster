fac8e15f8b806b7b232dd3912c70bd8a8a4c320d
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-07-04 12:54:07 +0200

    Update backtest_nn.js

--- ecosystem/backtest/backtest_nn.js
+++ ecosystem/backtest/backtest_nn.js
@@ -10,9 +10,11 @@ config.rl=0.3;
 
 //TradingAdvisor
 config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'NN'};//50minute
+
 //optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 , 610 , 987
 config.NN={threshold_buy:0.01,threshold_sell:-0.01,method:'adadelta',learning_rate:0.01,momentum:0.0,
-l1_decay:0.001,l2_decay:0.001,price_buffer_len:987,min_predictions:144,hodl_threshold:1,scale:1,batch_size:1,RSI:13,DEMA:1,SMMA:5,STOPLOSS:3};
+l1_decay:0.001,l2_decay:0.001,price_buffer_len:987,min_predictions:144,hodl_threshold:1,scale:1,batch_size:1,RSI:13,DEMA:1,SMMA:5,STOPLOSS:3,rl:0.3,high:70,low:30};
+
 
 //Date.prototype.toISOString()
 //Previous Month
