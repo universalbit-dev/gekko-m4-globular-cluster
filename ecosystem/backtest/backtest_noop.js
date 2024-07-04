92d6f697b819014951b31ae963769cd562304281
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-07-05 00:54:32 +0200

    Update backtest_noop.js

--- ecosystem/backtest/backtest_noop.js
+++ ecosystem/backtest/backtest_noop.js
@@ -5,8 +5,6 @@ config.debug =true;
 config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:60};
 config.api={interval:780000}; //13 minutes
 config.expects={candle:987}; //987 seconds
-/*profit approximation */
-config.rl=0.3;
 
 //optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377
 //Strategies
