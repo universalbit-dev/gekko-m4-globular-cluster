76c42ddb65e37a5323ebcda5b50dcddbd2f75847
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-07-04 13:42:10 +0200

    Update trade.config.js

--- trade.config.js
+++ trade.config.js
@@ -1,12 +1,70 @@
 module.exports = {
-  apps : [{
-    name: 'rsibullbearadx',
+  apps : [
+  {
+    name: 'inverter',
     script    : 'gekko.js',
-    args      : '-c ecosystem/trade/trade_rsibullbearadx_simulator.js',
-    name      : '|RSIBULLBEARADX|--simulator--|',
+    args      : '-c ecosystem/trade/trade_inverter_simulator.js',
+    name      : '|INVERTER|-simulator-|',
     instances : "1",
     exec_mode : "cluster",
     env: {NODE_ENV: "development",},
     env_production: {NODE_ENV: "production",}
-  }]
+  },
+
+  {
+  name: 'stochrsi',
+  script    : 'gekko.js',
+  args      : '-c globular/trade/trade_stochrsi_simulator.js',
+  name      : '|STOCHRSI|-simulator-|',
+  instances : "1",
+  exec_mode : "cluster"
+  },
+  
+  {
+  name: 'dema',
+  script    : 'gekko.js',
+  args      : '-c ecosystem/trade/trade_dema_simulator.js',
+  name      : '|DEMA|-simulator-|',
+  instances : "1",
+  exec_mode : "cluster"
+  },
+  
+  {
+  name: 'nn',
+  script    : 'gekko.js',
+  args      : '-c ecosystem/trade/trade_nn_simulator.js',
+  name      : '|NN|-simulator-|',
+  instances : "1",
+  exec_mode : "cluster"
+  },
+
+  {
+  name: 'supertrend',
+  script    : 'gekko.js',
+  args      : '-c ecosystem/trade/trade_supertrend_simulator.js',
+  name      : '|SUPERTREND|-simulator-|',
+  instances : "1",
+  exec_mode : "cluster"
+  },
+
+  {
+  name: 'rsibullbearadx',
+  script    : 'gekko.js',
+  args      : '-c ecosystem/trade/trade_rsibullbearadx_simulator.js',
+  name      : '|RSIBULLBEARADX|-simulator-|',
+  instances : "1",
+  exec_mode : "cluster"
+  },
+
+  {
+  name: 'noop',
+  script    : 'gekko.js',
+  args      : '-c ecosystem/trade/trade_noop_simulator.js',
+  name      : '|NOOP|-simulator-|',
+  instances : "1",
+  exec_mode : "cluster"
+  }
+
+]
+
 }
