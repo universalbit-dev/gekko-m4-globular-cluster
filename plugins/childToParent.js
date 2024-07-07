d3a1b13b89fcf4006917484122239ed1805a7d3c
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-07-07 10:51:40 +0200

    Update childToParent.js

--- plugins/childToParent.js
+++ plugins/childToParent.js
@@ -2,14 +2,16 @@
 // them and sends it to the parent process.
 
 const log = require('../core/log');
-const _ = require('lodash');
+const _ = require('../core/lodash');
 const subscriptions = require('../subscriptions');
 const config = require('../core/util').getConfig();
+const fs= require('node:fs');
+const async=require('async');
+async.map(['adviceLogger.js','backtestResultExporter.js','childToParent.js','eventLogger.js'], fs.stat, function(err, results){_.noop;});
 
 const ChildToParent = function() {
 
   subscriptions
-    // .filter(sub => config.childToParent.events.includes(sub.event))
     .forEach(sub => {
       this[sub.handler] = (event, next) => {
         process.send({type: sub.event, payload: event});
