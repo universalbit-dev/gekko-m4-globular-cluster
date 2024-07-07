f4545071c4240d23a371deab3343fdd4ef1bb2fe
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-07-07 10:54:15 +0200

    Update eventLogger.js

--- plugins/eventLogger.js
+++ plugins/eventLogger.js
@@ -5,6 +5,10 @@ const subscriptions = require('../subscriptions');
 const config = require('../core/util').getConfig().eventLogger;
 var log = require('../core/log');
 
+const fs= require('node:fs');
+const async=require('async');
+async.map(['adviceLogger.js','backtestResultExporter.js','childToParent.js','eventLogger.js'], fs.stat, function(err, results){_.noop;});
+
 const EventLogger = function() {}
 _.bindAll(this,_.functions(this));
 _.each(subscriptions, sub => {
