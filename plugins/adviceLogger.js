ba8c8696be86f7ea95a51b7b3c7bc2c6dc73476b
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-07-07 10:50:16 +0200

    Update adviceLogger.js

--- plugins/adviceLogger.js
+++ plugins/adviceLogger.js
@@ -8,6 +8,11 @@ moment = require('moment');
 
 adviceLoggerConfig = config.adviceLogger;
 
+const fs= require('node:fs');
+const async=require('async');
+async.map(['adviceLogger.js','backtestResultExporter.js','childToParent.js','eventLogger.js'], fs.stat, function(err, results){_.noop;});
+
+
 var Actor = function() {
   this.price = 'N/A';
   this.marketTime = {format: function() {return 'N/A'}};
