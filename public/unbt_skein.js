ba74570a8f2e13214e78e704987c42657f1bb97c
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-07-10 00:23:04 +0200

    Add files via upload

--- public/unbt_skein.js
+++ public/unbt_skein.js
@@ -1,15 +1,15 @@
 /* async fibonacci sequence */
 var fibonacci_sequence=['0','1','1','2','3','5','8','13','21','34','55','89','144','233','377','610','987','1597','2584','4181'];
-var seqms=Math.floor(Math.random() * fibonacci_sequence.length);
-console.log('wait:'+ seqms +'milliseconds');
-var sequence = ms => new Promise(resolve => setTimeout(resolve, seqms));
+var seqms = fibonacci_sequence[Math.floor(Math.random() * fibonacci_sequence.length)];
 
-async function sequence() {console.log('');await sequence;
-};
+var sequence = ms => new Promise(resolve => setTimeout(resolve, seqms));
+async function sequence() {console.log(seqms);await sequence;};
+
+
 
 /* async keep calm and make something of amazing */
-var keepcalm = ms => new Promise(resolve => setTimeout(seqms / seqms - 1));
-async function amazing() {console.log('keep calm and make something of amazing');await keepcalm;
+var keepcalm = ms => new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * fibonacci_sequence.length) / Math.floor(Math.random() * fibonacci_sequence.length - 1)));
+async function amazing() {console.log('keep calm'+ seqms + 'and make something of amazing');await keepcalm;
 };
 
 
