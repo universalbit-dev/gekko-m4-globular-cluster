a5d0de57d072ff8ea9697877e784a06ca31ef257
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-07-10 00:12:50 +0200

    Add files via upload

--- public/unbt_skein.js
+++ public/unbt_skein.js
@@ -1,36 +1,28 @@
 /* async fibonacci sequence */
 var fibonacci_sequence=['0','1','1','2','3','5','8','13','21','34','55','89','144','233','377','610','987','1597','2584','4181'];
-var sequence = ms => new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * fibonacci_sequence.length)));
+var seqms=Math.floor(Math.random() * fibonacci_sequence.length);
+console.log('wait:'+ seqms +'milliseconds');
+var sequence = ms => new Promise(resolve => setTimeout(resolve, seqms));
+
 async function sequence() {console.log('');await sequence;
 };
 
 /* async keep calm and make something of amazing */
-var keepcalm = ms => new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * fibonacci_sequence.length) / Math.floor(Math.random() * fibonacci_sequence.length - 1)));
+var keepcalm = ms => new Promise(resolve => setTimeout(seqms / seqms - 1));
 async function amazing() {console.log('keep calm and make something of amazing');await keepcalm;
 };
-var unbt_skein={};
 
-function bitnodes(){
+
+async function bitnodes(){
 //https://bitnodes.io/dashboard/
 const bitcoin= require('bitcoin');
 const fs= ('node:fs');
 
-fetch('https://bitnodes.io/api/v1/snapshots/latest/')
+  fetch('https://bitnodes.io/api/v1/snapshots/latest/')
   .then(response => response.json())
   .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));  
+  .catch(error => console.error('Error:', error));
 }
+
 bitnodes();sequence();amazing();
 
-
-module.exports= unbt_skein;
-
-/*
-Gekko M4 The MIT License (MIT) Copyright (c) 2024 UniversalBit BlockChain
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. https://en.wikipedia.org/wiki/MIT_License
-*/
