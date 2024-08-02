b826bd64603a3fff8599cc1c298e280a8dcecdce
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-08-02 10:06:46 +0200

    Update backtest_nn.js

--- ecosystem/backtest/backtest_nn.js
+++ ecosystem/backtest/backtest_nn.js
@@ -5,15 +5,13 @@ config.debug =true;
 config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:60}; //frequency information [3 - 10 - 20 - 60]
 config.api={interval:780000}; //13 minutes
 config.expects={candle:987}; //987 seconds
-/*profit approximation */
-config.rl=0.3;
 
 //TradingAdvisor
 config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'NN'};//50minute
 
 //optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 , 610 , 987
 config.NN={threshold_buy:0.8,threshold_sell:-0.8,method:'adadelta',learning_rate:0.01,momentum:0.0,
-l1_decay:0.001,l2_decay:0.001,price_buffer_len:987,min_predictions:144,hodl_threshold:1,scale:1,batch_size:1,RSI:13,DEMA:1,SMMA:5,STOPLOSS:3,rl:0.3,high:70,low:30};
+l1_decay:0.001,l2_decay:0.001,price_buffer_len:987,min_predictions:144,hodl_threshold:1,scale:1,batch_size:1,RSI:13,DEMA:1,SMMA:5,STOPLOSS:3,rl:0.3,high:70,low:30,rl:0.3};
 
 
 //Date.prototype.toISOString()
