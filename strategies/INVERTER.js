8c23f04167c4a5fbc23f67e1e7afcd999b478952
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-08-07 08:49:18 +0200

    Add files via upload

--- strategies/INVERTER.js
+++ strategies/INVERTER.js
@@ -40,12 +40,7 @@ this.RSIhistory = [];
 AuxiliaryIndicators();
 this.name = 'INVERTER';
 log.info('Start' , this.name);
-this.trend = {
-    direction: 'none',
-    duration: 0,
-    state:'none' ,
-    bb:'none',
-};
+this.trend = {direction: 'none',duration: 0,state:'none',ls:'none'};
 
 this.debug = true;
 //optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 ,610 ,987
@@ -123,23 +118,27 @@ switch (true) {
 }
 //https://www.investopedia.com/ask/answers/121714/what-are-differences-between-divergence-and-convergence.asp
 	var diff = di_plus - di_minus;
-	if(diff > 0){this.trend.state = 'long';} else if(diff < 0 ){this.trend.state = 'short';}
+	switch(diff){
+	case diff > 0 :this.trend.state = 'long';break;
+	case diff < 0 :this.trend.state = 'short';break;
+	default:_.noop;
+	}
 
 	switch (true)
 	{
 	case (adxstrength == 'weak'):this.trend.direction = 'weak';break;
-	case ((adxstrength == 'strong')&&(this.trend.state !== 'long')):this.trend.direction = 'screw_up';this.trend.bb='long';this.short();rl=[];break;
-	case ((adxstrength == 'strong')&&(this.trend.state !== 'short')):this.trend.direction = 'screw_down';this.trend.bb='short';this.long();rl=[];break;
-	case ((adxstrength == 'verystrong')&&(this.trend.state !== 'long')):this.trend.direction = 'screw_up';this.trend.bb='long';this.short();rl=[];break;
-	case ((adxstrength == 'verystrong')&&(this.trend.state !== 'short')):this.trend.direction = 'screw_down';this.trend.bb='short';this.long();rl=[];break;
-	case ((adxstrength == 'extremestrong')&&(this.trend.state !== 'long')):this.trend.direction = 'screw_up';this.trend.bb='long';this.short();rl=[];break;
-	case ((adxstrength == 'extremestrong')&&(this.trend.state !== 'short')):this.trend.direction = 'screw_down';this.trend.bb='short';this.long();rl=[];break;
-	default:_.noop;this.trend.direction = 'none';
+	case ((adxstrength == 'strong')&&(this.trend.state !== 'long')):this.trend.direction = 'screw_up';this.trend.ls='long';this.short();rl=[];break;
+	case ((adxstrength == 'strong')&&(this.trend.state !== 'short')):this.trend.direction = 'screw_down';this.trend.ls='short';this.long();rl=[];break;
+	case ((adxstrength == 'verystrong')&&(this.trend.state !== 'long')):this.trend.direction = 'screw_up';this.trend.ls='long';this.short();rl=[];break;
+	case ((adxstrength == 'verystrong')&&(this.trend.state !== 'short')):this.trend.direction = 'screw_down';this.trend.ls='short';this.long();rl=[];break;
+	case ((adxstrength == 'extremestrong')&&(this.trend.state !== 'long')):this.trend.direction = 'screw_up';this.trend.ls='long';this.short();rl=[];break;
+	case ((adxstrength == 'extremestrong')&&(this.trend.state !== 'short')):this.trend.direction = 'screw_down';this.trend.ls='short';this.long();rl=[];break;
+	default:_.noop;
 	}
 	//trend moving down
-    if ((longema < shortema)&&(di_plus != undefined)&&(di_minus != undefined)){this.trend.bb ='short';}
+    if ((longema < shortema)&&(di_plus != undefined)&&(di_minus != undefined)){this.trend.ls ='short';}
     //trend moving up
-    else if ((longema > shortema)&&(di_plus != undefined)&&(di_minus != undefined)){this.trend.bb ='long';}
+    else if ((longema > shortema)&&(di_plus != undefined)&&(di_minus != undefined)){this.trend.ls ='long';}
     else _.noop;
     if ('stoploss' == this.indicators.stoploss.action){sequence();}
 },
@@ -148,12 +147,13 @@ switch (true) {
   long: function() {
     if (this.trend.direction !== 'screw_down')
     {
-    this.trend.bb='long';
+    this.trend.ls='long';
     this.trend.direction = 'screw_down';
     var buyprice = this.candle.low;
     var profit = rl.push(((candle.close - buyprice)/buyprice*100).toFixed(2));
     log.info('Calculated relative profit:',_.sumBy(rl, Number).toFixed(2));
 	}
+    
     if ((_.sumBy(rl, Number) > this.settings.rl)){this.advice('long');rl=[];comparisonoperators();}
     if (this.debug) log.info('uptrend');
   },
@@ -162,14 +162,15 @@ switch (true) {
   short: function() {
     if (this.trend.direction !== 'screw_up')
     {
-    this.trend.bb='short';
+    this.trend.ls='short';
     this.trend.direction = 'screw_up';
     var sellprice = this.candle.high;
-    var profit = rl.push(((candle.close - sellprice)/buyprice*100).toFixed(2));
+    var profit = rl.push(((candle.close - sellprice)/sellprice*100).toFixed(2));
     log.info('Calculated relative profit:',_.sumBy(rl, Number).toFixed(2));
     }
+    
     if ((_.sumBy(rl, Number) > this.settings.rl)){this.advice('short');rl=[];comparisonoperators();}
-    if (this.debug) log.info('downtrend');
+    if (this.debug){log.info('downtrend');}
   },
 
 end: function(){log.info('|The End|');}
