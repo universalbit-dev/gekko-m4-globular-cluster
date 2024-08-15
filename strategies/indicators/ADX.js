b2880ffdfd34f30671b1c3062dff682040e52af7
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-08-15 23:36:07 +0200

    Add files via upload

--- strategies/indicators/ADX.js
+++ strategies/indicators/ADX.js
@@ -1,43 +1,32 @@
-// Average Directional Movement Index indicator;
-// usable on gekko trading bot. Same license as gekko.
-// "ported" from tulip: https://tulipindicators.org/adx
-// gab0 - 2018
-require('../../core/tulind');
-const util=require('../../core/util');
-var DX = require('./DX.js');
+// required indicators
+var RMA = require('./RMA.js');
 
-var Indicator = function (period)
-{
-    this.input = 'candle';
-    this.indicates = 'trend_strength';
+var Indicator = function(config) {
+  this.input = 'candle';
 
-    this.dx = new DX(period);
+  this.ADX = new RMA(config.ADXLength);
+  this.ATR = new RMA(config.DILength);
+  this.DIM = new RMA(config.DILength);
+  this.DIP = new RMA(config.DILength);
 
-    this.result = 0;
-    this.periodRatio = (period - 1)/period;
-    this.initadx = 0;
-    this.initialized = 1;
-    this.period = period;
-};
-util.makeEventEmitter(Indicator);
-
-Indicator.prototype.update = function (candle)
-{
-    this.dx.update(candle);
-
-    if (this.initialized > this.period)
-        this.result = this.periodRatio * this.result + this.dx.result/this.period;
-    else if (this.initialized == this.period)
-    {
-        this.initialized++;
-        this.result = this.initadx / this.period;
-
-    } else if (this.dx.result)
-    {
-        this.initadx += this.dx.result;
-        this.initialized +=1;
-    }
-
-    this.age++
+  this.lastCandle = false;
 }
 
+Indicator.prototype.update = function(candle) {
+  if(!this.lastCandle) this.lastCandle = candle;
+
+  let up = candle.high - this.lastCandle.high;
+  let down = -(candle.low - this.lastCandle.low);
+  let plusDM = up > down && up > 0 ? up : 0;
+  let minusDM = down > up && down > 0 ? down : 0;
+  let tr = Math.max(candle.high - candle.low, Math.abs(candle.high - this.lastCandle.close), Math.abs(candle.low - this.lastCandle.close));
+  let trur = this.ATR.update(tr);
+  this.plus = 100 * this.DIP.update(plusDM) / (trur == 0 ? 1 : trur);
+  this.minus = 100 * this.DIM.update(minusDM) / (trur == 0 ? 1 : trur);
+  let sum = this.plus + this.minus;
+  this.result = 100 * this.ADX.update(Math.abs(this.plus - this.minus) / (sum == 0 ? 1 : sum));
+
+  this.lastCandle = candle;
+}
+
+module.exports = Indicator;
--- strategies/indicators/DMI.js
+++ strategies/indicators/DMI.js
@@ -1,27 +1,20 @@
 // DMI coded by Gab0 04/14/2018;
 //settings: period;
-//sidlines are equivalent of plus_dm and minus_dm from TALib
+//sidlines are equivalent of plus_dm and minus_dm
 //    should maybe be like plus_di and minus_di?
 // that case just change dm_up to di_up on this code, also with _down;
-require('../../core/tulind');
-const util= require('../../core/util');
 var ADX = require('./ADX');
 var Indicator = function (period) {
     this.input = 'candle';
     this.ADX = new ADX(period);
     this.age = 0;
-};
-util.makeEventEmitter(Indicator);
+}
 
 Indicator.prototype.update = function (candle) {
-
     this.ADX.update(candle);
-
     this.result = this.ADX.result;
     this.DIup = this.ADX.dx.dm_up;
     this.DIdown = this.ADX.dx.dm_down;
-
     this.age++;
 }
-
 module.exports = Indicator;
