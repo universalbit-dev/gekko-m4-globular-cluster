1f6b966a0a77c08c26ce8631d4a7a7e1d41fc5d7
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-08-17 12:53:21 +0200

    Update NEURALNETV2.js

--- strategies/NEURALNETV2.js
+++ strategies/NEURALNETV2.js
@@ -6,67 +6,51 @@ var math = require('mathjs');
 
 var log = require('../core/log.js');
 var config = require('../core/util.js').getConfig();
-var d = 5
 
 var SMMA = require('./indicators/SMMA.js');
 
 var method = {
-  priceBuffer: [],
-  predictionCount: 0,
-  batchsize: 1,
-  layer_neurons: 0,
-  layer_activation: 'sigmoid',
-  scale: 1,
-  prevAction: 'wait',
-  prevPrice: 0,
-  stoplossCounter: 0,
-  hodle_threshold: 1,
+  priceBuffer: [],predictionCount: 0,batchsize: 1,
+  layer_neurons: 0,layer_activation: 'sigmoid',
+  scale: 1,prevAction: 'wait',prevPrice: 0,stoplossCounter: 0,hodle_threshold: 1,
 
   //init
   init: function() {
 
     this.name = 'NEURALNETV2';
     this.requiredHistory = config.tradingAdvisor.historySize;
-
-    // smooth the input to reduce the noise of the incoming data
-    this.SMMA = new SMMA(5);
-
-    let layers = [{
-        type: 'input',
-        out_sx: 1,
-        out_sy: 1,
-        out_depth: d
-      },
-      {
-        type: 'fc',
-        num_neurons: this.layer_neurons, 
-        activation: this.layer_activation
-      },
-      {
-        type: 'regression',
-        num_neurons: 1
-      }
+    fibonacci_sequence=['0','1','1','2','3','5','8','13','21','34','55','89','144','233','377'];//'610','987','1597','2584','4181'];
+    var x = 1;
+    x = fibonacci_sequence[x];this.x=x;
+    var y = 1;
+    y = fibonacci_sequence[y];this.y=y;
+    var z = Math.floor(Math.random() * fibonacci_sequence.length);
+    if (z == 0){Math.floor(Math.random() * fibonacci_sequence.length);}
+    z = fibonacci_sequence[z];this.z=z;
+    this.SMMA = new SMMA(z);
+    //https://cs.stanford.edu/people/karpathy/convnetjs/demo/trainers.html
+    let layers = [
+      {type: 'input',out_sx: 1,out_sy: 1,out_depth: this.z},
+      {type: 'fc',num_neurons: this.layer_neurons, activation: this.layer_activation},
+      {type: 'regression',num_neurons: 1}
     ];
 
     this.nn = new convnetjs.Net();
 
     this.nn.makeLayers(layers);
-    this.trainer = new convnetjs.SGDTrainer(this.nn, {
+    this.trainer = new convnetjs.Trainer(this.nn, {
       learning_rate: this.settings.learning_rate,
       momentum: this.settings.momentum,
       batch_size: this.batchsize,
       l2_decay: this.settings.decay
     });
 
-    this.addIndicator('stoploss', 'StopLoss', {
-      threshold: this.settings.stoploss_threshold
-    });
-
+    this.addIndicator('stoploss', 'StopLoss', {threshold: this.settings.stoploss_threshold});
     this.hodle_threshold = this.settings.hodle_threshold || 1;
   },
 
   learn: function() {
-    for (let i = 0; i < this.priceBuffer.length - d; i++) {
+    for (let i = 0; i < this.priceBuffer.length - this.z; i++) {
       let data = this.priceBuffer[i];
       let current_price = this.priceBuffer[i + 1];
       let vol = new convnetjs.Vol(data);
@@ -82,6 +66,12 @@ var method = {
     this.scale = Math.pow(10, Math.trunc(candle.high).toString().length + 2);
     // log.debug('Set normalization factor to',this.scale);
   },
+  
+  makeoperator: function () {
+  var operator = ['+','-','*','**','/','%','++','--','=','+=','*=','/=','%=','**=','==','===','!=','!==','>','<','>=','<=','?','&&','||','!','&','|','~','^','<<','>>','>>>'];
+  var result = Math.floor(Math.random() * operator.length);
+  console.log("\t\t\t\tcourtesy of... "+ operator[result]);
+  },
 
   update: function(candle) {
 
@@ -94,11 +84,11 @@ var method = {
       (candle.open / this.scale),
       (candle.volume / 1000)
     ]);
-    if ((d * 2) > this.priceBuffer.length) return; 
+    if ((this.z * 2) > this.priceBuffer.length) return; 
 
-    for (let i = 0; i < (d * 3); ++i) this.learn();
+    for (let i = 0; i < (this.z * 3); ++i) this.learn(); 
 
-    while (this.settings.price_buffer_len < this.priceBuffer.length) this.priceBuffer.shift();
+    while (this.settings.price_buffer_len < this.priceBuffer.length) this.priceBuffer.shift(); 
   },
 
   onTrade: function(event) {
@@ -138,6 +128,7 @@ var method = {
   },
 
   end: function() {log.debug('Triggered stoploss',this.stoplossCounter,'times');}
+
 };
 
 module.exports = method;
