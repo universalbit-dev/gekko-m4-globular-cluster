ea022a7f66e658cca5be8202cd9e115190d37004
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-08-23 09:27:25 +0200

    Update SCALPER.js

--- strategies/SCALPER.js
+++ strategies/SCALPER.js
@@ -1,6 +1,6 @@
 var _ = require('lodash');
 var log = require('../core/log.js');
-
+const { Chess } = require('chess.js');
 var method = {};
 
 method.makeoperator = function() {
@@ -12,6 +12,7 @@ console.log("\t\t\t\tcourtesy of... "+ operator[result]);
 method.init = function() {
   this.name = 'SCALPER';
   this.addTulipIndicator('ps', 'psar', {optInAcceleration: 0.25,optInMaximum: 0.50});
+  this.addIndicator('stoploss', 'StopLoss', {threshold: this.settings.stoploss_threshold});
   this.candle_queue = [];
   this.is_buyin = false;
   this.price_buyin = 0;
@@ -29,6 +30,12 @@ method.update = function(candle) {
   if (this.candle_queue.length > 0) {candle.delta = candle.close - this.candle_queue[0].close;}
 }
 
+method.onTrade = function(event) {
+    if ('buy' === event.action) {this.indicators.stoploss.long(event.price);}
+    this.prevAction = event.action;
+    this.prevPrice = event.price;
+}
+
 var percent = 35;
 var distance = 3;
 var Period = 14;
@@ -42,7 +49,23 @@ IsReversalUp = function(min, candle) {
 }
 
 var MoveCycle = [];
+
+method.fxchess = function(){
+  const chess = new Chess()
+  while (!chess.isGameOver()) {
+  const moves = chess.moves()
+  const move = moves[Math.floor(Math.random() * moves.length)]
+  chess.move(move)
+}
+return console.log(chess.pgn())
+}
+
 method.check = function(candle) {
+  if ('buy' === this.prevAction && this.settings.stoploss_enabled && 'stoploss' === this.indicators.stoploss.action) 
+      {this.stoplossCounter++;log.debug('>>> STOPLOSS triggered <<<');this.advice();} /* */
+      
+  log.debug("Operator ");this.makeoperator();
+  log.debug("Random game of Chess");this.fxchess();
   if (this.candle_queue.length >= 15) {
     runningMin = 99999999;
     runninMax = 0;
@@ -55,9 +78,7 @@ method.check = function(candle) {
     Min.push(runningMin);
     for (var barsBack = Math.min(this.candle_queue.length, Period - 1); barsBack > 0; barsBack--) {
       var bar = this.candle_queue[barsBack];
-      if (bar.close >= runninMax) {
-        var runninMax = bar.close;
-      }
+      if (bar.close >= runninMax) {var runninMax = bar.close;}
     }
 
     var LowerLow = Min[Min.length - 1] > Min[0];
@@ -65,14 +86,12 @@ method.check = function(candle) {
     MoveCycle.push((this.candle.close - runningMin) / 100);
     var Downslow = MoveCycle[MoveCycle.length - 1] > MoveCycle[0];
 
-
     var c1 = this.candle_queue[this.candle_queue.length - 2];
     var TrueRange = Math.max(runninMax, c1.close) - Math.min(runningMin, c1.close);
     var valid = TrueRange / (candle.close - c1.close);
     var Range = 100 * ((valid - runningMin) / (runninMax - runningMin));
     MovingTR.push(valid);
     var MovingSlower = MovingTR[MovingTR.length - 2] > valid;
-
 
     if (CandeLow && !MovingSlower && valid > 0 && !this.is_buyin && this.candle.close > this.psar) {
       this.price_buyin = candle.close;
