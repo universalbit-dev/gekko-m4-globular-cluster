a3b7e56b3d43636ce7d576ca8efb80d7e74495da
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-08-25 15:48:37 +0200

    Add files via upload

---
+++ exchange/wrappers/api/kraken-api.js
@@ -0,0 +1,17 @@
+async function QueryPublicEndpoint(endPointName, inputParameters) {
+let jsonData;
+const baseDomain = "https://api.kraken.com";
+const publicPath = "/0/public/";
+const apiEndpointFullURL =
+baseDomain + publicPath + endPointName + "?" + inputParameters;
+
+await axios
+.get(apiEndpointFullURL)
+.then((res) => {
+jsonData = res;
+})
+.catch((err) => {
+jsonData = err;
+});
+return jsonData.data;
+}
