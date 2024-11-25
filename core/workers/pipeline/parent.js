d827852ec58b56daded884d778f177fafa5a3e13
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-11-25 23:55:52 +0100

    Add files via upload

--- core/workers/pipeline/parent.js
+++ core/workers/pipeline/parent.js
@@ -1,27 +1,36 @@
-var Promise = require("bluebird");const _ = Promise.promisify(require("underscore"));
-const util = require('../../core/util');
-const EventEmitter = Promise.promisifyAll(require("node:events"));
-class Child extends EventEmitter {};
-const child = new Child();
-
-const fork = require('child_process').fork;config = util.getConfig();
-util.makeEventEmitter(child);util.inherit(child, EventEmitter);
+var fork = require('child_process').fork;
 
 module.exports = (mode, config, callback) => {
   var debug = typeof v8debug === 'object';
-  if (debug) {process.execArgv = [];}
+  if (debug) {
+    process.execArgv = [];
+  }
+
   var child = fork(__dirname + '/child');
+
+  // How we should handle client messages depends
+  // on the mode of the Pipeline that is being ran.
   var handle = require('./messageHandlers/' + mode + 'Handler')(callback);
-  var message = {what: 'start',mode: mode,config: config};
-  const done = _.once(callback);
-  
+
+  var message = {
+    what: 'start',
+    mode: mode,
+    config: config
+  };
+
   child.on('message', function(m) {
-    if(m === 'ready') return child.send(message);
-    if(m === 'done') return child.send({what: 'exit'});
+    if(m === 'ready')
+      return child.send(message);
+
+    if(m === 'done')
+      return child.send({what: 'exit'});
+
     handle.message(m);
   });
+
   child.on('exit', handle.exit);
-  return child.send(message);
+
+  return child;
 }
 /*
 The MIT License (MIT)
