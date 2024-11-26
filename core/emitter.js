ce7ad20688f0d86df95437c6cc793f23679287ac
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-11-26 14:02:23 +0100

    Add files via upload

--- core/emitter.js
+++ core/emitter.js
@@ -1,38 +1,20 @@
-// Gekko uses a custom event emitter within the GekkoStream (the plugins) to guarantee
-// the correct order of events that are triggered by eachother. Turns sync events from
-// LIFO into a FIFO stack based model.
-//
-// More details here: https://forum.gekko.wizb.it/thread-56579.html
-
-// Gekko uses a custom event emitter within the GekkoStream (the plugins) to guarantee
-// the correct order of events that are triggered by eachother. Turns sync events from
-// LIFO into a FIFO stack based model.
-//
-// More details here: https://forum.gekko.wizb.it/thread-56579.html
-
-const util = require('util');
-const events = require('events');
-const NativeEventEmitter = events.EventEmitter;
+const util = require('./util');var Promise = require("bluebird");const _ = Promise.promisifyAll(require("underscore"));
+const {EventEmitter} = require("events");
+class Event extends EventEmitter{};
+const eventEmitter = new Event();
 
 const GekkoEventEmitter = function() {
-  NativeEventEmitter.call(this);
+  EventEmitter.call(this);
   this.defferedEvents = [];
+  _.bindAll(this, _.functions(this));
 }
-util.inherits(GekkoEventEmitter, NativeEventEmitter);
+util.makeEventEmitter(GekkoEventEmitter);util.inherit(GekkoEventEmitter, EventEmitter);
 
-// push to stack
-GekkoEventEmitter.prototype.deferredEmit = function(name, payload) {
-  this.defferedEvents.push({name, payload});
-}
-
-// resolve FIFO
+GekkoEventEmitter.prototype.deferredEmit = function(name, payload) {this.defferedEvents.push({name, payload});}
 GekkoEventEmitter.prototype.broadcastDeferredEmit = function() {
-  if(this.defferedEvents.length === 0)
-    return false;
-
+  if(this.defferedEvents.length === 0) return false;
   const event = this.defferedEvents.shift();
-
-  this.emit(event.name, event.payload);
+  eventEmitter.emit(event.name, event.payload);
   return true;
 }
 
