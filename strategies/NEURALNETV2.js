36d38c81916a032efc5a70c3e81e3ff1bb444ef5
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-11-27 20:57:16 +0100

    Add files via upload

--- strategies/NEURALNETV2.js
+++ strategies/NEURALNETV2.js
@@ -2,17 +2,16 @@
 // Source: https://github.com/RJPGriffin/
 
 /* universalbit-dev decentralized strategies */
-//const { addon: ov } = require('openvino-node');
-
+const { addon: ov } = require('openvino-node');
 var Promise = require("bluebird");
 var convnetjs = require('../core/convnet.js');
 var math = require('mathjs');
 const { Chess } = require('chess.js')
-var log = require('../core/log.js');var util=require('../core/util');
+var log = require('../core/log.js');
 var config = require('../core/util.js').getConfig();
 var tulind=require('../core/tulind');
 var SMMA = require('./indicators/SMMA.js');
-var config = util.getConfig();
+var config = require('../core/util.js').getConfig();
 var fs = require("fs-extra");
 
 var method = {
@@ -143,10 +142,10 @@ return console.log(chess.pgn())},
       {return Promise.promisifyAll(require("../exchange/wrappers/ccxt/ccxtOrdersSell.js"));this.advice('short');} /* */
       
       switch(Alpha.length != 0){
-      case (min < math.mean(min,max,median)):this.advice('buy');break;
-      case (max > math.mean(min,max,median)):this.advice('sell');break;
-      case (median < math.mean(min,max,median)):return Promise.promisifyAll(require("../exchange/wrappers/ccxt/ccxtOrdersBuy.js"));this.advice('long');break;/* */
-      case (median > math.mean(min,max,median)):return Promise.promisifyAll(require("../exchange/wrappers/ccxt/ccxtOrdersSell.js"));this.advice('short');break;/* */
+      case (min < math.mean(min,max,median)):this.advice('long');break;
+      case (max > math.mean(min,max,median)):this.advice('short');break;
+      case (median < math.mean(min,max,median)):return Promise.promisifyAll(require("../exchange/wrappers/ccxt/ccxtOrdersBuy.js"));this.advice('buy');break;/* */
+      case (median > math.mean(min,max,median)):return Promise.promisifyAll(require("../exchange/wrappers/ccxt/ccxtOrdersSell.js"));this.advice('sell');break;/* */
       default: log.info('');
       }
     }
