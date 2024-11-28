14ed3f919a74b959def36089b5be3925e6cebf0f
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-11-29 00:16:19 +0100

    Update example.js

--- exchange/example.js
+++ exchange/example.js
@@ -1,12 +1,10 @@
-const {EventEmitter}=require('events');
-class Event extends EventEmitter{};order=new Event();
+const {EventEmitter}=require('events');class Event extends EventEmitter{};
     const Broker = require('./gekkoBroker.js');
     const wohoo = new Broker({
       currency: 'BTC',asset: 'LTC',private: true,exchange: 'wohoo',key: 'APIKEY',secret: 'APISECRET' 
     });
  const type = 'sticky';const side = 'buy';const amount = 0.01;//set amount
     const order = wohoo.createOrder(type, side, amount);
-
     order.on('statusChange', s => console.log(now(), 'new status', s));
     order.on('fill', s => console.log(now(), 'filled', s));
     order.on('error', s => console.log(now(), 'error!', e));
@@ -17,3 +15,4 @@ class Event extends EventEmitter{};order=new Event();
         console.log(JSON.stringify(s, null, 2));
       });
     });
+
