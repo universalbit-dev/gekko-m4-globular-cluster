ff1b9b0792820c63b7b11e486842ed04477f5c23
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-11-30 01:38:44 +0100

    Add files via upload

--- .env/import/import.js
+++ .env/import/import.js
@@ -1,55 +1,30 @@
 var config = {};
 //General Settings
 config.debug =true;
-config.watch = {exchange: '',currency:'BTC',asset:'LTC',tickrate:60};
 
-//optInTimePeriod : Fibonacci Sequence 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 , 610 , 987
-//Strategies
-
-//Trading Advisor
-config.tradingAdvisor = {enabled:false};
-
+config.watch = {exchange: 'kraken',currency:'XBT',asset:'LTC',tickrate:20};
 //Date.prototype.toISOString()
+
 //Previous Month
 var previous_month = new Date();
 previous_month.setDate(1);
 previous_month.setMonth(previous_month.getMonth()-1);
 previous_month.setDate(2); 
+config.from=previous_month;
 
 //Current Month
 var current_month = new Date();
 current_month.setDate(1);
 current_month.setMonth(current_month.getMonth());
 current_month.setDate(2); 
+config.to=current_month;
 
 config.importer = {enabled:true,daterange:{from:previous_month,to:current_month}};
-
-//Backtest
-config.backtest = {enabled:false};
-
 //DataBase
 config.adapter='sqlite';config.adapter.path= 'plugins/sqlite';
 config.sqlite = {path: 'plugins/sqlite',dataDirectory: 'history',version:'5.1.1',
 dependencies:[{module: 'sqlite3',version:'5.1.7'}] };
-
-//Trader
-config.trader={enabled:false};
-
 //Candle Writer
-config.candleWriter={enabled:true,adapter:'sqlite'};
-
-//Advice Logger
-config.adviceLogger={enabled:false};
-
-//Export BackTest Result
-config.backtestResultExporter = {enabled: false};
-
-//PaperTrader
-config.paperTrader = {enabled: false};
-
-//Performance Analyzer
-config.performanceAnalyzer = {enabled: false};
-
 config.candleWriter={enabled:true,adapter:'sqlite'};
 config['I understand that Gekko only automates MY OWN trading strategies']=true;
 module.exports = config;
