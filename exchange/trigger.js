f0a47a17380c554a84f817cb2d1ffed2ac5248fd
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-12-06 16:06:35 +0100

    Add files via upload

--- exchange/trigger.js
+++ exchange/trigger.js
@@ -1,64 +1,17 @@
-// wraps around a low level trigger and feeds
-// it live market data.
-const {EventEmitter} = require('events');
-var Promise = require("bluebird");const _ = Promise.promisifyAll(require("underscore"));
-const util = require('../core/util.js');
-class Event extends EventEmitter {};
-var exchangeUtils=require("./exchangeUtils.js");
-
-var bindAll = exchangeUtils.bindAll;//**//
-var triggers=require("./triggers/trailingStop.js");
-
-// @param api: a gekko broker wrapper instance
-// @param type: type of trigger to wrap
-// @param props: properties to feed to trigger
-
-class Trigger extends Event{
-  constructor({api, type, props, onTrigger}) {
-    super();
-    EventEmitter.call(this);
-    this.onTrigger = onTrigger;
-    this.api = api;
-    this.isLive = true;
-    // note: we stay on the safe side and trigger
-    // as soon as the bid goes below trail.
-    this.tickerProp = 'bid';
-    if(!_.has(triggers, type)) {throw new Error('Gekko Broker does not know trigger ' + type);}
-
-    this.CHECK_INTERVAL = this.api.interval * 10;
-    bindAll(this);
-    this.trigger = new triggers[type]({
-      onTrigger: this.propogateTrigger,
-      ...props
-    })
-    this.scheduleFetch();
-  }
-
-  scheduleFetch() {this.timout = setTimeout(this.fetch, this.CHECK_INTERVAL);}
-
-  fetch() {
-    if(!this.isLive) {return;}
-    this.api.getTicker(this.processTicker)
-  }
-
-  processTicker(err, ticker) {
-    if(!this.isLive) {return;}
-    if(err) {return console.log('[GB/trigger] failed to fetch ticker:', err);}
-
-    this.price = ticker[this.tickerProp];
-    this.trigger.updatePrice(this.price);
-    this.scheduleFetch();
-  }
-
-  cancel() {this.isLive = false;clearTimeout(this.timout);}
-  propogateTrigger(payload) {
-    if(!this.isLive) {return;} this.isLive = false;this.onTrigger(payload);
-  }
-}
-util.makeEventEmitter(Trigger);
-
-module.exports = Trigger;
-
+let _=require('lodash');
+var exchangeUtils=require('./exchangeUtils');var bindAll=exchangeUtils.bindAll;var triggers=require('./triggers');class Trigger{constructor({api,type,props,onTrigger}){this.onTrigger=onTrigger;this.api=api;this.isLive=true;this.tickerProp='bid';if(!_.has(triggers,type)){throw new Error('Gekko Broker does not know trigger '+type);}
+this.CHECK_INTERVAL=this.api.interval*10;bindAll(this);this.trigger=new triggers[type]({onTrigger:this.propogateTrigger,...props})
+this.scheduleFetch();}
+scheduleFetch(){this.timout=setTimeout(this.fetch,this.CHECK_INTERVAL);}
+fetch(){if(!this.isLive){return;}
+this.api.getTicker(this.processTicker)}
+processTicker(err,ticker){if(!this.isLive){return;}
+if(err){return console.log('[GB/trigger] failed to fetch ticker:',err);}
+this.price=ticker[this.tickerProp];this.trigger.updatePrice(this.price);this.scheduleFetch();}
+cancel(){this.isLive=false;clearTimeout(this.timout);}
+propogateTrigger(payload){if(!this.isLive){return;}
+this.isLive=false;this.onTrigger(payload);}}
+module.exports=Trigger;
 /*
 The MIT License (MIT)
 Copyright (c) 2014-2017 Mike van Rossum mike@mvr.me
