51b88231dd76feea44d3633975fb6a9e6b03019a
UniversalBit <infouniversalbits@gmail.com>
2024-12-23 19:07:18 +0100

    Update STOCHRSI.js

--- strategies/STOCHRSI.js
+++ strategies/STOCHRSI.js
@@ -8,29 +8,31 @@ var settings = config.STOCHRSI;this.settings=settings;
 
 const { Chess } = require('chess.js');
 
-/* async fibonacci sequence */
-var fibonacci_sequence=['0','1','1','2','3','5','8','13','21','34','55','89','144','233','377','610','987','1597','2584','4181'];
-var seqms = fibonacci_sequence[Math.floor(Math.random() * fibonacci_sequence.length)];
-var sequence = ms => new Promise(resolve => setTimeout(resolve, seqms));
-async function sequence() {await sequence;};
-/* async keep calm and make something of amazing */
-var keepcalm = ms => new Promise(resolve => setTimeout(resolve,seqms));
-async function amazing() {console.log('keep calm and make something of amazing');await keepcalm;};
+const sequence = async function() {
+    try {
+    fibonacci_sequence=['0','1','1','2','3','5','8','13','21','34','55','89','144','233','377','610','987','1597','2584','4181','6765'];
+    var fibonacci_number = Math.floor(Math.random() * fibonacci_sequence.length);fibonacci_number = fibonacci_sequence[fibonacci_number];
+    await console.log ('Fibonacci Sequence -- Wohoo! -- Number: ',fibonacci_number);
+    }
+    catch (e) {
+    console.log (exchange.iso8601 (Date.now ()), e.constructor.name, e.message);
+    console.log ('Fibonacci Sequence -- Error -- ');
+    }
+};
 
-function AuxiliaryIndicators(){
-   var directory = 'indicators/';
-   var extension = '.js';
-   var files = ['ATR','StopLoss','STOCH'];  
-   for (var file of files){ 
-       var auxiliaryindicators = require('./' + directory + file + extension);
-       log.debug('added', auxiliaryindicators);
-   }
- }
+const keepcalm = async function() {
+    try {
+    await console.log('Keep Calm and Make Something of Amazing -- Wohoo! --');
+    }
+    catch (e) {
+    console.log (exchange.iso8601 (Date.now ()), e.constructor.name, e.message);
+    console.log ('Keep Calm and Make Something of Amazing  -- Error -- ');
+    }
+};
  
 var method = {};
 
 method.init = function() {
-  AuxiliaryIndicators();
   this.name = 'STOCHRSI';
   log.info('Start' ,this.name);
   this.trend = {direction: 'none',duration: 0,persisted: false,adviced: false};
@@ -38,7 +40,6 @@ method.init = function() {
   this.requiredHistory = this.settings.historySize;
   this.addTulipIndicator('rsi', 'rsi', {optInTimePeriod: this.settings.RSI,optInFastPeriod:89,optInSlowPeriod:21});
   this.addTulipIndicator('stoch', 'stoch', {optInFastKPeriod: 89,optInSlowKPeriod:21,optInSlowDPeriod:this.settings.STOCH});
-  this.addIndicator('stoploss', 'StopLoss', {threshold : this.settings.stoploss_threshold});
 
   RSIhistory=[];this.RSIhistory = RSIhistory;
 
@@ -83,8 +84,6 @@ return console.log(chess.pgn())
 
 method.check = function(candle)
 {
-    //if ('buy' === this.prevAction && this.settings.stoploss_enabled && 'stoploss' === this.indicators.stoploss.action) 
-      //{this.stoplossCounter++;log.debug('>>> STOPLOSS triggered <<<');this.advice('sell')} /* */
     
     log.debug("Operator ");this.makeoperator();log.debug("Random game of Chess");this.fxchess();
     rsi=this.tulipIndicators.rsi.result.result;this.rsi=rsi;
