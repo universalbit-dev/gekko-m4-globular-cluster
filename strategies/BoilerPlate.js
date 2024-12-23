1bd0759d8e5267b3e3ebf66167af190fab326814
UniversalBit <infouniversalbits@gmail.com>
2024-12-23 11:29:46 +0100

    Update BoilerPlate.js

--- strategies/BoilerPlate.js
+++ strategies/BoilerPlate.js
@@ -1,24 +1,37 @@
+const { addon: ov } = require('openvino-node');
 // BoilerPlate Strategy
-require('../core/tulind');
-
-/* async fibonacci sequence */
-var fibonacci_sequence=['0','1','1','2','3','5','8','13','21','34','55','89','144','233','377','610','987','1597','2584','4181'];
-var sequence = ms => new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * fibonacci_sequence.length)));
-async function sequence() {console.log('');await sequence;};
-
-/* async keep calm and make something of amazing */ 
-var keepcalm = ms => new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * fibonacci_sequence.length)));
-async function amazing() {console.log('keep calm and make something of amazing');await keepcalm;};
-
+const tulind=require('../core/tulind');
 var log = require('../core/log.js');
 var config = require('../core/util.js').getConfig();
-var settings = config.yoursettings;this.settings=settings;
+var settings = this.settings;
+
+const sequence = async function() {
+    try {
+    fibonacci_sequence=['0','1','1','2','3','5','8','13','21','34','55','89','144','233','377','610','987','1597','2584','4181','6765'];
+    var fibonacci_number = Math.floor(Math.random() * fibonacci_sequence.length);fibonacci_number = fibonacci_sequence[fibonacci_number];
+    await console.log ('Fibonacci Sequence -- Wohoo! -- Number: ',fibonacci_number);
+    }
+    catch (e) {
+    console.log (exchange.iso8601 (Date.now ()), e.constructor.name, e.message);
+    console.log ('Fibonacci Sequence -- Error -- ');
+    }
+};
+
+const keepcalm = async function() {
+    try {
+    await console.log('Keep Calm and Make Something of Amazing -- Wohoo! --');
+    }
+    catch (e) {
+    console.log (exchange.iso8601 (Date.now ()), e.constructor.name, e.message);
+    console.log ('Keep Calm and Make Something of Amazing  -- Error -- ');
+    }
+};
 
 var method = {};
 
 method.init = function() {}
 method.update = function(candle) {}
 method.log = function() {}
-method.check = function(candle) {}
+method.check = function(candle) {sequence();keepcalm();}
 method.end = function() {}
 module.exports = method;
