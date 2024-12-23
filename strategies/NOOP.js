ab8e9fb0cb66583de774c8500b665a2eeb28da51
UniversalBit <infouniversalbits@gmail.com>
2024-12-23 11:27:17 +0100

    Update NOOP.js

--- strategies/NOOP.js
+++ strategies/NOOP.js
@@ -1,43 +1,46 @@
 const { addon: ov } = require('openvino-node');
-require('../core/tulind');
+const tulind = require('../core/tulind');
 const { spawn } = require('node:child_process');
 const { setTimeout: setTimeoutPromise } = require('node:timers/promises');
 var log = require('../core/log.js');
 var util= require('../core/util.js')
 var config = require('../core/util.js').getConfig();
-const _ = require('../core/lodash');
+const _ = require('underscore');
 //https://cs.stanford.edu/people/karpathy/convnetjs/started.html
 var convnetjs = require('../core/convnet.js');
 var deepqlearn= require('../core/deepqlearn');
 var math = require('mathjs');
-var Promise = require("bluebird");
-var fs = Promise.promisifyAll(require("fs-extra"));fs.createReadStream('/dev/null');
+var fs = require("fs-extra");fs.createReadStream('/dev/null');
 
 var settings = config.NOOP;this.settings=settings;
 
-/* async fibonacci sequence */
-var fibonacci_sequence=['0','1','1','2','3','5','8','13','21','34','55','89','144','233','377','610','987','1597','2584','4181'];
-var seqms = fibonacci_sequence[Math.floor(Math.random() * fibonacci_sequence.length)];
-
-var sequence = ms => new Promise(resolve => setTimeout(resolve, seqms));
-async function sequence() {await sequence;
+const sequence = async function() {
+    try {
+    fibonacci_sequence=['0','1','1','2','3','5','8','13','21','34','55','89','144','233','377','610','987','1597','2584','4181','6765'];
+    var fibonacci_number = Math.floor(Math.random() * fibonacci_sequence.length);fibonacci_number = fibonacci_sequence[fibonacci_number];
+    await console.log ('Fibonacci Sequence -- Wohoo! -- Number: ',fibonacci_number);
+    }
+    catch (e) {
+    console.log (exchange.iso8601 (Date.now ()), e.constructor.name, e.message);
+    console.log ('Fibonacci Sequence -- Error -- ');
+    }
 };
 
-/* async keep calm and make something of amazing */
-var keepcalm = ms => new Promise(resolve => setTimeout(resolve,seqms));
-async function amazing() {console.log('keep calm and make something of amazing');await keepcalm;
+const keepcalm = async function() {
+    try {
+    await console.log('Keep Calm and Make Something of Amazing -- Wohoo! --');
+    }
+    catch (e) {
+    console.log (exchange.iso8601 (Date.now ()), e.constructor.name, e.message);
+    console.log ('Keep Calm and Make Something of Amazing  -- Error -- ');
+    }
 };
 
-// This method is a noop (it doesn't do anything)
-var method = {
-init : async function(){this.name = 'NOOP';
-this.addTulipIndicator('dema', 'dema', {optInTimePeriod: 1});
-},
+var method = {};
 
-update : async function(candle){_.noop},
-log : async function(){_.noop},
-check : async function(candle){_.noop;sequence();},
-end : async function() {_.noop}
-};
-
-module.exports = method;
+method.init = function() {_.noop}
+method.update = function(candle) {_.noop}
+method.log = function() {_.noop}
+method.check = function(candle) {sequence();keepcalm();}
+method.end = function() {_.noop}
+module.exports = method
