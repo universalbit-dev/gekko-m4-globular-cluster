761fdc07866029668de5ec6a271bd470dfefb273
UniversalBit <lazzaragiuliano@gmail.com>
2025-02-06 21:48:04 +0100

    Add files via upload

--- core/markets/importer.js
+++ core/markets/importer.js
@@ -1,121 +1,135 @@
-var Promise = require("bluebird");const _ = Promise.promisifyAll(require("underscore"));
+var _ = require('lodash');
+var moment = require('moment');
 var util = require('../util');
 var config = util.getConfig();
 var dirs = util.dirs();
 var log = require(dirs.core + 'log');
-var moment = require('moment');
-var gekkoEnv = util.gekkoEnv();
+var ccxt = require('ccxt');
 
-var adapter = config[config.adapter];
 var daterange = config.importer.daterange;
-var from = moment.utc(daterange.from);moment(from).format('DD-MM-YYYY');
+var from = moment.utc(daterange.from);
+var to = daterange.to ? moment.utc(daterange.to) : moment.utc();
 
-//https://en.wikipedia.org/wiki/NOP_(code)
-var noop = require('node-noop').noop;
-require('fs-extra').writeFile('noop.out',"Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away."+"Antoine de Saint-Exupery",noop);
+if (!from.isValid())
+    util.die('invalid `from`');
 
+if (!to.isValid())
+    util.die('invalid `to`');
 
-if(daterange.to) {
-  var to = moment.utc(daterange.to);moment(to).format('DD-MM-YYYY');
-} else {
-  var to = moment().utc();
-  log.debug(
-    'No end date specified for importing, setting to',
-    to.format()
-  );
-}
-log.debug(to.format());
-
-if(!from.isValid())
-  util.die('invalid `from`');
-
-if(!to.isValid())
-  util.die('invalid `to`');
+if (to <= from)
+    util.die('This daterange does not make sense.');
 
 var TradeBatcher = require(dirs.dlna + 'tradeBatcher');
 var CandleManager = require(dirs.dlna + 'candleManager');
 var exchangeChecker = require(dirs.gekko + 'exchange/exchangeChecker');
 
 var error = exchangeChecker.cantFetchFullHistory(config.watch);
-if(error)
-  util.die(error, true);
+if (error)
+    util.die(error, true);
 
-var fetcher = require(dirs.importers + config.watch.exchange);
+var Fetcher = function(config) {
+    this.exchange = new ccxt[config.watch.exchange]({
+        apiKey: config.watch.key,
+        secret: config.watch.secret,
+    });
+    util.makeEventEmitter(this);
+};
 
-if(to <= from)
-  util.die('This daterange does not make sense.')
+Fetcher.prototype.getTrades = async function(since, limit, handleFetch) {
+    try {
+        const trades = await this.exchange.fetchTrades(config.watch.currency + '/' + config.watch.asset, since, { limit: limit });
+        handleFetch(null, trades);
+    } catch (err) {
+        handleFetch(err, []);
+    }
+};
+
+util.makeEventEmitter(Fetcher);
 
 var Market = function() {
-  _.bindAll(this,_.functions(this));
-  this.exchangeSettings = exchangeChecker.settings(config.watch);
+    _.bindAll(this, _.functions(this));
+    this.exchangeSettings = exchangeChecker.settings(config.watch);
 
-  this.tradeBatcher = new TradeBatcher(this.exchangeSettings.tid);
-  this.candleManager = new CandleManager;
-  this.fetcher = fetcher({
-    to: to,
-    from: from
-  });
+    this.tradeBatcher = new TradeBatcher(this.exchangeSettings.tid);
+    this.candleManager = new CandleManager();
+    this.fetcher = new Fetcher(config.watch);
 
-  this.done = false;
+    this.done = false;
 
-  this.fetcher.bus.on(
-    'trades',
-    this.processTrades
-  );
+    this.fetcher.on('trades', this.processTrades);
+    this.fetcher.on('done', () => {
+        this.done = true;
+    });
 
-  this.fetcher.bus.on(
-    'done',
-    function() {
-      this.done = true;
-    }.bind(this)
-  )
+    this.tradeBatcher.on('new batch', this.candleManager.processTrades);
+    this.candleManager.on('candles', this.pushCandles);
 
-  this.tradeBatcher.on(
-    'new batch',
-    this.candleManager.processTrades
-  );
+    Readable.call(this, { objectMode: true });
 
-  this.candleManager.on(
-    'candles',
-    this.pushCandles
-  );
-
-  Readable.call(this, {objectMode: true});
-
-  this.get();
+    this.get();
 }
 
 var Readable = require('stream').Readable;
 Market.prototype = Object.create(Readable.prototype, {
-  constructor: { value: Market }
+    constructor: { value: Market }
 });
 
 Market.prototype._read = _.noop;
 
 Market.prototype.pushCandles = function(candles) {
-  _.each(candles, this.push);
+    _.each(candles, this.push);
 }
 
 Market.prototype.get = function() {
-  this.fetcher.fetch();
+    this.fetcher.getTrades(from, 500, this.handleFetch);
+}
+
+Market.prototype.handleFetch = function(err, trades) {
+    if (!err && !trades.length) {
+        console.log('no trades');
+        err = 'No trades';
+    }
+
+    if (err) {
+        log.error(`There was an error importing from ${config.watch.exchange}: ${err}`);
+        this.fetcher.emit('done');
+        return this.fetcher.emit('trades', []);
+    }
+
+    var last = moment.unix(_.last(trades).timestamp / 1000).utc();
+    lastId = _.last(trades).timestamp;
+
+    if (last < from) {
+        log.debug('Skipping data, they are before from date', last.format());
+        return this.get();
+    }
+
+    if (last > to || lastId === prevLastId) {
+        this.fetcher.emit('done');
+        var endUnix = to.unix();
+        trades = _.filter(trades, t => t.timestamp / 1000 <= endUnix);
+    }
+
+    prevLastId = lastId;
+    this.fetcher.emit('trades', trades);
 }
 
 Market.prototype.processTrades = function(trades) {
-  this.tradeBatcher.write(trades);
+    this.tradeBatcher.write(trades);
 
-  if(this.done) {
-    log.info('Done importing!');
-    this.emit('end');
-    return;
-  }
+    if (this.done) {
+        log.info('Done importing!');
+        this.emit('end');
+        return;
+    }
 
-  if(_.size(trades) && gekkoEnv === 'child-process') {
-    let lastAtTS = _.last(trades).date;
-    let lastAt = moment.unix(lastAtTS).utc().format();
-    process.send({event: 'marketUpdate', payload: lastAt});
-  }
+    if (_.size(trades)) {
+        let lastAtTS = _.last(trades).timestamp;
+        let lastAt = moment.unix(lastAtTS / 1000).utc().format();
+        process.send({ event: 'marketUpdate', payload: lastAt });
+    }
 
-  setTimeout(this.get, 1000);
+    setTimeout(this.get, 1000);
 }
 
 module.exports = Market;
