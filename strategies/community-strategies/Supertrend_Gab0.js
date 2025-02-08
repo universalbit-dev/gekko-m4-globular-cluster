d12ae278e34e58666a3728ada42e2874acd36606
UniversalBit <lazzaragiuliano@gmail.com>
2025-02-08 11:02:25 +0100

    Update Supertrend_Gab0.js

--- strategies/community-strategies/Supertrend_Gab0.js
+++ strategies/community-strategies/Supertrend_Gab0.js
@@ -5,19 +5,19 @@ This JavaScript code defines a trading strategy for the Gekko trading bot, speci
         The code imports a logging module.
         It defines an empty strategy object strat.
 
-    Initialization Function (strat.init):
+    Initialization Function (method.init):
         Sets the required history size for the strategy.
         Adds an Average True Range (ATR) indicator with settings for exponential moving average (EMA).
         Initializes variables to keep track of the Supertrend bands and the last candle close price.
         Initializes a bought flag to track whether a position has been taken.
 
-    Update Function (strat.update):
+    Update Function (method.update):
         This function is called on every new candle, but it is currently empty and can be customized as needed.
 
-    Log Function (strat.log):
+    Log Function (method.log):
         This function is used for debugging purposes but is currently empty.
 
-    Check Function (strat.check):
+    Check Function (method.check):
         This function is called on every new candle to decide whether to buy or sell.
         Calculates the upper and lower basic bands using the ATR result and a band factor setting.
         Adjusts the upper and lower bands based on the previous Supertrend values and the last candle's close price.
@@ -26,11 +26,9 @@ This JavaScript code defines a trading strategy for the Gekko trading bot, speci
         Issues a "short" advice (sell) if the candle close price is below the Supertrend and a position is currently held.
         Updates the last candle close price and the last Supertrend values for the next iteration.
 
-    Exports:
-        The strategy object strat is exported for use by the Gekko trading bot.
-
 The strategy essentially uses the Supertrend indicator to decide when to buy or sell based on the market trends and price movements.
 */
+
 // Downloaded from: https://github.com/xFFFFF/Gekko-Strategies
 // Source: https://github.com/Gab0/gekko-adapted-strategies
 
