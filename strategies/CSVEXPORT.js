447098c62000cb9fe10b7c2e3ecfffed987a18ee
UniversalBit <lazzaragiuliano@gmail.com>
2025-02-08 22:13:13 +0100

    Update CSVEXPORT.js

--- strategies/CSVEXPORT.js
+++ strategies/CSVEXPORT.js
@@ -1,36 +1,44 @@
 /*
-  Export data to .csv
-
-	(CC-BY-SA 4.0) Rowan Griffin
-	https://creativecommons.org/licenses/by-sa/4.0/
-
+  Export data to file .csv 
+  (CC-BY-SA 4.0) Rowan Griffin
+  https://creativecommons.org/licenses/by-sa/4.0/
 */
-require('../core/tulind');
+
+const ccxt = require("ccxt");
 var log = require('../core/log.js');
 var config = require('../core/util.js').getConfig();
-const _ = require("underscore");
-let fs = require('fs-extra');
+let _ = require('../core/lodash');
+let fs = require('fs-extra');fs.createReadStream('/dev/null');
+var Wrapper = require('../strategyWrapperRules.js');
+var method = Wrapper;
 
-var method = {
-
-  /* INIT */
-  init: function() {
-    this.name = 'CSVExport';
+var id = 'kraken'; /* Exchange Name */
+//ccxt histogram variables
+var candle_open=0.00;var candle_high=0.00;var candle_low=0.00;var candle_close=0.00;var candle_volume=0.00;
+var exchange = new ccxt[id] ({
+        verbose: false,
+        apiKey: process.env.API_KEY || '',
+        secret: process.env.API_SECRET || '',
+    });
+    
+/* INIT */
+  method.init = function() {
+    this.name = 'CSVEXPORT';
     this.requiredHistory = this.settings.historySize;
     this.startTime = new Date();
     this.debug = true;
   },
  
-//general purpose log  {data}
-update: function(candle) {
-    fs.appendFile('logs/csv/' + config.watch.asset + ':' + config.watch.currency + ' ' + this.startTime + '.csv', candle.high + "," + candle.low + "," + candle.close + "," + candle.volume + "," + candle.trades + "\n", function(err) {
-      if (err) {return console.log(err);}
-    });
+//General Purpose Log  {data}
+  method.update = function(candle) {
+    fs.appendFile('logs/csv/' + config.watch.asset + ':' + config.watch.currency + ' ' + this.startTime + '.csv', candle.high + "," + candle.low + "," + candle.close + "," +
+    candle.volume + "," + candle.trades + "\n", function(err) 
+    {if (err) {return console.log(err);}});
   },
-  /* CHECK */
-  check: function() {_.noop;},
-  /* END backtest */
-  end: function() {_.noop;}
-};
-
-module.exports = method;
+	  
+/* CHECK */
+  method.check = function() {_.noop;},
+	  
+/* END backtest */
+  method.end = function() {_.noop;}
+  module.exports = method;
