9cff5253a4bffba51b1292b43a07123ec9a32ff3
UniversalBit <lazzaragiuliano@gmail.com>
2025-02-09 12:49:48 +0100

    Update trade.config.js

--- trade.config.js
+++ trade.config.js
@@ -3,14 +3,14 @@ module.exports = {
   {
   name: 'neuralnet',
   script    : 'gekko.js',
-  args      : '-c .env/trade/trade_neuralnet_simulator.js',
+  args      : '-c env/trade/trade_neuralnet_simulator.js',
   instances : "1",
   exec_mode : "cluster"
   },
   {
   name: 'noop',
   script    : 'gekko.js',
-  args      : '-c .env/trade/trade_noop_simulator.js',
+  args      : '-c env/trade/trade_noop_simulator.js',
   instances : "1",
   exec_mode : "cluster"
   }
