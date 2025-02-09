80ac59475325ad67318c94be1c95c0d3e506e3be
UniversalBit <lazzaragiuliano@gmail.com>
2025-02-09 23:03:30 +0100

    Update trade_rsibullbearadx_simulator.js

--- env/trade/trade_rsibullbearadx_simulator.js
+++ env/trade/trade_rsibullbearadx_simulator.js
@@ -5,11 +5,10 @@ config.watch = {exchange: process.env.exchange,exchangeId:process.env.exchangeId
 
 config.trader={enabled:false,
 exchange:'exchangesimulator',currency:'BTC',asset:'LTC',key:'',secret:''};
-config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'RSIBULLBEARADX'};
 
-config.RSIBULLBEARADX={SMA_long:10,SMA_short:55,RSI:14,BULL_RSI:10,
-BULL_RSI_high:80,BULL_RSI_low:60,BEAR_RSI:15,BEAR_RSI_high:50,BEAR_RSI_low:20,
-BULL_MOD_high:5,BULL_MOD_low:-5,BEAR_MOD_high:15,BEAR_MOD_low:-5,ADX:3,ADX_high:70,ADX_low:50};
+config.tradingAdvisor = {enabled:true,candleSize:5,historySize:10,method:'RSIBULLBEARADX'};
+config.RSIBULLBEARADX={SMA_long:1000,SMA_short:50,BULL_RSI:10,BULL_RSI_high:80,
+BULL_RSI_low:60,BEAR_RSI:15,BEAR_RSI_high:60,BEAR_RSI_low:20,BULL_mod_high:5,BULL_mod_low :-5,BEAR_mod_high:15,BEAR_mod_low:-5,RSI:14,ADX:3,ADX_high:70,ADX_low:50};
 
 config.adapter='sqlite';config.adapter.path= 'plugins/sqlite';
 config.sqlite = {path: 'plugins/sqlite',dataDirectory: 'history',version:'5.1.1',
@@ -33,7 +32,7 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 Disclaimer:
                               USE AT YOUR OWN RISK!
-The author of this project is NOT responsible for any damage or loss caused
+The author of this project and contributors are NOT responsible for any damage or loss caused
 by this software. There can be bugs and the bot may not perform as expected
 or specified. Please consider testing it first with paper trading and/or
 backtesting on historical data. Also look at the code to see what how
