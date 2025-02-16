dadd93c613727ac25c8de47507e352f1d18c24a3
UniversalBit <lazzaragiuliano@gmail.com>
2025-02-16 10:21:12 +0100

    Update ccxtBroker.js
    duplicate setTicker method.

--- exchange/ccxtBroker.js
+++ exchange/ccxtBroker.js
@@ -113,35 +113,15 @@ class CcxtBroker extends EventEmitter {
   }
 }
 
-   async setTicker() {
-    try {
-      const ticker = await this.api.fetchTicker(this.market);
-      this.ticker = ticker;
-    } catch (err) {
-      console.log(this.api.name, err.message);
+  async setTicker() {
+  try {
+    const ticker = await this.api.fetchTicker(this.market);
+    this.ticker = ticker;
+  } catch (err) {
+    console.log(this.api.name, err.message);
       throw new errors.ExchangeError(err);
-    }
   }
-  setTicker(callback) {
-    this.api.getTicker((err, ticker) => {
-
-      if(err) {
-        if(err.message) {
-          console.log(this.api.name, err.message);
-          throw err;
-        } else {
-          console.log('err not wrapped in error:', err);
-          throw new errors.ExchangeError(err);
-        }
-      }
-
-      this.ticker = ticker;
-
-      if(_.isFunction(callback))
-        callback();
-    });
   }
-
 
   isValidOrder(amount, price) {
     return isValidOrder({
@@ -208,4 +188,3 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-
