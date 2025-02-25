90d0d47af0550fea67d076e313c7b52c95061073
UniversalBit <lazzaragiuliano@gmail.com>
2025-02-25 08:45:03 +0100

    Update trade_rsibullbearadx_simulator.js

--- env/trade/trade_rsibullbearadx_simulator.js
+++ env/trade/trade_rsibullbearadx_simulator.js
@@ -7,7 +7,7 @@ config.trader={enabled:false,
 exchange:process.env.exchange,exchangeId:process.env.exchangeId,currency:process.env.currency,asset:process.env.asset,key:process.env.key,secret:process.env.secret};
 
 config.tradingAdvisor = {enabled:true,candleSize:1,historySize:987,method:'RSIBULLBEARADX'};
-config.RSIBULLBEARADX={SMA_long:1000,SMA_short:50,BULL_RSI:10,BULL_RSI_high:80,
+config.RSIBULLBEARADX={SMA_long:987,SMA_short:50,BULL_RSI:10,BULL_RSI_high:80,
 BULL_RSI_low:60,BEAR_RSI:15,BEAR_RSI_high:60,BEAR_RSI_low:20,BULL_mod_high:5,BULL_mod_low :-5,BEAR_mod_high:15,BEAR_mod_low:-5,RSI:14,ADX:3,ADX_high:70,ADX_low:50};
 
 config.adapter='sqlite';config.adapter.path= 'plugins/sqlite';
