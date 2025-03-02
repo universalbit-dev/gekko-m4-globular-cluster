170f6e170ed86d0fc42ee9a4c477551253e3899c
UniversalBit <lazzaragiuliano@gmail.com>
2025-03-02 09:21:04 +0100

    Update parent.js
    blend parent.js and child.js

--- core/workers/loadCandles/parent.js
+++ core/workers/loadCandles/parent.js
@@ -1,17 +1,37 @@
-const fork = require('child_process').fork;const { EventEmitter } = require("events");
-var Promise = require("bluebird");const _ = Promise.promisifyAll(require("underscore"));
+const fork = require('child_process').fork;
+const { EventEmitter } = require("events");
+const _ = require("underscore");
+
+var start = (config, candleSize, daterange) => {
+  var util = require(__dirname + '/../../util');
+  util.setGekkoEnv('child-process');
+  config.debug = false;
+  util.setConfig(config);
+
+  var dirs = util.dirs();
+  var load = require(dirs.tools + 'candleLoader');
+  load(config.candleSize, candles => { this.send(candles); });
+}
 
 module.exports = (config, callback) => {
   var debug = typeof v8debug === 'object';
-  if (debug) {process.execArgv = [];}
+  if (debug) { process.execArgv = []; }
 
   const child = fork(__dirname + '/child');
-  const message = {what: 'start',config}
+  const message = { what: 'start', config }
   const done = _.once(callback);
-  this.on('message', function(m) {if(m === 'ready') return this.send(message);done(null, m);
-  if (this.connected) {this.disconnect();}
+  this.on('message', function (m) {
+    if (m === 'ready') return this.send(message);
+    done(null, m);
+    if (this.connected) { this.disconnect(); }
   });
-  this.on('exit', code => {if(code !== 0) done('ERROR, unable to load candles, please check the console.');});
+  this.on('exit', code => { if (code !== 0) done('ERROR, unable to load candles, please check the console.'); });
+
+  this.send('ready');
+  this.on('message', (m) => {
+    if (m.what === 'start') start(m.config, m.candleSize, m.daterange);
+  });
+  this.on('disconnect', function () { process.exit(0); });
 }
 
 /*
