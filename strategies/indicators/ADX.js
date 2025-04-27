e9067c6ea1d9eb169d55095c406336d158287e0a
UniversalBit <lazzaragiuliano@gmail.com>
2025-04-28 00:32:24 +0200

    Update ADX.js

--- strategies/indicators/ADX.js
+++ strategies/indicators/ADX.js
@@ -1,42 +1,52 @@
-// Average Directional Movement Index indicator;
-// usable on gekko trading bot. Same license as gekko.
-// "ported" from tulip: https://tulipindicators.org/adx
-// gab0 - 2018
+// Average Directional Movement Index (ADX) Indicator
+// Usable with the Gekko trading bot. Same license as Gekko.
+// Ported from Tulip Indicators: https://tulipindicators.org/adx
+// Author: gab0 - 2018
 
-var DX = require('./DX.js');
+const DX = require('./DX.js');
 
-var Indicator = function (period)
-{
+class ADXIndicator {
+  constructor(period) {
+    if (period <= 0) {
+      throw new Error("Period must be a positive integer.");
+    }
+
     this.input = 'candle';
     this.indicates = 'trend_strength';
 
-    this.dx = new DX(period);
+    this.dx = new DX(period); // DX indicator instance
 
-    this.result = 0;
-    this.periodRatio = (period - 1)/period;
-    this.initadx = 0;
-    this.initialized = 1;
-    this.period = period;
-}
+    this.result = 0; // Current ADX result
+    this.periodRatio = (period - 1) / period;
+    this.initSumDX = 0; // Sum of DX values for initialization
+    this.initializedCount = 0; // Counter for initialization phase
+    this.period = period; // ADX calculation period
+  }
 
-Indicator.prototype.update = function (candle)
-{
-    this.dx.update(candle);
-
-    if (this.initialized > this.period)
-        this.result = this.periodRatio * this.result + this.dx.result/this.period;
-    else if (this.initialized == this.period)
-    {
-        this.initialized++;
-        this.result = this.initadx / this.period;
-
-    } else if (this.dx.result)
-    {
-        this.initadx += this.dx.result;
-        this.initialized +=1;
+  update(candle) {
+    if (!candle) {
+      throw new Error("Invalid candle data provided.");
     }
 
-    this.age++
+    // Update the DX indicator with the new candle
+    this.dx.update(candle);
+
+    // Initialization phase: Aggregate DX values
+    if (this.initializedCount < this.period) {
+      if (this.dx.result) {
+        this.initSumDX += this.dx.result;
+        this.initializedCount++;
+      }
+
+      // Once initialized, calculate the initial ADX value
+      if (this.initializedCount === this.period) {
+        this.result = this.initSumDX / this.period;
+      }
+    } else {
+      // Main ADX calculation phase
+      this.result = this.periodRatio * this.result + this.dx.result / this.period;
+    }
+  }
 }
 
-module.exports = Indicator;
+module.exports = ADXIndicator;
