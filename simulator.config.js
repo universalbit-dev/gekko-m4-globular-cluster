0e08fc0c18acf89f2074cecf55ce9ac78e677eb3
UniversalBit <lazzaragiuliano@gmail.com>
2025-06-10 11:57:02 +0200

    Add files via upload

--- simulator.config.js
+++ simulator.config.js
@@ -8,31 +8,11 @@ module.exports = {
   exec_mode : "cluster"
   },
   {
-    name: "grafana_neuralnet",
-    script: "serve",
-    env: {
-      PM2_SERVE_PATH: "./logs/json",
-      PM2_SERVE_PORT: 9560,
-      PM2_SERVE_SPA: true,
-      PM2_SERVE_HOMEPAGE: "./logs/json/neuralnet.json"
-    }
-  },
-  {
   name: 'cci_simulator',
   script    : 'gekko.js',
   args      : '-c env/simulator/trade_cci_simulator.js',
   instances : "1",
   exec_mode : "cluster"
-  },
-  {
-    name: "grafana_cci",
-    script: "serve",
-    env: {
-      PM2_SERVE_PATH: "./logs/json",
-      PM2_SERVE_PORT: 9561,
-      PM2_SERVE_SPA: true,
-      PM2_SERVE_HOMEPAGE: "./logs/json/cci.json"
-    }
   },
   {
   name: 'dema_simulator',
@@ -42,31 +22,11 @@ module.exports = {
   exec_mode : "cluster"
   },
   {
-    name: "grafana_dema",
-    script: "serve",
-    env: {
-      PM2_SERVE_PATH: "./logs/json",
-      PM2_SERVE_PORT: 9562,
-      PM2_SERVE_SPA: true,
-      PM2_SERVE_HOMEPAGE: "./logs/json/dema.json"
-    }
-  },
-  {
   name : 'rsibullbearadx_simulator',
   script    : 'gekko.js',
   args      : '-c env/simulator/trade_rsibullbearadx_simulator.js',
   instances : "1",
   exec_mode : "cluster"
-  },
-  {
-    name: "grafana_rsibullbearadx",
-    script: "serve",
-    env: {
-      PM2_SERVE_PATH: "./logs/json",
-      PM2_SERVE_PORT: 9563,
-      PM2_SERVE_SPA: true,
-      PM2_SERVE_HOMEPAGE: "./logs/json/rsibullbearadx.json"
-    }
   },
   {
   name: 'bollingerband_simulator',
@@ -76,31 +36,11 @@ module.exports = {
   exec_mode : "cluster"
   },
   {
-    name: "grafana_bollingerband",
-    script: "serve",
-    env: {
-      PM2_SERVE_PATH: "./logs/json",
-      PM2_SERVE_PORT: 9564,
-      PM2_SERVE_SPA: true,
-      PM2_SERVE_HOMEPAGE: "./logs/json/bollingerband.json"
-    }
-  },
-  {
   name: 'noop_simulator',
   script    : 'gekko.js',
   args      : '-c env/simulator/trade_noop_simulator.js',
   instances : "1",
   exec_mode : "cluster"
-  },
-  {
-    name: "grafana_noop",
-    script: "serve",
-    env: {
-      PM2_SERVE_PATH: "./logs/json",
-      PM2_SERVE_PORT: 9565,
-      PM2_SERVE_SPA: true,
-      PM2_SERVE_HOMEPAGE: "./logs/json/noop.json"
-    }
   },
   {
   name: 'neuralnet_refinements_simulator',
@@ -110,31 +50,11 @@ module.exports = {
   exec_mode : "cluster"
   },
   {
-    name: "grafana_neuralnet_refinements",
-    script: "serve",
-    env: {
-      PM2_SERVE_PATH: "./logs/json",
-      PM2_SERVE_PORT: 9566,
-      PM2_SERVE_SPA: true,
-      PM2_SERVE_HOMEPAGE: "./logs/json/neuralnet_refinements.json"
-    }
-  },
-  {
      name: "csvexport",
      script    : 'gekko.js',
      args      : '-c env/simulator/trade_csvexport.js',
      instances : "1",
      exec_mode : "cluster"
-  },
-  {
-    name: "ohlcv_data",
-    script: "serve",
-    env: {
-      PM2_SERVE_PATH: "./logs/csv",
-      PM2_SERVE_PORT: 9567,
-      PM2_SERVE_SPA: true,
-      PM2_SERVE_HOMEPAGE: "./logs/csv/ohlcv_data.csv"
-    }
   },
   {
     name: "pm2-cpu-watchdog",
