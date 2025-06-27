4bee706e9262e91105a4c6c4ef05548691a03d9c
UniversalBit <lazzaragiuliano@gmail.com>
2025-06-27 17:42:18 +0200

    Add files via upload

--- tools/tools.config.js
+++ tools/tools.config.js
@@ -3,42 +3,40 @@ module.exports = {
   {
   name: 'train ccxt ohlcv',
   script    : 'train_ccxt_ohlcv.js',
-  args      : '',
   instances : "1",
   exec_mode : "cluster"
   },
   {
   name: 'train ohlcv',
   script    : 'train_ohlcv.js',
-  args      : '',
   instances : "1",
   exec_mode : "cluster"
   },
   {
   name: 'chart ccxt recognition',
-  script    : 'chart_ccxt_recognition.js',
-  args      : '',
-  instances : "1",
-  exec_mode : "cluster"
+  script: 'chart_ccxt_recognition.js',
+  instances: 1,
+  exec_mode: 'cluster',
+  cron_restart: '0 * * * *',//1Hour
+  autorestart: false        
   },
   {
   name: 'chart ccxt recognition fine grained',
-  script    : 'chart_ccxt_recognition_fine_grained.js',
-  args      : '',
-  instances : "1",
-  exec_mode : "cluster"
+  script: 'chart_ccxt_recognition_fine_grained.js',
+  instances: 1,
+  exec_mode: 'cluster',
+  cron_restart: '*/15 * * * *',//15min
+  autorestart: false
   },
   {
   name: 'chart recognition',
   script    : 'chart_recognition.js',
-  args      : '',
   instances : "1",
   exec_mode : "cluster"
   },
   {
   name: 'pm2-tools-watchdog',
   script    : 'pm2-tools-watchdog.js',
-  args      : '',
   instances : "1",
   exec_mode : "cluster"
   },
