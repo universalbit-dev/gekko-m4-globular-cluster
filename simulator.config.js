516175a8ac9f246082d530ddcc41ae5f9a8f67c1
UniversalBit <lazzaragiuliano@gmail.com>
2025-06-28 16:08:03 +0200

    Add files via upload

--- simulator.config.js
+++ simulator.config.js
@@ -4,62 +4,78 @@ module.exports = {
   name: 'neuralnet_simulator',
   script    : 'gekko.js',
   args      : '-c env/simulator/trade_neuralnet_simulator.js',
-  instances : "1",
-  exec_mode : "cluster"
+  instances : "max",
+  exec_mode : "cluster",
+  cron_restart: '*/45 * * * *',//45min
+  autorestart: false
   },
   {
   name: 'cci_simulator',
   script    : 'gekko.js',
   args      : '-c env/simulator/trade_cci_simulator.js',
-  instances : "1",
-  exec_mode : "cluster"
+  instances : "max",
+  exec_mode : "cluster",
+  cron_restart: '*/45 * * * *',//45min
+  autorestart: false
   },
   {
   name: 'dema_simulator',
   script    : 'gekko.js',
   args      : '-c env/simulator/trade_dema_simulator.js',
-  instances : "1",
-  exec_mode : "cluster"
+  instances : "max",
+  exec_mode : "cluster",
+  cron_restart: '*/45 * * * *',//45min
+  autorestart: false
   },
   {
   name : 'rsibullbearadx_simulator',
   script    : 'gekko.js',
   args      : '-c env/simulator/trade_rsibullbearadx_simulator.js',
-  instances : "1",
-  exec_mode : "cluster"
+  instances : "max",
+  exec_mode : "cluster",
+  cron_restart: '*/45 * * * *',//45min
+  autorestart: false
   },
   {
   name: 'bollingerband_simulator',
   script    : 'gekko.js',
   args      : '-c env/simulator/trade_bollingerband_simulator.js',
-  instances : "1",
-  exec_mode : "cluster"
+  instances : "max",
+  exec_mode : "cluster",
+  cron_restart: '*/45 * * * *',//45min
+  autorestart: false
   },
   {
   name: 'noop_simulator',
   script    : 'gekko.js',
   args      : '-c env/simulator/trade_noop_simulator.js',
-  instances : "1",
-  exec_mode : "cluster"
+  instances : "max",
+  exec_mode : "cluster",
+  cron_restart: '*/45 * * * *',//45min
+  autorestart: false
   },
   {
   name: 'neuralnet_refinements_simulator',
   script    : 'gekko.js',
   args      : '-c env/simulator/trade_neuralnet_simulator.js',
-  instances : "1",
-  exec_mode : "cluster"
+  instances : "max",
+  exec_mode : "cluster",
+  cron_restart: '*/45 * * * *',//45min
+  autorestart: false
   },
   {
-     name: "csvexport",
-     script    : 'gekko.js',
-     args      : '-c env/simulator/trade_csvexport.js',
-     instances : "1",
-     exec_mode : "cluster"
+  name: "csvexport",
+  script    : 'gekko.js',
+  args      : '-c env/simulator/trade_csvexport.js',
+  instances : "max",
+  exec_mode : "cluster",
+  cron_restart: '*/45 * * * *',//45min
+  autorestart: false
   },
   {
-    name: "pm2-cpu-watchdog",
-    script: "./pm2-cpu-watchdog.js",
-    watch: false
+  name: "pm2-cpu-watchdog",
+  script: "./pm2-cpu-watchdog.js",
+  watch: false
   },
 ]
 }
