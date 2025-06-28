85a6164c039a8fa12a0b0202eba8791971e66708
UniversalBit <lazzaragiuliano@gmail.com>
2025-06-28 16:11:03 +0200

    Update jsonbin_chess_uploader.sh

--- logs/json/chess/jsonbin_chess_uploader.sh
+++ logs/json/chess/jsonbin_chess_uploader.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#
+# jsonbin_chess_uploader.sh
+#
+# Description:
+#   This script uploads chess-related JSON log files to a remote server or service.
+#   It is intended for use in the gekko-m4-globular-cluster project.
+#
+# Usage:
+#   ./jsonbin_chess_uploader.sh
+#
+# Author: universalbit-dev
+# Repository: https://github.com/universalbit-dev/gekko-m4-globular-cluster
+# Last Updated: 2025-06-28
+#
 
 # Start the jsonbin uploader via PM2
 pm2 start jsonbin_randomchess.js --name randomchess
