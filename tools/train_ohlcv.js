1620e2cdb43bc8807188196bf6e561231f4ffe68
UniversalBit <lazzaragiuliano@gmail.com>
2025-08-16 13:52:14 +0200

    Update train_ohlcv.js

--- tools/train_ohlcv.js
+++ tools/train_ohlcv.js
@@ -1,6 +1,5 @@
 // train_ohlcv.js
 require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
-const path = require('path');
 const fs = require('fs');
 const dir = path.resolve(__dirname, 'trained_ohlcv'); 
 const ConvNet = require('../core/convnet.js'); // Adjust path as needed
