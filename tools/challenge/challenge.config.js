ef94b0f947e4f4d403dab0bebe9840054541845b
UniversalBit <lazzaragiuliano@gmail.com>
2025-10-05 09:10:57 +0200

    Add files via upload

---
+++ tools/challenge/challenge.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: "challenge_log_writer",
+      script: "challenge_log_writer.js",
+      instances: 1,
+      exec_mode: "cluster",
+      cwd: __dirname,
+      watch: false,
+      env: {
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "challenge_analysis",
+      script: "challenge_analysis.js",
+      instances: 1,
+      exec_mode: "cluster",
+      cwd: __dirname,
+      watch: false,
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
