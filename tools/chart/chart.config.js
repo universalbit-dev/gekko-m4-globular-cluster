faee83b4422e5b09c2244c9fe1683585ab754617
UniversalBit <lazzaragiuliano@gmail.com>
2025-10-22 10:50:52 +0200

    Add chart configuration for multiple apps

---
+++ tools/chart/chart.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  apps: [
+    {
+      name: "chart_ccxt_multi",
+      script: "chart_ccxt_multi.js",
+      instances: 1,
+      exec_mode: "cluster",
+      cwd: __dirname,
+      watch: false,
+      env: {
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "chart_ccxt_recognition",
+      script: "chart_ccxt_recognition.js",
+      instances: 1,
+      exec_mode: "cluster",
+      cwd: __dirname,
+      watch: false,
+      env: {
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "chart_ccxt_recognition_magnitude",
+      script: "chart_ccxt_recognition_magnitude.js",
+      instances: 1,
+      exec_mode: "cluster",
+      cwd: __dirname,
+      watch: false,
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
